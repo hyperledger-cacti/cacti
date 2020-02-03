24e7ebf7b03d0f8779dd05bb0d96c8c6ce4d2295
Ben LeFevre <blefevre@users.noreply.github.com>
2020-02-03 11:42:02 -0500

    feature: Add node 10 support
    Signed-off-by: Ben LeFevre <blefevre@users.noreply.github.com>

--- packages/core/examples/simple-asset-transfer/fabric/scripts/start.sh
+++ packages/core/examples/simple-asset-transfer/fabric/scripts/start.sh
@@ -17,7 +17,7 @@ echo " [Logs after : $timestamp]" >>$path_to_log_file
 
 echo -n "Starting the SDK server ... "
 cd ../api
-PORT=4000 node app >>$path_to_log_file 2>&1 &
+HOST=0.0.0.0 PORT=4000 node app >>$path_to_log_file 2>&1 &
 cd -
 sleep 10
 while ! lsof -i :4000 -sTCP:LISTEN -t; do
