2299cff9931996a979b9b1e0ddb492843de916c0
Peter Somogyvari <peter.somogyvari@accenture.com>
2020-05-16 23:55:06 -0700

    feat(cactus-common): add Objects utility class to get owned and inherited methods of class instance
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-common/src/main/typescript/public-api.ts
+++ packages/cactus-common/src/main/typescript/public-api.ts
@@ -1,3 +1,4 @@
 export { LoggerProvider } from "./logging/logger-provider";
 export { Logger, ILoggerOptions } from "./logging/logger";
 export { LogLevelDesc } from "loglevel";
+export { Objects } from "./objects";
