4a1c37e359ce082b2f36e473c520e2195d28eb9b
Jacob Weate <jacob.weate@accenture.com>
2020-06-09 18:16:21 -0400

    Build and deploy cactus cordapps
    add source code to build the cactus cordapps in the container and modify init script to do so

--- tools/all-in-one/corda/init_node.sh
+++ tools/all-in-one/corda/init_node.sh
@@ -1,2 +1,13 @@
+echo "Gradle build..."
+cd /opt/corda/corda-source
+./gradlew build -x test
+
+echo "Gradle deploy..."
+./gradlew deployNodes
+
+echo "Copying cordapps to Network.."
+cp /opt/corda/corda-source/build/nodes/PartyA/cordapps/workflows-0.1.jar /opt/corda/partyA/cordapps/workflows-0.1.jar
+cp /opt/corda/corda-source/build/nodes/PartyA/cordapps/contracts-0.1.jar  /opt/corda/partyA/cordapps/contracts-0.1.jar
+
 echo "Starting corda nodes..."
 java -jar /opt/corda/partyA/corda.jar --base-directory=/opt/corda/partyA
