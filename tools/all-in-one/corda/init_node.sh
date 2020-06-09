d731a4cde6028fba90f904c71217d00b98212550
Jacob Weate <jacob.weate@accenture.com>
2020-06-09 18:19:24 -0400

    Add PartyB Node
    Add the files needed to run PartyB and the additional-node-info files so both nodes know about each other

--- tools/all-in-one/corda/init_node.sh
+++ tools/all-in-one/corda/init_node.sh
@@ -8,6 +8,8 @@ echo "Gradle deploy..."
 echo "Copying cordapps to Network.."
 cp /opt/corda/corda-source/build/nodes/PartyA/cordapps/workflows-0.1.jar /opt/corda/partyA/cordapps/workflows-0.1.jar
 cp /opt/corda/corda-source/build/nodes/PartyA/cordapps/contracts-0.1.jar  /opt/corda/partyA/cordapps/contracts-0.1.jar
+cp /opt/corda/corda-source/build/nodes/PartyA/cordapps/workflows-0.1.jar /opt/corda/partyB/cordapps/workflows-0.1.jar
+cp /opt/corda/corda-source/build/nodes/PartyA/cordapps/contracts-0.1.jar  /opt/corda/partyB/cordapps/contracts-0.1.jar
 
 echo "Starting corda nodes..."
-java -jar /opt/corda/partyA/corda.jar --base-directory=/opt/corda/partyA
+java -jar /opt/corda/partyA/corda.jar --base-directory=/opt/corda/partyA & java -jar /opt/corda/partyB/corda.jar --base-directory=/opt/corda/partyB && fg
