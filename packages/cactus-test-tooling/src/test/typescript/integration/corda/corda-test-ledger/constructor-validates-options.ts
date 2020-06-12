09f269a55626dede4069b710ed274c4b971c4f73
Jacob Weate <jacob.weate@accenture.com>
2020-06-12 15:37:39 -0400

    stop and destroy in test case

--- packages/cactus-test-tooling/src/test/typescript/integration/corda/corda-test-ledger/constructor-validates-options.ts
+++ packages/cactus-test-tooling/src/test/typescript/integration/corda/corda-test-ledger/constructor-validates-options.ts
@@ -12,3 +12,25 @@ tap.test("constructor throws if invalid input is provided", (assert: any) => {
   assert.throws(() => new CordaTestLedger({ containerImageVersion: "nope" }));
   assert.end();
 });
+
+tap.test(
+  "constructor does not throw if valid input is provided",
+  (assert: any) => {
+    assert.ok(CordaTestLedger);
+    assert.doesNotThrow(() => new CordaTestLedger());
+    assert.end();
+  }
+);
+
+tap.test("starts/stops/destroys a docker container", async (assert: any) => {
+  const cordaTestLedger = new CordaTestLedger();
+  const container: Container = await cordaTestLedger.start();
+  assert.ok(container);
+  const ipAddress: string = await cordaTestLedger.getContainerIpAddress();
+  assert.ok(ipAddress);
+  assert.ok(ipAddress.length);
+
+  await besuTestLedger.stop();
+  await besuTestLedger.destroy();
+  assert.end();
+});
