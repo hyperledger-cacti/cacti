7f00c6ee4c48ad84ddaf09177ef803104f43d87d
Peter Somogyvari <peter.somogyvari@accenture.com>
2020-06-15 21:23:15 -0700

    feat(common): add utility class Strings with replaceAll
    This is a method that is used pretty often but not included
    in the Javascript language itself.
    What the method does is what you would expect, it finds
    all occurrences of a substring and replaces all of them with
    another one. The deafult Javascript String.replace function
    only does this with the first occurrence by default so it is not
    a good solution.
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-common/src/main/typescript/public-api.ts
+++ packages/cactus-common/src/main/typescript/public-api.ts
@@ -2,3 +2,4 @@ export { LoggerProvider } from "./logging/logger-provider";
 export { Logger, ILoggerOptions } from "./logging/logger";
 export { LogLevelDesc } from "loglevel";
 export { Objects } from "./objects";
+export { Strings } from "./strings";
