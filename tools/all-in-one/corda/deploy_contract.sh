8c4a8a123cdb8b99015792c181c3db4e2f8fa5c0
Jacob Weate <jacob.weate@accenture.com>
2020-08-19 16:58:08 -0400

    image with ssh and deploy-contract

--- tools/all-in-one/corda/deploy_contract.sh
+++ tools/all-in-one/corda/deploy_contract.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
-if [ "$#" -ne 1 ]; then
-    echo "Illegal number of parameters"
-    exit 2
-fi
+# if [ "$#" -ne 1 ]; then
+#     echo "Illegal number of parameters"
+#     exit 2
+# fi
 
-cordapp=$1
+cp /root/smart-contracts/upload.zip /opt/corda/builder/upload.zip
+cd /opt/corda/builder
+unzip upload.zip
 
-echo "building cordapp: $1"
-cp smart-contracts
+echo "Building Cordapps"
+./gradlew build -x test
+./gradlew deployNodes
+
+sleep 20
+echo "Kill all running corda nodes"
+kill $(ps aux | grep corda | awk '{print $1}')
+
+echo "Deploying Cordapps"
+cp /opt/corda/builder/build/nodes/PartyA/cordapps/*.jar /opt/corda/partyA/cordapps/
+cp /opt/corda/builder/build/nodes/PartyA/cordapps/*.jar /opt/corda/partyB/cordapps/
+java -jar /opt/corda/partyA/corda.jar --base-directory=/opt/corda/partyA & java -jar /opt/corda/partyB/corda.jar --base-directory=/opt/corda/partyB && fg
--- tools/all-in-one/corda/init_node.sh
+++ tools/all-in-one/corda/init_node.sh
@@ -4,4 +4,5 @@ echo "Starting ssh server..."
 nohup /usr/sbin/sshd -D &
 
 echo "Starting corda node.."
+# tail -f /dev/null
 java -jar /opt/corda/partyA/corda.jar --base-directory=/opt/corda/partyA & java -jar /opt/corda/partyB/corda.jar --base-directory=/opt/corda/partyB && fg
