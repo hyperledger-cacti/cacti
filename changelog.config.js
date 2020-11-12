34f3e2366d795d75fd7dc723add750201ced7f3a
Peter Somogyvari <peter.somogyvari@accenture.com>
2020-11-12 12:38:29 -0800

    build: 🏗 fix git-cz mismatch of possible commit message types
    Commitlint and git-cz (commitizen) does not always agree on what should
    be the list of types for commit messages and this was causing scenarios
    where you could author a commit message with npm run commit that were
    accepted at the time but then rejected by the commit hook of commitlint
    (husky)
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

---
+++ changelog.config.js
@@ -0,0 +1,92 @@
+module.exports = {
+  disableEmoji: true,
+  list: [
+    "build",
+    "chore",
+    "ci",
+    "docs",
+    "feat",
+    "fix",
+    "perf",
+    "refactor",
+    "revert",
+    "style",
+    "test",
+  ],
+  maxMessageLength: 64,
+  minMessageLength: 3,
+  maxHeaderWidth: 72,
+  questions: [
+    "type",
+    "scope",
+    "subject",
+    "body",
+    "breaking",
+    "issues",
+    "lerna",
+  ],
+  scopes: [],
+  types: {
+    build: {
+      description:
+        "Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)",
+      emoji: "🏗",
+      value: "build",
+    },
+    chore: {
+      description: "Build process or auxiliary tool changes",
+      emoji: "🤖",
+      value: "chore",
+    },
+    ci: {
+      description: "CI related changes",
+      emoji: "🎡",
+      value: "ci",
+    },
+    docs: {
+      description: "Documentation only changes",
+      emoji: "✏️",
+      value: "docs",
+    },
+    feat: {
+      description: "A new feature",
+      emoji: "🎸",
+      value: "feat",
+    },
+    fix: {
+      description: "A bug fix",
+      emoji: "🐛",
+      value: "fix",
+    },
+    perf: {
+      description: "A code change that improves performance",
+      emoji: "⚡️",
+      value: "perf",
+    },
+    refactor: {
+      description: "A code change that neither fixes a bug or adds a feature",
+      emoji: "💡",
+      value: "refactor",
+    },
+    release: {
+      description: "Create a release commit",
+      emoji: "🏹",
+      value: "release",
+    },
+    revert: {
+      description: "Reverts a previous commit",
+      emoji: "⏪",
+      value: "revert",
+    },
+    style: {
+      description: "Markup, white-space, formatting, missing semi-colons...",
+      emoji: "💄",
+      value: "style",
+    },
+    test: {
+      description: "Adding missing tests",
+      emoji: "💍",
+      value: "test",
+    },
+  },
+};
