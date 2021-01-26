2f6acc3e7fcaaf0625e8add8a10385f3dabec77d
Giron Amezcua, Jordi <jordi.giron.amezcua@accenture.com>
2021-01-26 09:09:34 +0100

    [WIP] Client first approach: Load Contracts & Call
    Signed-off-by: Jordi Giron <jordi.giron.amezcua@accenture.com>

--- packages/cactus-plugin-htlc-eth-besu/contracts/truffle-config.js
+++ packages/cactus-plugin-htlc-eth-besu/contracts/truffle-config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    }
+  },
   compilers: {
     solc: {
       version: "0.7.3",
