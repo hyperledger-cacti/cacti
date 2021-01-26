759472bd7af3a16bb0a53949041b5d3d8538ec59
Giron Amezcua, Jordi <jordi.giron.amezcua@accenture.com>
2021-01-26 16:15:04 +0100

    [WIP] Client second approach: SendTx & Refactor

--- packages/cactus-plugin-htlc-eth-besu/src/main/typescript/environment.ts
+++ packages/cactus-plugin-htlc-eth-besu/src/main/typescript/environment.ts
@@ -4,8 +4,4 @@ export const environment = {
     ACCOUNT_ADDRESS: "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1",
     ACCOUNT_PRIVATEKEY: "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d",
     CONTRACT_PATH: "./contracts/build/contracts/",
-    GET_GAS_ENDPOINT: "https://ethgasstation.info/json/ethgasAPI.json",
-    TX_SPEED: 'fast', //"fast", "fastest", "safeLow", "average"
-    MAX_GASPRICE: 100000,
-    MIN_GASPRICE: 0
 };
