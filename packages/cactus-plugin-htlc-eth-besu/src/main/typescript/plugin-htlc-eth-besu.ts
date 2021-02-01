28eb91355c265ea67cd3c9189ee9c3bdef81ed66
Peter Somogyvari <peter.somogyvari@accenture.com>
2021-01-31 23:06:40 -0800

    test(htlc-eth-besu): add atomic swap test
    work in progress
    
    Test currently still fails because the endpoint
    class does not implement everything yet.
    The idea here is to demonstrate how the
    JSON artifacts produced by truffle compile
    can be imported by the test cases and then
    deployed onto test ledgers that we programmatically
    pull up for the test cases with the AIO containers.

--- packages/cactus-plugin-htlc-eth-besu/src/main/typescript/plugin-htlc-eth-besu.ts
+++ packages/cactus-plugin-htlc-eth-besu/src/main/typescript/plugin-htlc-eth-besu.ts
@@ -1,8 +1,8 @@
-import { GetSingleStatusEndpoint } from "./web-services/getSingleStatus-endpoint";
-import { Express } from "express";
-import { Optional } from "typescript-optional";
 import { Server } from "http";
 import { Server as SecureServer } from "https";
+
+import { Express } from "express";
+import { Optional } from "typescript-optional";
 
 import {
   IPluginWebService,
@@ -11,20 +11,49 @@ import {
   IWebServiceEndpoint,
   PluginAspect,
 } from "@hyperledger/cactus-core-api";
-import { LogLevelDesc } from "@hyperledger/cactus-common";
+
+import {
+  Checks,
+  Logger,
+  LoggerProvider,
+  LogLevelDesc,
+} from "@hyperledger/cactus-common";
+
+import Client from "./client";
+import { GetSingleStatusEndpoint } from "./web-services/getSingleStatus-endpoint";
+import { PluginRegistry } from "@hyperledger/cactus-core";
+import {
+  EthContractInvocationType,
+  PluginLedgerConnectorBesu,
+} from "@hyperledger/cactus-plugin-ledger-connector-besu";
+
+import HashTimeLockJson from "../../../contracts/build/contracts/HashTimeLock.json";
 
 export interface IPluginHtlcEthBesuOptions extends ICactusPluginOptions {
   logLevel?: LogLevelDesc;
+  pluginRegistry: PluginRegistry;
 }
 export class PluginHtlcEthBesu implements ICactusPlugin, IPluginWebService {
   public static readonly CLASS_NAME = "PluginHtlcEthBesu";
   private readonly instanceId: string;
+  private readonly log: Logger;
+  private readonly pluginRegistry: PluginRegistry;
 
   public get className(): string {
     return PluginHtlcEthBesu.CLASS_NAME;
   }
 
   constructor(public readonly opts: IPluginHtlcEthBesuOptions) {
+    const fnTag = `${this.className}#constructor()`;
+    Checks.truthy(opts, `${fnTag} opts`);
+    Checks.truthy(opts.instanceId, `${fnTag} opts.instanceId`);
+    Checks.truthy(opts.pluginRegistry, `${fnTag} opts.pluginRegistry`);
+    Checks.nonBlankString(opts.instanceId, `${fnTag} opts.instanceId`);
+
+    this.pluginRegistry = opts.pluginRegistry;
+
+    const level = opts.logLevel || "INFO";
+    this.log = LoggerProvider.getOrCreate({ level, label: this.className });
     this.instanceId = opts.instanceId;
   }
   /**
@@ -51,7 +80,7 @@ export class PluginHtlcEthBesu implements ICactusPlugin, IPluginWebService {
 
   //TODO: Change to SWAP
   public getAspect(): PluginAspect {
-    return PluginAspect.CONSORTIUM;
+    return PluginAspect.ATOMIC_SWAP;
   }
 
   public async installWebServices(
@@ -68,5 +97,37 @@ export class PluginHtlcEthBesu implements ICactusPlugin, IPluginWebService {
     //TODO: add other endpoints.
 
     return endpoints;
+  }
+
+  public async atomicSwapV1(req: any): Promise<any> {
+    const fnTag = `${this.className}#atomicSwapV1()`;
+
+    const { connectorInstanceId: instanceId } = req;
+
+    const plugins = this.pluginRegistry.getPlugins();
+    const plugin = plugins.find((p) => p.getInstanceId() === instanceId);
+
+    Checks.truthy(
+      plugin instanceof PluginLedgerConnectorBesu,
+      `${fnTag}:connector`,
+    );
+
+    const connector = plugin as PluginLedgerConnectorBesu;
+
+    // could work something like this but I'm just throwing this here without
+    // testing for now...
+    await connector.invokeContract({
+      contractAbi: HashTimeLockJson.abi,
+      contractAddress: req.contractAddress,
+      invocationType: EthContractInvocationType.SEND,
+      methodName: "newContract",
+      params: ["... not sure"],
+      web3SigningCredential: req.web3SigningCredential,
+      gas: req.gas,
+      gasPrice: req.gasPrice,
+      timeoutMs: req.timeoutMs,
+    });
+
+    return;
   }
 }
