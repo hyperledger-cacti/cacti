691f7b62b3f4259a8bd2b9ac4a73708f2133219c
Jordi Giron <jordi.giron.amezcua@accenture.com>
2021-02-02 17:59:28 +0100

    feat(htlc-eth): wip improve test and loadcontracts

--- packages/cactus-plugin-htlc-eth-besu/src/main/typescript/environment.ts
+++ packages/cactus-plugin-htlc-eth-besu/src/main/typescript/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   NODE: "http://localhost:8545",
-  CONTRACT_PATH: "./contracts/build/contracts/",
+  ACCOUNT_ADDRESS: "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1",
+  CONTRACT_PATH: "../../contracts/build/contracts/",
 };
+//FIXME: Contract relative path doesn't work
--- packages/cactus-plugin-htlc-eth-besu/src/main/typescript/web-services/getSingleStatus-endpoint.ts
+++ packages/cactus-plugin-htlc-eth-besu/src/main/typescript/web-services/getSingleStatus-endpoint.ts
@@ -11,6 +11,7 @@ import {
   IWebServiceEndpoint,
 } from "@hyperledger/cactus-core-api";
 import { registerWebServiceEndpoint } from "@hyperledger/cactus-core";
+import { environment } from "../environment";
 
 export interface IGetSingleStatusEndpointOptions {
   logLevel?: LogLevelDesc;
@@ -18,24 +19,29 @@ export interface IGetSingleStatusEndpointOptions {
 export class GetSingleStatusEndpoint implements IWebServiceEndpoint {
   public static readonly CLASS_NAME = "GetSingleStatusEndpoint";
   private readonly log: Logger;
-  public get className() {
+
+  public get className(): string {
     return GetSingleStatusEndpoint.CLASS_NAME;
   }
+
   private client: Client;
   constructor(public readonly options: IGetSingleStatusEndpointOptions) {
+    this.client = new Client();
     const fnTag = `${this.className}#constructor()`;
     Checks.truthy(options, `${fnTag} arg options`);
     const level = this.options.logLevel || "INFO";
     const label = this.className;
     this.log = LoggerProvider.getOrCreate({ level, label });
-    this.client = new Client();
   }
+
   public getVerbLowerCase(): string {
     return "get";
   }
+
   public getPath(): string {
     return "/api/v1/plugins/@hyperledger/cactus-plugin-htlc-eth-besu/getSingleStatus/:id";
   }
+
   public registerExpress(expressApp: Express): IWebServiceEndpoint {
     registerWebServiceEndpoint(expressApp, this);
     return this;
@@ -48,12 +54,11 @@ export class GetSingleStatusEndpoint implements IWebServiceEndpoint {
   public async handleRequest(req: Request, res: Response): Promise<void> {
     const fnTag = "GetSingleStatusEndpoint#handleRequest()";
     this.log.debug(`GET ${this.getPath()}`);
-
+    await this.client.loadContracts(environment.CONTRACT_PATH!);
     const id = req.params.id;
 
-    this.log.info("Getting ID");
-    this.log.info("ID: " + id);
-
+    this.log.debug(`${fnTag} Id: ${id}`);
+    //TODO: Modify endpoint for set account address
     try {
       const result = await this.client.sendCall(
         "getSingleStatus",
@@ -61,7 +66,7 @@ export class GetSingleStatusEndpoint implements IWebServiceEndpoint {
         "HashTimeLock",
         "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1",
       );
-      this.log.info("RESULT: " + result);
+      this.log.debug(`${fnTag} Result: ${result}`);
       res.send(result);
     } catch (ex) {
       this.log.error(`${fnTag} failed to serve request`, ex);
