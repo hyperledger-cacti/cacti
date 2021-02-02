a7a1f31628e3a3d0ab5fb471373a7e9062d4a84f
AzaharaC <a.castano.benito@accenture.com>
2021-02-02 12:35:51 +0100

    feat(htlc-eth-besu): added endpoints files in web-serices
    Signed-off-by: AzaharaC <a.castano.benito@accenture.com>

--- packages/cactus-plugin-htlc-eth-besu/src/main/typescript/web-services/withdraw-endpoint.ts
+++ packages/cactus-plugin-htlc-eth-besu/src/main/typescript/web-services/withdraw-endpoint.ts
@@ -12,17 +12,17 @@ import {
 } from "@hyperledger/cactus-core-api";
 import { registerWebServiceEndpoint } from "@hyperledger/cactus-core";
 
-export interface IGetSingleStatusEndpointOptions {
+export interface IWithdrawEndpointOptions {
   logLevel?: LogLevelDesc;
 }
-export class GetSingleStatusEndpoint implements IWebServiceEndpoint {
-  public static readonly CLASS_NAME = "GetSingleStatusEndpoint";
+export class WithdrawEndpoint implements IWebServiceEndpoint {
+  public static readonly CLASS_NAME = "WithdrawEndpoint";
   private readonly log: Logger;
   public get className() {
-    return GetSingleStatusEndpoint.CLASS_NAME;
+    return WithdrawEndpoint.CLASS_NAME;
   }
   private client: Client;
-  constructor(public readonly options: IGetSingleStatusEndpointOptions) {
+  constructor(public readonly options: IWithdrawEndpointOptions) {
     const fnTag = `${this.className}#constructor()`;
     Checks.truthy(options, `${fnTag} arg options`);
     const level = this.options.logLevel || "INFO";
@@ -31,10 +31,10 @@ export class GetSingleStatusEndpoint implements IWebServiceEndpoint {
     this.client = new Client();
   }
   public getVerbLowerCase(): string {
-    return "get";
+    return "post";
   }
   public getPath(): string {
-    return "/api/v1/plugins/@hyperledger/cactus-plugin-htlc-eth-besu/getSingleStatus/:id";
+    return "/api/v1/plugins/@hyperledger/cactus-plugin-htlc-eth-besu/Withdraw/:id/:secret";
   }
   public registerExpress(expressApp: Express): IWebServiceEndpoint {
     registerWebServiceEndpoint(expressApp, this);
@@ -46,15 +46,20 @@ export class GetSingleStatusEndpoint implements IWebServiceEndpoint {
   }
 
   public async handleRequest(req: Request, res: Response): Promise<void> {
-    const fnTag = "GetSingleStatusEndpoint#handleRequest()";
-    this.log.debug(`GET ${this.getPath()}`);
+    const fnTag = "WithdrawEndpoint#handleRequest()";
+    this.log.debug(`POST ${this.getPath()}`);
     const id = req.params["id"];
+    const secret = req.params["secret"];
     try {
-      const result = this.client.sendCall(
-        "getSingleStatus",
-        [id],
+      //TODO: check that it works and remove from here.
+      const result = this.client.sendTx(
+        "Withdraw",
+        [id, secret],
         "HashTimeLock",
         "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1",
+        "",
+        "",
+        "",
       );
       res.send(result);
     } catch (ex) {
