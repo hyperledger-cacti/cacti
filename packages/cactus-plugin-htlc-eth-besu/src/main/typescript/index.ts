b88fedfc0e4707f3efcfca9074d410f9d38df8fd
Jordi Giron <jordi.giron.amezcua@accenture.com>
2021-02-02 12:44:14 +0100

    feat(htlc-eth): first approach testing endpoints

--- packages/cactus-plugin-htlc-eth-besu/src/main/typescript/index.ts
+++ packages/cactus-plugin-htlc-eth-besu/src/main/typescript/index.ts
@@ -1,1 +1,28 @@
 export * from "./public-api";
+/*
+import client from "./client";
+import Web3 from "web3";
+
+const eth_client = new client();
+console.log(__dirname);
+setTimeout(async () => {
+  const params = [
+    "0x05",
+    "2147483648",
+    "0x3c335ba7f06a8b01d0596589f73c19069e21c81e5013b91f408165d1bf623d32",
+    "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0",
+    "BTC",
+    "1AcVYm7M3kkJQH28FXAvyBFQzFRL6xPKu8",
+  ];
+  const transactionReceipt = await eth_client.sendTx(
+    "newContract",
+    params,
+    "HashTimeLock",
+    "0xCfEB869F69431e42cdB54A4F4f105C19C080A601",
+    "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1",
+    "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d",
+    "115",
+  );
+  console.log("Transaction receipt: " + JSON.stringify(transactionReceipt));
+}, 1200);
+*/
