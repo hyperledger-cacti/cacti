4c812150a1af9f9989acf5f234acb3a24570f539
Enrique Alcázar <e.alcazar.garzas@accenture.com>
2021-02-08 05:24:23 -0800

    Added HTLC smart contracts for ETH
    Signed-off-by: Jordi Giron <jordi.giron.amezcua@accenture.com>

---
+++ packages/cactus-plugin-htlc-eth-besu/contracts/test/constants.js
@@ -0,0 +1,18 @@
+// Constants
+
+const constants = {
+  // Common
+  // Year 2038
+  MAXIMUM_UNIX_TIMESTAMP: "2147483648",
+  SECONDS_IN_ONE_MINUTE: 60,
+  ZERO: 0,
+
+  // Statuses
+  INVALID: "INVALID",
+  ACTIVE: "ACTIVE",
+  REFUNDED: "REFUNDED",
+  WITHDRAWN: "WITHDRAWN",
+  EXPIRED: "EXPIRED"
+};
+
+module.exports = constants;
