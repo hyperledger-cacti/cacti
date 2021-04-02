8dda0f61937c6e1a85afee0345af44b1bfa09c0a
Peter Somogyvari <peter.somogyvari@accenture.com>
2021-04-02 13:32:28 -0700

    feat(api-server): ability to install plugins at runtime #764
    The API server will now use npm as a dependency to install
    plugin packages before trying to import them (via require())
    
    This is helpful in being able to ship a container image that
    contains the API server and does not need additional chores
    to be done in order to be able to use plugins (such as
    starting a new node package with its own dependencies).
    
    The security implications here are of course that we are
    pulling code form the internet at runtime and if npm's
    verification method for the code fails or if the plugin
    package is malicious to begin with then that's a full breach
    but these were security implications of the plugin architecture
    to begin with so there is not much additional that we can
    probably do here apart from a few addditional guard rails
    to protect users from themselves by maybe always requiring
    explicit versions so that auto-upgrade is out of the question.
    
    Fixes #764
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-cmd-api-server/src/test/typescript/integration/plugin-import-with-npm-install.test.ts
+++ packages/cactus-cmd-api-server/src/test/typescript/integration/plugin-import-with-npm-install.test.ts
@@ -1,9 +1,14 @@
 import test, { Test } from "tape-promise/tape";
 import { v4 as uuidv4 } from "uuid";
+import { JWK } from "jose";
 
 import { LogLevelDesc } from "@hyperledger/cactus-common";
 
-import { PluginImportType } from "@hyperledger/cactus-core-api";
+import {
+  PluginImportType,
+  ConsortiumDatabase,
+  ICactusPlugin,
+} from "@hyperledger/cactus-core-api";
 
 import { ApiServer, ConfigService } from "../../../main/typescript/public-api";
 
@@ -13,7 +18,18 @@ import { DefaultApi as ApiServerApi } from "../../../main/typescript/public-api"
 
 const logLevel: LogLevelDesc = "TRACE";
 
-test("can import plugins at runtime (CLI)", async (t: Test) => {
+test("can instal plugins at runtime based on imports", async (t: Test) => {
+  // Adding a new plugin to update the prometheus metric K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS
+  const keyPair = await JWK.generate("EC", "secp256k1", { use: "sig" }, true);
+  const keyPairPem = keyPair.toPEM(true);
+  const db: ConsortiumDatabase = {
+    cactusNode: [],
+    consortium: [],
+    consortiumMember: [],
+    ledger: [],
+    pluginInstance: [],
+  };
+
   const configService = new ConfigService();
   const apiServerOptions = configService.newExampleConfig();
   apiServerOptions.configFile = "";
@@ -31,6 +47,15 @@ test("can import plugins at runtime (CLI)", async (t: Test) => {
         logLevel,
       },
     },
+    {
+      packageName: "@hyperledger/cactus-plugin-consortium-manual",
+      type: PluginImportType.LOCAL,
+      options: {
+        instanceId: uuidv4(),
+        keyPairPem: keyPairPem,
+        consortiumDatabase: db,
+      },
+    },
   ];
   const config = configService.newExampleConfigConvict(apiServerOptions);
 
@@ -39,7 +64,10 @@ test("can import plugins at runtime (CLI)", async (t: Test) => {
   });
 
   const startResponse = apiServer.start();
-  await t.doesNotReject(startResponse, "started API server dynamic imports OK");
+  await t.doesNotReject(
+    startResponse,
+    "failed to start API server with dynamic plugin imports configured for it...",
+  );
   t.ok(startResponse, "startResponse truthy OK");
 
   const addressInfoApi = (await startResponse).addressInfoApi;
@@ -64,15 +92,47 @@ test("can import plugins at runtime (CLI)", async (t: Test) => {
       K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS +
       '{type="' +
       K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS +
-      '"} 1';
+      '"} 2';
     t.ok(res);
     t.ok(res.data);
     t.equal(res.status, 200);
     t.true(
       res.data.includes(promMetricsOutput),
-      "Total 1 plugins imported as expected. RESULT OK",
+      "Total 2 plugins imported as expected. RESULT OK",
     );
   }
+
+  const pluginRegistry = await apiServer.getOrInitPluginRegistry();
+
+  // this is not a working plugin but we are just testing the monitoring so
+  // it's okay for this particular test case. Do not copy this to other test
+  // cases or if you do remove right after you copied it ;-)
+  pluginRegistry.plugins.push({} as ICactusPlugin);
+
+  {
+    const res = await apiClient.getPrometheusExporterMetricsV1();
+    const promMetricsOutput =
+      "# HELP " +
+      K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS +
+      " Total number of plugins imported\n" +
+      "# TYPE " +
+      K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS +
+      " gauge\n" +
+      K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS +
+      '{type="' +
+      K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS +
+      '"} 3';
+    t.ok(res);
+    t.ok(res.data);
+    t.equal(res.status, 200);
+    t.true(
+      res.data.includes(promMetricsOutput),
+      "Total 3 plugins imported as expected. RESULT OK",
+    );
+  }
+  // clean up after ourselves,
+  // e.g. remove the dummy plugin instance we just pushed
+  pluginRegistry.plugins.pop();
 
   test.onFinish(() => apiServer.shutdown());
 });
