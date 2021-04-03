77ac3993933133563d20920fc068b5bc1053963b
Peter Somogyvari <peter.somogyvari@accenture.com>
2021-04-02 21:44:00 -0700

    build(webpack): exclude npm from bundling
    This is necessary because otherwise it just won't build. Details can be seen
    here:
    
    https://github.com/npm/feedback/discussions/280
    
    https://github.com/hyperledger/cactus/pull/769/checks?check_run_id=2257401552#step:5:14642
    
    ERROR in ./node_modules/npm/node_modules/node-gyp/lib/Find-VisualStudio.cs 9:6
    Module parse failed: Unexpected token (9:6)
    You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders
    | // This script needs to be compatible with PowerShell v2 to run on Windows 2008R2 and Windows 7.
    |
    > using System;
    | using System.Text;
    | using System.Runtime.InteropServices;
     @ ./node_modules/npm/node_modules/node-gyp/lib/ sync ^\.\/.*$ ./Find-VisualStudio.cs
     @ ./node_modules/npm/node_modules/node-gyp/lib/node-gyp.js 41:13-36 190:36-53
     @ ./node_modules/npm/node_modules/node-gyp/bin/node-gyp.js 7:12-26
     @ ./node_modules/npm/node_modules/@npmcli/run-script/lib/make-spawn-args.js 5:28-71
     @ ./node_modules/npm/node_modules/@npmcli/run-script/lib/run-script-pkg.js 1:22-53
     @ ./node_modules/npm/node_modules/@npmcli/run-script/lib/run-script.js 2:21-51
     @ ./node_modules/npm/lib/publish.js 6:18-47
     @ ./node_modules/npm/lib/ sync ^\.\/.*\.js$ ./publish.js
     @ ./node_modules/npm/lib/npm.js 20:19-44
     @ ./src/main/typescript/api-server.ts 12:30-44
     @ ./src/main/typescript/public-api.ts 14:19-42
     @ ./src/main/typescript/index.ts 13:13-36
    
    Ideally we would want to have all the bundling work correctly so that nothing
    has to be marked as external for webpack (e.g. to skip the bundling of it).
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- webpack.dev.node.js
+++ webpack.dev.node.js
@@ -68,6 +68,7 @@ module.exports = {
   externals: {
     "swarm-js": "swarm-js",
     "node-ssh": "node-ssh",
+    npm: "npm",
     "fabric-client": "fabric-client",
     "fabric-ca-client": "fabric-ca-client",
   },
