ae86df3f89a0526db663113258c59b7af50defcf
sannish <sannish1@in.ibm.com>
2021-04-09 07:48:12 +0000

    deleting interop folder from shared/chaincodes as well before copying new
    Signed-off-by: sannish <sannish1@in.ibm.com>

--- tests/network-setups/fabric/dev/scripts/setupCCLocal.sh
+++ tests/network-setups/fabric/dev/scripts/setupCCLocal.sh
@@ -3,12 +3,22 @@ directory=$(dirname $0)
 CHAINCODE_PATH=$directory/../../../fabric/shared/chaincode
 INTEROP_CC_PATH=$PWD/../../../../core/network/fabric-interop-cc
 
+echo "Setting up Interop CC..."
+
 cd $CHAINCODE_PATH
 if [ -d "fabric-interop-cc" ]; then
     echo "Deleting INTEROP CC in the test folder to copy the latest from the main folder...."
     rm -rf fabric-interop-cc
 fi
+if [ -d "interop"]; then
+    echo "Deleting previously built interop cc folder"
+    rm -rf interop
+fi
 cp -r ${INTEROP_CC_PATH} .
+
 cd fabric-interop-cc
 WEAVER_ROOT=$PWD/../../../../../.. make protos-local
 cp -R contracts/interop ../interop
+
+echo "Done."
+
