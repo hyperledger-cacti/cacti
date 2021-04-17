hyperledger-cacti/cacti

commit d75b9af764241ab2e10914769412201fb040b1ed

fix(deps): elliptic upgrade to >6.5.3 for CVE-2020-28498
The updates here were automatically done by running the
`npx lerna-audit` shell command in the project root.

https://github.com/advisories/GHSA-r9p9-mrjm-926w

CVE-2020-28498
moderate severity
Vulnerable versions: < 6.5.4
Patched version: 6.5.4
The npm package elliptic before version 6.5.4 are vulnerable to Cryptographic Issues via the secp256k1 implementation in elliptic/ec/key.js. There is no check to confirm that the public key point passed into the derive function actually exists on the secp256k1 curve. This results in the potential for the private key used in this implementation to be revealed after a number of ECDH operations are performed.

Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

packages/cactus-plugin-ledger-connector-quorum/src/main/typescript/plugin-ledger-connector-quorum.ts
@@ -227,7 +227,7 @@ export class PluginLedgerConnectorQuorum
     }
 
     const { contractAddress } = req;
-    const aContract = new this.web3.eth.Contract(abi, contractAddress);
+    const aContract = new Contract(abi, contractAddress);
     const methodRef = aContract.methods[req.methodName];
 
     Checks.truthy(methodRef, `${fnTag} YourContract.${req.methodName}`);
@@ -320,7 +320,7 @@ export class PluginLedgerConnectorQuorum
         contractJSON.networks = network;
         keychainPlugin.set(contractName, contractJSON);
       }
-      const contract = new this.web3.eth.Contract(
+      const contract = new Contract(
         contractJSON.abi,
         contractJSON.networks[networkId].address,
       );
@@ -575,7 +575,7 @@ export class PluginLedgerConnectorQuorum
           req.contractName,
         )) as any;
         this.log.info(JSON.stringify(contractJSON));
-        const contract = new this.web3.eth.Contract(
+        const contract = new Contract(
           contractJSON.abi,
           receipt.transactionReceipt.contractAddress,
         );

commit d7f00975452f3343e92f4e2e6629ddc5b000124e

fix(connector-quorum): web3 Contract type usage
This was broken by an earlier commit:
d75b9af764241ab2e10914769412201fb040b1ed

Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

packages/cactus-plugin-ledger-connector-quorum/src/main/typescript/plugin-ledger-connector-quorum.ts
@@ -5,8 +5,11 @@ import { Express } from "express";
 import { promisify } from "util";
 import { Optional } from "typescript-optional";
 import Web3 from "web3";
-
-import { Contract, ContractSendMethod } from "web3-eth-contract";
+// The strange way of obtaining the contract class here is like this because
+// web3-eth internally sub-classes the Contract class at runtime
+// @see https://stackoverflow.com/a/63639280/698470
+const Contract = new Web3().eth.Contract;
+import { ContractSendMethod } from "web3-eth-contract";
 import { TransactionReceipt } from "web3-eth";
 
 import {
@@ -83,7 +86,8 @@ export class PluginLedgerConnectorQuorum
   private readonly web3: Web3;
   private httpServer: Server | SecureServer | null = null;
   private contracts: {
-    [name: string]: Contract;
+    // @see https://stackoverflow.com/a/63639280/698470
+    [name: string]: InstanceType<typeof Contract>;
   } = {};
 
   private endpoints: IWebServiceEndpoint[] | undefined;
@@ -227,7 +231,7 @@ export class PluginLedgerConnectorQuorum
     }
 
     const { contractAddress } = req;
-    const aContract = new Contract(abi, contractAddress);
+    const aContract = new this.web3.eth.Contract(abi, contractAddress);
     const methodRef = aContract.methods[req.methodName];
 
     Checks.truthy(methodRef, `${fnTag} YourContract.${req.methodName}`);
@@ -320,7 +324,7 @@ export class PluginLedgerConnectorQuorum
         contractJSON.networks = network;
         keychainPlugin.set(contractName, contractJSON);
       }
-      const contract = new Contract(
+      const contract = new this.web3.eth.Contract(
         contractJSON.abi,
         contractJSON.networks[networkId].address,
       );
@@ -575,7 +579,7 @@ export class PluginLedgerConnectorQuorum
           req.contractName,
         )) as any;
         this.log.info(JSON.stringify(contractJSON));
-        const contract = new Contract(
+        const contract = new this.web3.eth.Contract(
           contractJSON.abi,
           receipt.transactionReceipt.contractAddress,
         );