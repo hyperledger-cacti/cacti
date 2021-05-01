491ec8d39a5c7959383d3fbb2efcf1ecf7844416
Peter Somogyvari <peter.somogyvari@accenture.com>
2021-05-01 16:47:04 -0700

    build: webpack ts-loader transpileOnly set to true
    Needed this because otherwise the perfectly valid Typescript code would
    not compile when using TS loader in cases when multiple Cactus packages
    were depending on types from certain dependencies (example both core-api
    and the besu connector would depend on the socketio package and its
    "Server" class would be claimed as incompatible by the TS loader
    compilation process even when the source files for those types were byte
    to byte the same (but the path was different to them because they
    existed in different node_modules directories)
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- webpack.dev.web.js
+++ webpack.dev.web.js
@@ -22,7 +22,7 @@ module.exports = {
           {
             loader: "ts-loader",
             options: {
-              transpileOnly: false,
+              transpileOnly: true,
               configFile: "tsconfig.json",
             },
           },
