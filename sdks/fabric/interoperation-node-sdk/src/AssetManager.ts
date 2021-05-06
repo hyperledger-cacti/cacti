fa2190dcc780c6b6ae2bfaca1b17529301f860d4
VRamakrishna <vramakr2@in.ibm.com>
2021-05-06 21:52:22 +0000

    Added asset lock and claim functions in Fabric Interop Node SDK
    Also refactored code and added a new signing certificate.
    
    Signed-off-by: VRamakrishna <vramakr2@in.ibm.com>

--- sdks/fabric/interoperation-node-sdk/src/AssetManager.ts
+++ sdks/fabric/interoperation-node-sdk/src/AssetManager.ts
@@ -18,18 +18,94 @@ import { Contract } from "fabric-network";
 const logger = log4js.getLogger("InteroperableHelper");
 
 
+// Create an asset exchange agreement structure
+function createAssetExchangeAgreementSerialized(assetType, assetID, recipientECert, locker)
+{
+    const assetExchangeAgreement = new assetLocksPb.AssetExchangeAgreement();
+    assetExchangeAgreement.setType(assetType);
+    assetExchangeAgreement.setId(assetID);
+    assetExchangeAgreement.setRecipient(recipientECert);
+    assetExchangeAgreement.setLocker(locker);
+    return assetExchangeAgreement.serializeBinary().toString();
+}
+
+// Create a fungible asset exchange agreement structure
+function createFungibleAssetExchangeAgreementSerialized(assetType, numUnits, recipientECert, locker)
+{
+    const assetExchangeAgreement = new assetLocksPb.FungibleAssetExchangeAgreement();
+    assetExchangeAgreement.setType(assetType);
+    assetExchangeAgreement.setNumunits(numUnits);
+    assetExchangeAgreement.setRecipient(recipientECert);
+    assetExchangeAgreement.setLocker(locker);
+    return assetExchangeAgreement.serializeBinary().toString();
+}
+
+// Create an asset lock structure
+function createAssetLockInfoSerialized(hashValue, expiryTimeSecs)
+{
+    const lockInfoHTLC = new assetLocksPb.AssetLockHTLC();
+    lockInfoHTLC.setHash(hashValue);
+    lockInfoHTLC.setExpirytimesecs(expiryTimeSecs);
+    lockInfoHTLC.setTimespec(assetLocksPb.AssetLockHTLC.TimeSpec.EPOCH)
+    const lockInfoHTLCSerialized = lockInfoHTLC.serializeBinary();
+    const lockInfo = new assetLocksPb.AssetLock();
+    lockInfo.setLockmechanism(assetLocksPb.LockMechanism.HTLC);
+    lockInfo.setLockinfo(lockInfoHTLCSerialized);
+    return lockInfo.serializeBinary().toString();
+}
+
+// Create an asset claim structure
+function createAssetClaimInfoSerialized(hashPreimage)
+{
+    const claimInfoHTLC = new assetLocksPb.AssetClaimHTLC();
+    claimInfoHTLC.setHashpreimage(Buffer.from(hashPreimage).toString('base64'));
+    const claimInfoHTLCSerialized = claimInfoHTLC.serializeBinary();
+    const claimInfo = new assetLocksPb.AssetClaim();
+    claimInfo.setLockmechanism(assetLocksPb.LockMechanism.HTLC);
+    claimInfo.setClaiminfo(claimInfoHTLCSerialized);
+    return claimInfo.serializeBinary().toString();
+}
+
+// Create a SHA-256 hash over an ASCII string
+function createSHA256Hash(preimage)
+{
+    return crypto.createHash('sha256').update(preimage).digest('base64');
+}
+
+
 /**
- * First step of a Hashed Time Lock Contract
+ * First/second step of a Hashed Time Lock Contract
  * - Lock a unique asset instance using a hash
  **/
 const createHTLC = async (
     contract: Contract,
     assetType: string,
     assetID: string,
-    recipientID: string,
+    recipientECert: string,
     hashValue: string,
     expiryTimeSecs: number,
 ): Promise<{ preimage: any; result: any }> => {
+
+    if (!contract)
+    {
+        logger.error("Contract handle not supplied");
+        return { preimage: "", result: false };
+    }
+    if (!assetType)
+    {
+        logger.error("Asset type not supplied");
+        return { preimage: "", result: false };
+    }
+    if (!assetID)
+    {
+        logger.error("Asset ID not supplied");
+        return { preimage: "", result: false };
+    }
+    if (!recipientECert)
+    {
+        logger.error("Recipient ECert not supplied");
+        return { preimage: "", result: false };
+    }
 
     let defaultExpiryTimeSecs = 0, preimage = "";
     if (hashValue && hashValue.length > 0)
@@ -47,32 +123,17 @@ const createHTLC = async (
         }
 
         // Hash the preimage
-        hashValue = crypto.createHash('sha256').update(preimage).digest('base64');
+        hashValue = createSHA256Hash(preimage);
     }
     const currTimeSecs = Math.floor(Date.now()/1000);   // Convert epoch milliseconds to seconds
     if (expiryTimeSecs <= currTimeSecs)
     {
-        logger.warn("Supplied HTLC expiry time is invalid or in the past: %d", expiryTimeSecs)
-        expiryTimeSecs = currTimeSecs + defaultExpiryTimeSecs
+        logger.warn("Supplied HTLC expiry time is invalid or in the past: %d", expiryTimeSecs);
+        expiryTimeSecs = currTimeSecs + defaultExpiryTimeSecs;
     }
 
-    // Create an asset exchange agreement structure
-    const assetExchangeAgreement = new assetLocksPb.AssetExchangeAgreement();
-    assetExchangeAgreement.setType(assetType);
-    assetExchangeAgreement.setId(assetID);
-    assetExchangeAgreement.setRecipient(recipientID);
-    const assetExchangeAgreementStr = assetExchangeAgreement.serializeBinary().toString();
-
-    // Create an asset lock structure
-    const lockInfoHTLC = new assetLocksPb.AssetLockHTLC();
-    lockInfoHTLC.setHash(hashValue);
-    lockInfoHTLC.setExpirytimesecs(expiryTimeSecs);
-    lockInfoHTLC.setTimespec(assetLocksPb.AssetLockHTLC.TimeSpec.EPOCH)
-    const lockInfoHTLCSerialized = lockInfoHTLC.serializeBinary();
-    const lockInfo = new assetLocksPb.AssetLock();
-    lockInfo.setLockmechanism(assetLocksPb.LockMechanism.HTLC);
-    lockInfo.setLockinfo(lockInfoHTLCSerialized);
-    const lockInfoStr = lockInfo.serializeBinary().toString();
+    const assetExchangeAgreementStr = createAssetExchangeAgreementSerialized(assetType, assetID, recipientECert, "");
+    const lockInfoStr = createAssetLockInfoSerialized(hashValue, expiryTimeSecs);
 
     // Normal invoke function
     const [result, submitError] = await helpers.handlePromise(
@@ -84,13 +145,191 @@ const createHTLC = async (
     return { preimage: preimage, result: result };
 };
 
+/**
+ * First/second step of a Hashed Time Lock Contract
+ * - Lock a unique asset instance using a hash
+ **/
+const createFungibleHTLC = async (
+    contract: Contract,
+    assetType: string,
+    numUnits: number,
+    recipientECert: string,
+    hashValue: string,
+    expiryTimeSecs: number,
+): Promise<{ preimage: any; result: any }> => {
+
+    if (!contract)
+    {
+        logger.error("Contract handle not supplied");
+        return { preimage: "", result: false };
+    }
+    if (!assetType)
+    {
+        logger.error("Asset type not supplied");
+        return { preimage: "", result: false };
+    }
+    if (numUnits <= 0)
+    {
+        logger.error("Asset count must be a positive integer");
+        return { preimage: "", result: false };
+    }
+    if (!recipientECert)
+    {
+        logger.error("Recipient ECert not supplied");
+        return { preimage: "", result: false };
+    }
+
+    let defaultExpiryTimeSecs = 0, preimage = "";
+    if (hashValue && hashValue.length > 0)
+    {
+        defaultExpiryTimeSecs = 5 * 60     // 5 mins: this is the 't' used to time out the second contract of the HTLC pair
+    }
+    else
+    {
+        defaultExpiryTimeSecs = 10 * 60     // 10 mins: this is the '2t' used to time out the first contract of the HTLC pair
+
+        // Create a random preimage of 20 bytes
+        for (let i = 0 ; i < 20 ; i++)
+        {
+            preimage += String.fromCharCode(Math.floor(Math.random() * 256))
+        }
+
+        // Hash the preimage
+        hashValue = createSHA256Hash(preimage);
+    }
+    const currTimeSecs = Math.floor(Date.now()/1000);   // Convert epoch milliseconds to seconds
+    if (expiryTimeSecs <= currTimeSecs)
+    {
+        logger.warn("Supplied HTLC expiry time is invalid or in the past: %d", expiryTimeSecs);
+        expiryTimeSecs = currTimeSecs + defaultExpiryTimeSecs;
+    }
+
+    const assetExchangeAgreementStr = createFungibleAssetExchangeAgreementSerialized(assetType, numUnits, recipientECert, "");
+    const lockInfoStr = createAssetLockInfoSerialized(hashValue, expiryTimeSecs);
+
+    // Normal invoke function
+    const [result, submitError] = await helpers.handlePromise(
+        contract.submitTransaction("LockFungibleAsset", assetExchangeAgreementStr, lockInfoStr),
+    );
+    if (submitError) {
+        throw new Error(`LockFungibleAsset submitTransaction Error: ${submitError}`);
+    }
+    return { preimage: preimage, result: result };
+};
+
+/**
+ * Latter step of a Hashed Time Lock Contract
+ * - Claim a unique asset instance using a hash preimage
+ **/
+const claimAssetInHTLC = async (
+    contract: Contract,
+    assetType: string,
+    assetID: string,
+    lockerECert: string,
+    hashPreimage: string,
+): Promise<any> => {
+
+    if (!contract)
+    {
+        logger.error("Contract handle not supplied");
+        return false;
+    }
+    if (!assetType)
+    {
+        logger.error("Asset type not supplied");
+        return false;
+    }
+    if (!assetID)
+    {
+        logger.error("Asset ID not supplied");
+        return false;
+    }
+    if (!lockerECert)
+    {
+        logger.error("Locker ECert not supplied");
+        return false;
+    }
+    if (!hashPreimage)
+    {
+        logger.error("Hash Preimage not supplied");
+        return false;
+    }
+
+    const assetExchangeAgreementStr = createAssetExchangeAgreementSerialized(assetType, assetID, "", lockerECert);
+    const claimInfoStr = createAssetClaimInfoSerialized(hashPreimage);
+
+    // Normal invoke function
+    const [result, submitError] = await helpers.handlePromise(
+        contract.submitTransaction("ClaimAsset", assetExchangeAgreementStr, claimInfoStr),
+    );
+    if (submitError) {
+        throw new Error(`ClaimAsset submitTransaction Error: ${submitError}`);
+    }
+    return result;
+};
+
+/**
+ * Latter step of a Hashed Time Lock Contract
+ * - Claim a fungible asset instance using a hash preimage
+ **/
+const claimFungibleAssetInHTLC = async (
+    contract: Contract,
+    assetType: string,
+    numUnits: number,
+    lockerECert: string,
+    hashPreimage: string,
+): Promise<any> => {
+
+    if (!contract)
+    {
+        logger.error("Contract handle not supplied");
+        return false;
+    }
+    if (!assetType)
+    {
+        logger.error("Asset type not supplied");
+        return false;
+    }
+    if (numUnits <= 0)
+    {
+        logger.error("Asset count must be a positive integer");
+        return false;
+    }
+    if (!lockerECert)
+    {
+        logger.error("Locker ECert not supplied");
+        return false;
+    }
+    if (!hashPreimage)
+    {
+        logger.error("Hash Preimage not supplied");
+        return false;
+    }
+
+    const assetExchangeAgreementStr = createFungibleAssetExchangeAgreementSerialized(assetType, numUnits, "", lockerECert);
+    const claimInfoStr = createAssetClaimInfoSerialized(hashPreimage);
+
+    // Normal invoke function
+    const [result, submitError] = await helpers.handlePromise(
+        contract.submitTransaction("ClaimFungibleAsset", assetExchangeAgreementStr, claimInfoStr),
+    );
+    if (submitError) {
+        throw new Error(`ClaimFungibleAsset submitTransaction Error: ${submitError}`);
+    }
+    return result;
+};
+
 export {
+    createAssetExchangeAgreementSerialized,
+    createFungibleAssetExchangeAgreementSerialized,
+    createAssetLockInfoSerialized,
+    createAssetClaimInfoSerialized,
     createHTLC,
-    /*createFungibleHTLC,
-    isAssetLockedInHTLC,
-    isFungibleAssetLockedInHTLC,
+    createFungibleHTLC,
     claimAssetInHTLC,
     claimFungibleAssetInHTLC,
+    /*isAssetLockedInHTLC,
+    isFungibleAssetLockedInHTLC,
     reclaimAssetInHTLC,
     reclaimFungibleAssetInHTLC,*/
 };
--- sdks/fabric/interoperation-node-sdk/test/AssetManager.js
+++ sdks/fabric/interoperation-node-sdk/test/AssetManager.js
@@ -20,6 +20,7 @@ const { Wallets } = require("fabric-network");
 const { ContractImpl } = require("fabric-network/lib/contract");
 const { NetworkImpl } = require("fabric-network/lib/network");
 const assetManager = require("../src/AssetManager");
+import assetLocksPb from "../protos-js/common/asset_locks_pb";
 
 describe("AssetManager", () => {
     const mspId = "mspId";
@@ -28,7 +29,10 @@ describe("AssetManager", () => {
 
     const assetType = "bond";
     const assetID = "A001";
-    const recipientID = "Bob";
+    const fungibleAssetType = "cbdc";
+    const numUnits = 1000;
+    const recipientECert = fs.readFileSync(`${__dirname}/data/anotherSignCert.pem`).toString();
+    let lockerECert;
 
     let wallet;
     let amc;
@@ -38,6 +42,7 @@ describe("AssetManager", () => {
         const signCertFile = `${__dirname}/data/signCert.pem`;
         const privateKeyStr = fs.readFileSync(privKeyFile).toString();
         const signCert = fs.readFileSync(signCertFile).toString();
+        lockerECert = signCert;
         wallet = await Wallets.newInMemoryWallet();
         const userIdentity = {
             credentials: { certificate: signCert, privateKey: privateKeyStr },
@@ -47,6 +52,7 @@ describe("AssetManager", () => {
         await wallet.put(userName, userIdentity);
         return userIdentity;
     }
+
     beforeEach(async () => {
         await initializeWallet();
         const network = sinon.createStubInstance(NetworkImpl);
@@ -58,19 +64,198 @@ describe("AssetManager", () => {
     });
 
     describe("create HTLC for unique asset", () => {
+        let amcStub;
+
         beforeEach(() => {
-            const amcStub = sinon.stub(amc, "submitTransaction").resolves(true);
-            //const amcStub = sinon.stub(amc, "submitTransaction").resolves(false);
-            //amcStub.withArgs("LockAsset", assetAgreementStr, lockInfoStr).resolves(true);
+            amcStub = sinon.stub(amc, "submitTransaction").resolves(false);
+        });
+
+        it("asset lock fails with invalid parameters", async () => {
+            let assetLockInvocation = await assetManager.createHTLC(null, assetType, assetID, recipientECert, "", 0);
+            expect(assetLockInvocation).to.be.an('object').that.has.all.keys('preimage', 'result');
+            expect(assetLockInvocation.preimage).to.be.a("string");
+            expect(assetLockInvocation.preimage.length).to.equal(0);
+            expect(assetLockInvocation.result).to.be.a('boolean');
+            expect(assetLockInvocation.result).to.equal(false);
+            assetLockInvocation = await assetManager.createHTLC(amc, "", assetID, recipientECert, "", 0);
+            expect(assetLockInvocation).to.be.an('object').that.has.all.keys('preimage', 'result');
+            expect(assetLockInvocation.preimage).to.be.a("string");
+            expect(assetLockInvocation.preimage.length).to.equal(0);
+            expect(assetLockInvocation.result).to.be.a('boolean');
+            expect(assetLockInvocation.result).to.equal(false);
+            assetLockInvocation = await assetManager.createHTLC(amc, assetType, "", recipientECert, "", 0);
+            expect(assetLockInvocation).to.be.an('object').that.has.all.keys('preimage', 'result');
+            expect(assetLockInvocation.preimage).to.be.a("string");
+            expect(assetLockInvocation.preimage.length).to.equal(0);
+            expect(assetLockInvocation.result).to.be.a('boolean');
+            expect(assetLockInvocation.result).to.equal(false);
+            assetLockInvocation = await assetManager.createHTLC(amc, assetType, assetID, "", "", 0);
+            expect(assetLockInvocation).to.be.an('object').that.has.all.keys('preimage', 'result');
+            expect(assetLockInvocation.preimage).to.be.a("string");
+            expect(assetLockInvocation.preimage.length).to.equal(0);
+            expect(assetLockInvocation.result).to.be.a('boolean');
+            expect(assetLockInvocation.result).to.equal(false);
         });
 
         it("submit asset lock invocation", async () => {
-            const assetLockInvocation = await assetManager.createHTLC(amc, assetType, assetID, recipientID, "", 0);
+            let assetAgreementStr = assetManager.createAssetExchangeAgreementSerialized(assetType, assetID, recipientECert, "");
+            const hashValue = "abcdef123456";
+            let expiryTimeSecs = Math.floor(Date.now()/1000) + 300;   // Convert epoch milliseconds to seconds and add 5 minutes
+            let lockInfoStr = assetManager.createAssetLockInfoSerialized(hashValue, expiryTimeSecs);
+            amcStub.withArgs("LockAsset", assetAgreementStr, lockInfoStr).resolves(true);
+            let assetLockInvocation = await assetManager.createHTLC(amc, assetType, assetID, recipientECert, hashValue, expiryTimeSecs);
+            expect(assetLockInvocation).to.be.an('object').that.has.all.keys('preimage', 'result');
+            expect(assetLockInvocation.preimage).to.be.a("string");
+            expect(assetLockInvocation.preimage.length).to.equal(0);
+            expect(assetLockInvocation.result).to.be.a('boolean');
+            expect(assetLockInvocation.result).to.equal(true);
+            amcStub.withArgs("LockAsset", assetAgreementStr, sinon.match.any).resolves(true);
+            assetLockInvocation = await assetManager.createHTLC(amc, assetType, assetID, recipientECert, hashValue, 0);
+            expect(assetLockInvocation).to.be.an('object').that.has.all.keys('preimage', 'result');
+            expect(assetLockInvocation.preimage).to.be.a("string");
+            expect(assetLockInvocation.preimage.length).to.equal(0);
+            expect(assetLockInvocation.result).to.be.a('boolean');
+            expect(assetLockInvocation.result).to.equal(true);
+            assetLockInvocation = await assetManager.createHTLC(amc, assetType, assetID, recipientECert, "", 0);
             expect(assetLockInvocation).to.be.an('object').that.has.all.keys('preimage', 'result');
             expect(assetLockInvocation.preimage).to.be.a("string");
             expect(assetLockInvocation.preimage.length).to.equal(20);
             expect(assetLockInvocation.result).to.be.a('boolean');
             expect(assetLockInvocation.result).to.equal(true);
+        });
+    });
+
+    describe("create HTLC for fungible asset", () => {
+        let amcStub;
+
+        beforeEach(() => {
+            amcStub = sinon.stub(amc, "submitTransaction").resolves(false);
+        });
+
+        it("asset lock fails with invalid parameters", async () => {
+            let assetLockInvocation = await assetManager.createFungibleHTLC(null, fungibleAssetType, numUnits, recipientECert, "", 0);
+            expect(assetLockInvocation).to.be.an('object').that.has.all.keys('preimage', 'result');
+            expect(assetLockInvocation.preimage).to.be.a("string");
+            expect(assetLockInvocation.preimage.length).to.equal(0);
+            expect(assetLockInvocation.result).to.be.a('boolean');
+            expect(assetLockInvocation.result).to.equal(false);
+            assetLockInvocation = await assetManager.createFungibleHTLC(amc, "", numUnits, recipientECert, "", 0);
+            expect(assetLockInvocation).to.be.an('object').that.has.all.keys('preimage', 'result');
+            expect(assetLockInvocation.preimage).to.be.a("string");
+            expect(assetLockInvocation.preimage.length).to.equal(0);
+            expect(assetLockInvocation.result).to.be.a('boolean');
+            expect(assetLockInvocation.result).to.equal(false);
+            assetLockInvocation = await assetManager.createFungibleHTLC(amc, fungibleAssetType, -1, recipientECert, "", 0);
+            expect(assetLockInvocation).to.be.an('object').that.has.all.keys('preimage', 'result');
+            expect(assetLockInvocation.preimage).to.be.a("string");
+            expect(assetLockInvocation.preimage.length).to.equal(0);
+            expect(assetLockInvocation.result).to.be.a('boolean');
+            expect(assetLockInvocation.result).to.equal(false);
+            assetLockInvocation = await assetManager.createFungibleHTLC(amc, fungibleAssetType, numUnits, "", "", 0);
+            expect(assetLockInvocation).to.be.an('object').that.has.all.keys('preimage', 'result');
+            expect(assetLockInvocation.preimage).to.be.a("string");
+            expect(assetLockInvocation.preimage.length).to.equal(0);
+            expect(assetLockInvocation.result).to.be.a('boolean');
+            expect(assetLockInvocation.result).to.equal(false);
+        });
+
+        it("submit asset lock invocation", async () => {
+            let assetAgreementStr = assetManager.createFungibleAssetExchangeAgreementSerialized(fungibleAssetType, numUnits, recipientECert, "");
+            const hashValue = "abcdef123456";
+            let expiryTimeSecs = Math.floor(Date.now()/1000) + 300;   // Convert epoch milliseconds to seconds and add 5 minutes
+            let lockInfoStr = assetManager.createAssetLockInfoSerialized(hashValue, expiryTimeSecs);
+            amcStub.withArgs("LockFungibleAsset", assetAgreementStr, lockInfoStr).resolves(true);
+            let assetLockInvocation = await assetManager.createFungibleHTLC(amc, fungibleAssetType, numUnits, recipientECert, hashValue, expiryTimeSecs);
+            expect(assetLockInvocation).to.be.an('object').that.has.all.keys('preimage', 'result');
+            expect(assetLockInvocation.preimage).to.be.a("string");
+            expect(assetLockInvocation.preimage.length).to.equal(0);
+            expect(assetLockInvocation.result).to.be.a('boolean');
+            expect(assetLockInvocation.result).to.equal(true);
+            amcStub.withArgs("LockFungibleAsset", assetAgreementStr, sinon.match.any).resolves(true);
+            assetLockInvocation = await assetManager.createFungibleHTLC(amc, fungibleAssetType, numUnits, recipientECert, hashValue, 0);
+            expect(assetLockInvocation).to.be.an('object').that.has.all.keys('preimage', 'result');
+            expect(assetLockInvocation.preimage).to.be.a("string");
+            expect(assetLockInvocation.preimage.length).to.equal(0);
+            expect(assetLockInvocation.result).to.be.a('boolean');
+            expect(assetLockInvocation.result).to.equal(true);
+            assetLockInvocation = await assetManager.createFungibleHTLC(amc, fungibleAssetType, numUnits, recipientECert, "", 0);
+            expect(assetLockInvocation).to.be.an('object').that.has.all.keys('preimage', 'result');
+            expect(assetLockInvocation.preimage).to.be.a("string");
+            expect(assetLockInvocation.preimage.length).to.equal(20);
+            expect(assetLockInvocation.result).to.be.a('boolean');
+            expect(assetLockInvocation.result).to.equal(true);
+        });
+    });
+
+    describe("claim unique asset locked in HTLC", () => {
+        let amcStub;
+        const hashPreimage = "xyz+123-*ty%";
+
+        beforeEach(() => {
+            amcStub = sinon.stub(amc, "submitTransaction").resolves(false);
+        });
+
+        it("asset claim fails with invalid parameters", async () => {
+            let assetClaimInvocation = await assetManager.claimAssetInHTLC(null, assetType, assetID, lockerECert, hashPreimage);
+            expect(assetClaimInvocation).to.be.a('boolean');
+            expect(assetClaimInvocation).to.equal(false);
+            assetClaimInvocation = await assetManager.claimAssetInHTLC(amc, "", assetID, lockerECert, hashPreimage);
+            expect(assetClaimInvocation).to.be.a('boolean');
+            expect(assetClaimInvocation).to.equal(false);
+            assetClaimInvocation = await assetManager.claimAssetInHTLC(amc, assetType, "", lockerECert, hashPreimage);
+            expect(assetClaimInvocation).to.be.a('boolean');
+            expect(assetClaimInvocation).to.equal(false);
+            assetClaimInvocation = await assetManager.claimAssetInHTLC(amc, assetType, assetID, "", hashPreimage);
+            expect(assetClaimInvocation).to.be.a('boolean');
+            expect(assetClaimInvocation).to.equal(false);
+            assetClaimInvocation = await assetManager.claimAssetInHTLC(amc, assetType, assetID, lockerECert, "");
+            expect(assetClaimInvocation).to.be.a('boolean');
+            expect(assetClaimInvocation).to.equal(false);
+        });
+
+        it("submit asset claim invocation", async () => {
+            let assetAgreementStr = assetManager.createAssetExchangeAgreementSerialized(assetType, assetID, "", lockerECert);
+            let claimInfoStr = assetManager.createAssetClaimInfoSerialized(hashPreimage);
+            amcStub.withArgs("ClaimAsset", assetAgreementStr, claimInfoStr).resolves(true);
+            let assetClaimInvocation = await assetManager.claimAssetInHTLC(amc, assetType, assetID, lockerECert, hashPreimage);
+            expect(assetClaimInvocation).to.be.a('boolean');
+            expect(assetClaimInvocation).to.equal(true);
+        });
+    });
+
+    describe("claim fungible asset locked in HTLC", () => {
+        let amcStub;
+        const hashPreimage = "xyz+123-*ty%";
+
+        beforeEach(() => {
+            amcStub = sinon.stub(amc, "submitTransaction").resolves(false);
+        });
+
+        it("asset claim fails with invalid parameters", async () => {
+            let assetClaimInvocation = await assetManager.claimFungibleAssetInHTLC(null, fungibleAssetType, numUnits, lockerECert, hashPreimage);
+            expect(assetClaimInvocation).to.be.a('boolean');
+            expect(assetClaimInvocation).to.equal(false);
+            assetClaimInvocation = await assetManager.claimFungibleAssetInHTLC(amc, "", numUnits, lockerECert, hashPreimage);
+            expect(assetClaimInvocation).to.be.a('boolean');
+            expect(assetClaimInvocation).to.equal(false);
+            assetClaimInvocation = await assetManager.claimFungibleAssetInHTLC(amc, fungibleAssetType, -1, lockerECert, hashPreimage);
+            expect(assetClaimInvocation).to.be.a('boolean');
+            expect(assetClaimInvocation).to.equal(false);
+            assetClaimInvocation = await assetManager.claimFungibleAssetInHTLC(amc, fungibleAssetType, numUnits, "", hashPreimage);
+            expect(assetClaimInvocation).to.be.a('boolean');
+            expect(assetClaimInvocation).to.equal(false);
+            assetClaimInvocation = await assetManager.claimFungibleAssetInHTLC(amc, fungibleAssetType, numUnits, lockerECert, "");
+            expect(assetClaimInvocation).to.be.a('boolean');
+            expect(assetClaimInvocation).to.equal(false);
+        });
+
+        it("submit asset claim invocation", async () => {
+            let assetAgreementStr = assetManager.createFungibleAssetExchangeAgreementSerialized(fungibleAssetType, numUnits, "", lockerECert);
+            let claimInfoStr = assetManager.createAssetClaimInfoSerialized(hashPreimage);
+            amcStub.withArgs("ClaimFungibleAsset", assetAgreementStr, claimInfoStr).resolves(true);
+            let assetClaimInvocation = await assetManager.claimFungibleAssetInHTLC(amc, fungibleAssetType, numUnits, lockerECert, hashPreimage);
+            expect(assetClaimInvocation).to.be.a('boolean');
+            expect(assetClaimInvocation).to.equal(true);
         });
     });
 });
