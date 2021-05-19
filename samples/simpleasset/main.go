68fd54e1880e263e7e87928dcd916959f0b089ef
sandeep.nishad1 <sandeep.nishad1@ibm.com>
2021-05-19 13:25:18 +0530

    symbolic link to base class, updated main.go to call Configure
    Signed-off-by: sandeep.nishad1 <sandeep.nishad1@ibm.com>

--- samples/simpleasset/main.go
+++ samples/simpleasset/main.go
@@ -6,13 +6,23 @@ import (
 
   "github.com/hyperledger/fabric-chaincode-go/shim"
   "github.com/hyperledger/fabric-contract-api-go/contractapi"
-  am "github.com/hyperledger-labs/weaver/samples/simpleasset/asset-mgmt"
+  am "github.com/hyperledger-labs/weaver-dlt-interoperability/core/network/fabric-interop-cc/interfaces/asset-mgmt"
 )
 
 // SmartContract provides functions for managing an BondAsset and TokenAsset
 type SmartContract struct {
 	contractapi.Contract
-    am.AssetManagementContract
+  am.AssetManagementContract
+}
+
+func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface, ccType string, interopChaincodeId string) error {
+  s.Configure(interopChaincodeId)
+  if ccType == "Bond" {
+    return s.InitBondAssetLedger(ctx)
+  } else if ccType == "Token" {
+    return s.InitTokenAssetLedger(ctx)
+  }
+  return fmt.Errorf("only Bond and Token are accepted as ccType.")
 }
 
 func main() {
