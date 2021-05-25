89c455a5209ecb07ff65b64efb36131b27d04536
Konstantin Rybalko <konstantin.rybalko@gmail.com>
2021-05-25 17:10:58 +0300

    Script to cleanup electricity-trade app data after test

---
+++ examples/electricity-trade/cleanup.sh
@@ -0,0 +1,24 @@
+docker stop sawtooth-shell-default
+docker stop sawtooth-settings-tp-default
+docker stop sawtooth-xo-tp-python-default
+docker stop sawtooth-devmode-engine-rust-default
+docker stop sawtooth-rest-api-default
+docker stop sawtooth-intkey-tp-python-default
+docker stop sawtooth-validator-default
+docker stop geth1
+docker stop gethtestnet_init-chain-geth1_run_1
+docker stop gethtestnet_init-chain-geth1_run_2
+
+docker rm sawtooth-shell-default
+docker rm sawtooth-settings-tp-default
+docker rm sawtooth-xo-tp-python-default
+docker rm sawtooth-devmode-engine-rust-default
+docker rm sawtooth-rest-api-default
+docker rm sawtooth-intkey-tp-python-default
+docker rm sawtooth-validator-default
+docker rm geth1
+docker rm gethtestnet_init-chain-geth1_run_1
+docker rm gethtestnet_init-chain-geth1_run_2
+
+sudo rm -R ../../tools/docker/geth-testnet/data-geth1/geth
+sudo rm ../../tools/docker/geth-testnet/data-geth1/geth.ipc*
