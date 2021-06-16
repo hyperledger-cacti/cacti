b370dc74d536c33e625208562b52c28e5c7fdf29
Hana Awad <awadhana0825@gmail.com>
2021-06-16 03:18:00 -0400

    test: api-surface test refactored in various packages. Batch 2
    Fixes #1044
    
    Signed-off-by: Hana Awad <awadhana0825@gmail.com>

--- packages/cactus-plugin-ledger-connector-besu/src/test/typescript/integration/api-surface.ts
+++
@@ -1,9 +0,0 @@
-const tap = require("tap");
-import { PluginFactoryLedgerConnector } from "../../../main/typescript/public-api";
-
-tap.pass("Test file can be executed");
-
-tap.test("Library can be loaded", (assert: any) => {
-  assert.plan(1);
-  assert.ok(PluginFactoryLedgerConnector);
-});
