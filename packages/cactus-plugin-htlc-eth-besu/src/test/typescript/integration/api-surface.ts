b8bd39e43f2c4b35341a5a2398067d9e855ebb5a
Jeffrey Ushry <jeffrey.ushry@accenture.com>
2021-06-17 01:00:04 -0400

    test: renamed and refactored test files Fixes hyperledger#1046
    Signed-off-by: Jeffrey Ushry <jeffrey.ushry@accenture.com>

--- packages/cactus-plugin-htlc-eth-besu/src/test/typescript/integration/api-surface.ts
+++
@@ -1,8 +0,0 @@
-import test, { Test } from "tape";
-import { PluginHtlcEthBesu } from "../../../main/typescript/public-api";
-
-test("Library can be loaded", (t: Test) => {
-  t.plan(2);
-  t.ok(PluginHtlcEthBesu);
-  t.pass("Test file can be executed");
-});
