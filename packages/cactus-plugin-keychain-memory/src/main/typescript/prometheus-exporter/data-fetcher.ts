8a946edd683bce1c6927aa56ec857067b3230cb1
Youngone Lee <youngone.lee@accenture.com>
2021-06-23 11:57:22 -0500

    style(data-fetcher): multiple linter fix
    Signed-off-by: Youngone Lee <youngone.lee@accenture.com>

--- packages/cactus-plugin-keychain-memory/src/main/typescript/prometheus-exporter/data-fetcher.ts
+++ packages/cactus-plugin-keychain-memory/src/main/typescript/prometheus-exporter/data-fetcher.ts
@@ -5,7 +5,7 @@ import {
   K_CACTUS_KEYCHAIN_MEMORY_TOTAL_KEY_COUNT,
 } from "./metrics";
 
-export async function collectMetrics(keyCount: KeyCount) {
+export async function collectMetrics(keyCount: KeyCount): Promise<void> {
   totalKeyCount
     .labels(`${K_CACTUS_KEYCHAIN_MEMORY_TOTAL_KEY_COUNT}`)
     .set(keyCount.counter);
--- packages/cactus-plugin-keychain-vault/src/main/typescript/prometheus-exporter/data-fetcher.ts
+++ packages/cactus-plugin-keychain-vault/src/main/typescript/prometheus-exporter/data-fetcher.ts
@@ -5,7 +5,7 @@ import {
   K_CACTUS_KEYCHAIN_VAULT_MANAGED_KEY_COUNT,
 } from "./metrics";
 
-export async function collectMetrics(vaultKeys: VaultKeys) {
+export async function collectMetrics(vaultKeys: VaultKeys): Promise<void> {
   totalKeyCount
     .labels(K_CACTUS_KEYCHAIN_VAULT_MANAGED_KEY_COUNT)
     .set(vaultKeys.size);
