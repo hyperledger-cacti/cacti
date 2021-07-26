76895e45588285f1c1af9f380d94dfc336e418d4
Krishnasuri Narayanam <knaraya3@in.ibm.com>
2021-07-26 15:59:56 +0530

    changing asset exchange interop cc as a package
    Signed-off-by: Krishnasuri Narayanam <knaraya3@in.ibm.com>

--- tests/network-setups/fabric/dev/scripts/setupCC.sh
+++ tests/network-setups/fabric/dev/scripts/setupCC.sh
@@ -2,6 +2,7 @@ directory=$(dirname $0)
 
 TMP_PATH=$PWD/../shared/tmp
 CHAINCODE_PATH=$PWD/../shared/chaincode
+INTEROPCC_CHAINCODE_PATH=$PWD/../../../../core/network
 
 # interop cc module
 INTEROPCC_MOD=github.com/hyperledger-labs/weaver-dlt-interoperability/core/network/fabric-interop-cc/contracts/interop
@@ -15,6 +16,7 @@ export GOPATH=$TMP_PATH
 # Download interopcc and copy it into correct folder
 go get -d "${INTEROPCC_MOD}${INTEROP_VERSION}"
 cp -r $TMP_PATH/pkg/mod/github.com/hyperledger-labs/weaver-dlt-interoperability/core/network/fabric-interop-cc/contracts/interop* $CHAINCODE_PATH/interop
+cp -r $INTEROPCC_CHAINCODE_PATH/fabric-interop-cc/libs/assetexchange $CHAINCODE_PATH/interop
 
 # Clean tmp and Undo gopath
 go clean -modcache
