ce076d709f8e0cba143f8fe9d71f1de1df8f71dc
Peter Somogyvari <peter.somogyvari@accenture.com>
2021-08-10 15:27:29 -0700

    fix(prometheus): metrics.ts leaks to global registry #1202
    1. Specified a `register` property of the gauges as an empty
    array so that it does not pollute the global namespace. This
    is how this is supposed to be done as per the docs of prom-client.
    
    2. Once the change from 1) took place, the issue became that
    the metrics gathering code was still trying to hit up the
    global scope for the metrics, e.g. calling the get metrics
    methods directly on the promClient object instead of the
    registry that we create for each prmoetheus exporter object
    separately. So a little additional refactor ensued to fix this
    as well by making sure that we grab a reference of the registry
    object at construction time and then re-use that wherever needed
    instead of going through the global promClient object.
    
    3. Added missing .startMetricsCollection calls in the plugin
    constructors to ensure that the prometheus exporter object
    gets initialized properly (this is where the registry gets
    created as well so without this there are crashes happening
    when one tries to access the metrics through the registry)
    
    Why though?
    The problem was that the metrics.ts file that we have for all the
    plugin's metrics constructs a new Metric (Gauge) object at import
    time which then defaults to registering the metric in the global
    registry of prom-client by default.
    
    The latter was causing crashes when separate versions of the same
    metrics.ts file are imported in a scenario were the API server
    imports plugins from a different directory (this issue is coming
    from the branch where I'm working on plugin sandboxing via the
    live-plugin-manager).
    
    Fixes #1202
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-plugin-keychain-memory/src/main/typescript/prometheus-exporter/metrics.ts
+++ packages/cactus-plugin-keychain-memory/src/main/typescript/prometheus-exporter/metrics.ts
@@ -4,6 +4,7 @@ export const K_CACTUS_KEYCHAIN_MEMORY_TOTAL_KEY_COUNT =
   "cactus_keychain_memory_total_key_count";
 
 export const totalKeyCount = new Gauge({
+  registers: [],
   name: K_CACTUS_KEYCHAIN_MEMORY_TOTAL_KEY_COUNT,
   help: "Total keys present in memory",
   labelNames: ["type"],
