1437b3809efd27239b56ca6e2599a2a0a7a96367
Dhinakaran Vinayagamurthy <dhinakaran2705@gmail.com>
2021-08-16 05:19:17 +0000

    Cleaning Makefile and fixing a mistake in port specification
    Signed-off-by: Dhinakaran Vinayagamurthy <dhinakaran2705@gmail.com>

--- tests/network-setups/besu/scripts/Node_session.sh
+++ tests/network-setups/besu/scripts/Node_session.sh
@@ -5,7 +5,7 @@ then
 	bootnodeID=$(curl -X POST --data '{"jsonrpc":"2.0","method":"net_enode","params":[],"id":1}' http://127.0.0.1:8545 | python3 -c "import sys, json; print(json.load(sys.stdin)['result'])")
 elif [ $2 == 2 ]
 then
-	bootnodeID=$(curl -X POST --data '{"jsonrpc":"2.0","method":"net_enode","params":[],"id":1}' http://127.0.0.1:9545 | python3 -c "import sys, json; print(json.load(sys.stdin)['result'])")
+	bootnodeID=$(curl -X POST --data '{"jsonrpc":"2.0","method":"net_enode","params":[],"id":1}' http://127.0.0.1:9544 | python3 -c "import sys, json; print(json.load(sys.stdin)['result'])")
 fi
 
 besu --config-file=config$1.toml --bootnodes=$bootnodeID
