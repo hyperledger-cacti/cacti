18e2dd773cef2ac805d7a3b1d291c53d7e55dfe9
VRamakrishna <vramakr2@in.ibm.com>
2021-09-13 20:31:01 +0000

    Removed testMode guard from chaincode unit tests
    Instead, added logic to associate suitable certificates with mock chaincode stub clients
    
    Signed-off-by: VRamakrishna <vramakr2@in.ibm.com>

--- samples/fabric/simplestatewithacl/go.mod
+++ samples/fabric/simplestatewithacl/go.mod
@@ -3,9 +3,11 @@ module github.com/hyperledger-labs/weaver-dlt-interoperability/samples/fabric/si
 go 1.15
 
 require (
-	github.com/hyperledger-labs/weaver-dlt-interoperability/core/network/fabric-interop-cc/libs/testutils v0.0.0-20210829173151-532300d55db5
-	github.com/hyperledger-labs/weaver-dlt-interoperability/core/network/fabric-interop-cc/libs/utils v0.0.0-20210829173151-532300d55db5
+	github.com/golang/protobuf v1.5.2
+	github.com/hyperledger-labs/weaver-dlt-interoperability/core/network/fabric-interop-cc/libs/testutils v0.0.0-20210909191523-de832057a3ab
+	github.com/hyperledger-labs/weaver-dlt-interoperability/core/network/fabric-interop-cc/libs/utils v0.0.0-20210909191523-de832057a3ab
 	github.com/hyperledger/fabric-chaincode-go v0.0.0-20210718160520-38d29fabecb9
 	github.com/hyperledger/fabric-contract-api-go v1.1.1
+	github.com/hyperledger/fabric-protos-go v0.0.0-20210720123151-f0dc3e2a0871
 	github.com/stretchr/testify v1.7.0
 )
--- samples/fabric/simplestatewithacl/simplestate_test.go
+++ samples/fabric/simplestatewithacl/simplestate_test.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"encoding/base64"
 	"testing"
 	"fmt"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/stretchr/testify/require"
+	mspProtobuf "github.com/hyperledger/fabric-protos-go/msp"
 	wtest "github.com/hyperledger-labs/weaver-dlt-interoperability/core/network/fabric-interop-cc/libs/testutils"
 )
+
+// function that supplies value that is to be returned by ctx.GetStub().GetCreator()
+func getCreator() string {
+	serializedIdentity := &mspProtobuf.SerializedIdentity{}
+	eCertBytes, _ := base64.StdEncoding.DecodeString(getTxCreatorECertBase64())
+	serializedIdentity.IdBytes = []byte(eCertBytes)
+	serializedIdentity.Mspid = "Org1MSP"
+	serializedIdentityBytes, _ := proto.Marshal(serializedIdentity)
+
+	return string(serializedIdentityBytes)
+}
+
+// function that supplies the ECert in base64 for the transaction creator
+func getTxCreatorECertBase64() string {
+	eCertBase64 := "LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUNoekNDQWkyZ0F3SUJBZ0lVV0tOUmlmVStDRENTZ0pGRFB1RjhCQnpmOEtNd0NnWUlLb1pJemowRUF3SXcKYURFTE1Ba0dBMVVFQmhNQ1ZWTXhGekFWQmdOVkJBZ1REazV2Y25Sb0lFTmhjbTlzYVc1aE1SUXdFZ1lEVlFRSwpFd3RJZVhCbGNteGxaR2RsY2pFUE1BMEdBMVVFQ3hNR1JtRmljbWxqTVJrd0Z3WURWUVFERXhCbVlXSnlhV010ClkyRXRjMlZ5ZG1WeU1CNFhEVEl4TURjek1EQTFORGt3TUZvWERUSXlNRGN6TURBMU5UUXdNRm93UWpFd01BMEcKQTFVRUN4TUdZMnhwWlc1ME1Bc0dBMVVFQ3hNRWIzSm5NVEFTQmdOVkJBc1RDMlJsY0dGeWRHMWxiblF4TVE0dwpEQVlEVlFRREV3VnlaV3hoZVRCWk1CTUdCeXFHU000OUFnRUdDQ3FHU000OUF3RUhBMElBQkNvekYzTTFMMmlJCklSN1JhWWFQTnZWY2Z0R2x0b0tlZ085TUJQZE1oeEJ5aUJxU2lxSW53cjVlVU84M3FOd1VhWHZ3TVZtUzh2LzQKYmcvMGZlYnE4ZXlqZ2Rvd2dkY3dEZ1lEVlIwUEFRSC9CQVFEQWdlQU1Bd0dBMVVkRXdFQi93UUNNQUF3SFFZRApWUjBPQkJZRUZLZmtwOHR3cU9PMGp3K3Y5SDdlL3lGWmFOaENNQjhHQTFVZEl3UVlNQmFBRkhLRzlZTDZPQzg2Cnk1RklYcHF0ZTdiT1hTRDhNSGNHQ0NvREJBVUdCd2dCQkd0N0ltRjBkSEp6SWpwN0ltaG1Ma0ZtWm1sc2FXRjAKYVc5dUlqb2liM0puTVM1a1pYQmhjblJ0Wlc1ME1TSXNJbWhtTGtWdWNtOXNiRzFsYm5SSlJDSTZJbkpsYkdGNQpJaXdpYUdZdVZIbHdaU0k2SW1Oc2FXVnVkQ0lzSW5KbGJHRjVJam9pZEhKMVpTSjlmVEFLQmdncWhrak9QUVFECkFnTklBREJGQWlFQTJwaDMvbkZZOXF5MmhyKzBWMkFHdDlqRWhEeC9kNmxZVitPck5PWmN6NEFDSUFQZW9GTEcKeGJ4M0lBQUQ2T2xsUjBCTVhqTHIzcHRDc3ExWlNVR2xoSzBwCi0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0K"
+	return eCertBase64
+}
 
 func TestCreate(t *testing.T) {
 	ctx, chaincodeStub := wtest.PrepMockStub()
@@ -28,7 +48,9 @@ func TestCreate(t *testing.T) {
 func TestRead(t *testing.T) {
 	ctx, chaincodeStub := wtest.PrepMockStub()
 	ss := SmartContract{}
-	ss.testMode = true
+	chaincodeStub.GetCreatorReturns([]byte(getCreator()), nil)
+	interopCCId := "interopcc"
+	wtest.SetMockStubCCId(chaincodeStub, "mycc")
 
 	key := "test-key"
 	value := "test-value"
@@ -39,8 +61,18 @@ func TestRead(t *testing.T) {
 	require.EqualError(t, err, "Failed to read key '" + key + "' from world state. " + "failed reading from ledger")
 
 	valueBytes := []byte(value)
-	chaincodeStub.GetStateReturnsOnCall(1, valueBytes, nil)
+	chaincodeStub.GetStateReturnsOnCall(1, []byte(interopCCId), nil)
+
+	// Test failure: local CC is directly invoked by the client without going through interop CC
 	retValue, err := ss.Read(ctx, key)
+	require.Error(t, err)
+	require.EqualError(t, err, "Illegal access by relay")
+
+	// Now test success after setting the interop CC as the chaincode first invoked by the client
+	wtest.SetMockStubCCId(chaincodeStub, interopCCId)
+	chaincodeStub.GetStateReturnsOnCall(2, []byte(interopCCId), nil)
+	chaincodeStub.GetStateReturnsOnCall(3, valueBytes, nil)
+	retValue, err = ss.Read(ctx, key)
 	require.NoError(t, err)
 	require.Equal(t, retValue, value)
 }
