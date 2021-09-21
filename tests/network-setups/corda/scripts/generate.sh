7099940546b2fc9f950366020ccd85e267f51449
sannish <sandeep.nishad1@ibm.com>
2021-09-21 08:47:10 +0000

    added partyb to corda network, upgrade to corda 4.8
    Signed-off-by: sannish <sandeep.nishad1@ibm.com>

--- tests/network-setups/corda/scripts/generate.sh
+++ tests/network-setups/corda/scripts/generate.sh
@@ -10,7 +10,12 @@ rm -rf $directory/../build/nodes/Notary/persistence
 rm -rf $directory/../build/nodes/PartyA/certificates
 rm -rf $directory/../build/nodes/PartyA/persistence
 
+rm -rf $directory/../build/nodes/PartyB/certificates
+rm -rf $directory/../build/nodes/PartyB/persistence
+
 cp -r $directory/../credentials/notary-certificates/certificates  $directory/../build/nodes/Notary
 cp -r $directory/../credentials/notary-certificates/persistence  $directory/../build/nodes/Notary
 cp -r $directory/../credentials/partyA-certificates/certificates  $directory/../build/nodes/PartyA
 cp -r $directory/../credentials/partyA-certificates/persistence  $directory/../build/nodes/PartyA
+cp -r $directory/../credentials/partyB-certificates/certificates  $directory/../build/nodes/PartyB
+cp -r $directory/../credentials/partyB-certificates/persistence  $directory/../build/nodes/PartyB
--- tests/network-setups/corda/scripts/start-nodes.sh
+++ tests/network-setups/corda/scripts/start-nodes.sh
@@ -2,21 +2,26 @@
 
 directory=$(dirname $0)
 simpleAppVersion="0.4"
-weaverVersion="1.2.3"
+weaverVersion="1.2.4-alpha.1"
 
 cp $directory/../artifacts/contracts-kotlin-$simpleAppVersion.jar build/nodes/Notary/cordapps
 cp $directory/../artifacts/contracts-kotlin-$simpleAppVersion.jar build/nodes/PartyA/cordapps
+cp $directory/../artifacts/contracts-kotlin-$simpleAppVersion.jar build/nodes/PartyB/cordapps
 cp $directory/../artifacts/workflows-kotlin-$simpleAppVersion.jar build/nodes/Notary/cordapps
 cp $directory/../artifacts/workflows-kotlin-$simpleAppVersion.jar build/nodes/PartyA/cordapps
+cp $directory/../artifacts/workflows-kotlin-$simpleAppVersion.jar build/nodes/PartyB/cordapps
 
 cp $directory/../artifacts/interop-contracts-$weaverVersion.jar build/nodes/Notary/cordapps
 cp $directory/../artifacts/interop-contracts-$weaverVersion.jar build/nodes/PartyA/cordapps
+cp $directory/../artifacts/interop-contracts-$weaverVersion.jar build/nodes/PartyB/cordapps
 cp $directory/../artifacts/interop-workflows-$weaverVersion.jar build/nodes/Notary/cordapps
 cp $directory/../artifacts/interop-workflows-$weaverVersion.jar build/nodes/PartyA/cordapps
+cp $directory/../artifacts/interop-workflows-$weaverVersion.jar build/nodes/PartyB/cordapps
 
 cp $directory/../artifacts/protos-java-kt-$weaverVersion.jar build/nodes/Notary/cordapps
 cp $directory/../artifacts/protos-java-kt-$weaverVersion.jar build/nodes/PartyA/cordapps
+cp $directory/../artifacts/protos-java-kt-$weaverVersion.jar build/nodes/PartyB/cordapps
 
 docker-compose up -d
 docker ps -a
-docker logs corda_partya_1 -f
+#docker logs corda_partya_1 -f
