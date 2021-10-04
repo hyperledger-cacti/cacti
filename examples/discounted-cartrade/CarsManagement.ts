99be64c04008b6786625b1ae2b26dfb23b22dcdd
Takuma TAKEUCHI <takeuchi.takuma@fujitsu.com>
2021-10-04 21:47:47 +0900

    refactor(examples): adjust disc-cartrade to API design
    Signed-off-by: Takuma TAKEUCHI <takeuchi.takuma@fujitsu.com>

--- examples/discounted-cartrade/CarsManagement.ts
+++ examples/discounted-cartrade/CarsManagement.ts
@@ -5,74 +5,79 @@
  * CarsManagement.ts
  */
 
-import { LPInfoHolder } from '../../packages/cactus-cmd-socketio-server/src/main/typescript/routing-interface/util/LPInfoHolder';
-import { VerifierBase } from '../../packages/cactus-cmd-socketio-server/src/main/typescript/verifier/VerifierBase';
-import { ConfigUtil } from '../../packages/cactus-cmd-socketio-server/src/main/typescript/routing-interface/util/ConfigUtil';
+import { LPInfoHolder } from "../../packages/cactus-cmd-socketio-server/src/main/typescript/routing-interface/util/LPInfoHolder";
+import { Verifier } from "../../packages/cactus-cmd-socketio-server/src/main/typescript/verifier/Verifier";
+import { ConfigUtil } from "../../packages/cactus-cmd-socketio-server/src/main/typescript/routing-interface/util/ConfigUtil";
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const config: any = ConfigUtil.getConfig();
 import { getLogger } from "log4js";
-const moduleName = 'CarsManagement';
+const moduleName = "CarsManagement";
 const logger = getLogger(`${moduleName}`);
 logger.level = config.logLevel;
 
 export class CarsManagement {
-    private connectInfo: LPInfoHolder = null;                   // connection information
-    private verifierFabric: VerifierBase = null;
+  private connectInfo: LPInfoHolder = null; // connection information
+  private verifierFabric: Verifier = null;
 
-    constructor() {
-        this.connectInfo = new LPInfoHolder();
-    }
+  constructor() {
+    this.connectInfo = new LPInfoHolder();
+  }
 
-    queryCar(carID: string): Promise<any> {
-        return new Promise((resolve, reject) => {
-            if (this.verifierFabric === null) {
-                logger.debug("create verifierFabric");
-                const ledgerPluginInfo: string = this.connectInfo.getLegerPluginInfo("r9IS4dDf");
-                this.verifierFabric = new VerifierBase(ledgerPluginInfo);
-            }
+  queryCar(carID: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      if (this.verifierFabric === null) {
+        logger.debug("create verifierFabric");
+        const ledgerPluginInfo: string =
+          this.connectInfo.getLegerPluginInfo("r9IS4dDf");
+        this.verifierFabric = new Verifier(ledgerPluginInfo);
+      }
 
-            const contract = {"channelName": "mychannel", "contractName": "fabcar"};
-            const method = {type: "evaluateTransaction", command: "queryCar"};
-            const template = "default";
-            const args = {"args": [carID]};
-            // const method = "default";
-            // const args = {"method": {type: "evaluateTransaction", command: "queryCar"},"args": {"args": [carID]}};
+      const contract = { channelName: "mychannel", contractName: "fabcar" };
+      const method = { type: "evaluateTransaction", command: "queryCar" };
+      const template = "default";
+      const args = { args: [carID] };
+      // const method = "default";
+      // const args = {"method": {type: "evaluateTransaction", command: "queryCar"},"args": {"args": [carID]}};
 
-            this.verifierFabric.execSyncFunction(contract, method, template, args).then(result => {
-                resolve(result);
-            }).catch((err) => {
-                logger.error(err);
-                reject(err);
-            });
-
+      this.verifierFabric
+        .sendSyncRequest(contract, method, args)
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((err) => {
+          logger.error(err);
+          reject(err);
         });
-    }
+    });
+  }
 
-    queryAllCars(): Promise<any> {
-        return new Promise((resolve, reject) => {
-            if (this.verifierFabric === null) {
-                logger.debug("create verifierFabric");
-                const ledgerPluginInfo: string = this.connectInfo.getLegerPluginInfo("r9IS4dDf");
-                this.verifierFabric = new VerifierBase(ledgerPluginInfo);
-            }
+  queryAllCars(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      if (this.verifierFabric === null) {
+        logger.debug("create verifierFabric");
+        const ledgerPluginInfo: string =
+          this.connectInfo.getLegerPluginInfo("r9IS4dDf");
+        this.verifierFabric = new Verifier(ledgerPluginInfo);
+      }
 
-            const contract = {"channelName": "mychannel", "contractName": "fabcar"};
-            const method = {type: "evaluateTransaction", command: "queryAllCars"};
-            const template = "default";
-            const args = {"args": []};
-            // const method = "default";
-            // const args = {"method": {type: "evaluateTransaction", command: "queryAllCars"}, "args": {"args": []}};
+      const contract = { channelName: "mychannel", contractName: "fabcar" };
+      const method = { type: "evaluateTransaction", command: "queryAllCars" };
+      const template = "default";
+      const args = { args: [] };
+      // const method = "default";
+      // const args = {"method": {type: "evaluateTransaction", command: "queryAllCars"}, "args": {"args": []}};
 
-            this.verifierFabric.execSyncFunction(contract, method, template, args).then(result => {
-                resolve(result);
-            }).catch((err) => {
-                logger.error(err);
-                reject(err);
-            });
-
+      this.verifierFabric
+        .sendSyncRequest(contract, method, args)
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((err) => {
+          logger.error(err);
+          reject(err);
         });
-    }
-
+    });
+  }
 }
