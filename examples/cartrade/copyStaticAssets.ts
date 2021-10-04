b7e98769de4b788731903fe8e791a727ae3b7015
Takuma TAKEUCHI <takeuchi.takuma@fujitsu.com>
2021-10-04 20:56:05 +0900

    refactor(examples): adjust car-trade to API design
    Signed-off-by: Takuma TAKEUCHI <takeuchi.takuma@fujitsu.com>

--- examples/cartrade/copyStaticAssets.ts
+++ examples/cartrade/copyStaticAssets.ts
@@ -5,11 +5,11 @@
  * copyStaticAssets.ts
  */
 
-import * as shell from 'shelljs';
+import * as shell from "shelljs";
 
 // NOTE: Copy the static assets to the dist folder.
 //      Example:
 //        shell.cp('-R', 'src/routing-interface/views', 'dist/routing-interface/views/');
 //shell.cp('-R', 'config/default.json', 'dist/examples/cartrade/config/');
-shell.cp('-R', 'config/default.yaml', '/etc/cactus/');
-shell.cp('-R', 'config/usersetting.yaml', '/etc/cactus/');
+shell.cp("-R", "config/default.yaml", "/etc/cactus/");
+shell.cp("-R", "config/usersetting.yaml", "/etc/cactus/");
