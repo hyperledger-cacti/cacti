cec18a1f9386438242fd2cf597bb19ec83158bd8
Takuma TAKEUCHI <takeuchi.takuma@fujitsu.com>
2021-10-04 21:59:23 +0900

    feat(cmd-server-socket): add a communication test to open-api validators
    Signed-off-by: Takuma TAKEUCHI <takeuchi.takuma@fujitsu.com>

--- examples/test-run-transaction/supply-chain-app-stub/app/app.ts
+++ examples/test-run-transaction/supply-chain-app-stub/app/app.ts
@@ -6,13 +6,13 @@
  */
 
 // Load the library
-import express from 'express';
-import helmet from 'helmet';
-import cors from 'cors';
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
 const app = express();
 app.use(helmet());
 app.use(cors());
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 //import indexRouter from   './routes/index';
 ///import loginRouter from   './routes/login';
@@ -32,23 +32,25 @@ app.use(bodyParser.json());
 // Dynamic loading
 console.debug(`start Dynamic loading`);
 const targetJsArray = [
-//    {uri: '/api/v1/login/', jsPath: './routes/login.js'},
-//    {uri: '/api/v1/cars/', jsPath: './routes/cars.js'},
-    {uri: '/api/v1/plugins/@hyperledger/cactus-plugin-ledger-connector-fabric/test-run-transaction/', jsPath: './routes/supply-chain-app.js'}
+  //    {uri: '/api/v1/login/', jsPath: './routes/login.js'},
+  //    {uri: '/api/v1/cars/', jsPath: './routes/cars.js'},
+  {
+    uri: "/api/v1/plugins/@hyperledger/cactus-plugin-ledger-connector-fabric/test-run-transaction/",
+    jsPath: "./routes/supply-chain-app.js",
+  },
 ];
-for (var targetJs of targetJsArray) {
-    console.debug(`url: ${targetJs.uri}, jsPath: ${targetJs.jsPath}`);
-    // var targetRouter = require(targetJs.jsPath).default;
-    app.use(targetJs.uri, require(targetJs.jsPath).default);
+for (const targetJs of targetJsArray) {
+  console.debug(`url: ${targetJs.uri}, jsPath: ${targetJs.jsPath}`);
+  // var targetRouter = require(targetJs.jsPath).default;
+  app.use(targetJs.uri, require(targetJs.jsPath).default);
 }
-
 
 const port = process.env.PORT || 5053; // Specify the port number
 
-app.get('/helloWorld', (req, res) => {
-    res.status(200).send({ message: 'hello, world' });
+app.get("/helloWorld", (req, res) => {
+  res.status(200).send({ message: "hello, world" });
 });
 
 // Server startup
 app.listen(port);
-console.log('listen on port ' + port);
+console.log("listen on port " + port);
