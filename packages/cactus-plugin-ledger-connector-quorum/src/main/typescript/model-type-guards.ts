f7eb39bb1392b2762adac3a189da071249f4eca3
katherine <krosenk729@gmail.com>
2021-10-17 10:12:09 -0400

    fix(lint): fix issue #1359
    Signed-off-by: katherine <krosenk729@gmail.com>

--- packages/cactus-plugin-ledger-connector-quorum/src/main/typescript/model-type-guards.ts
+++ packages/cactus-plugin-ledger-connector-quorum/src/main/typescript/model-type-guards.ts
@@ -6,27 +6,29 @@ import {
   Web3SigningCredentialType,
 } from "./generated/openapi/typescript-axios/api";
 
-export function isWeb3SigningCredentialPrivateKeyHex(
-  x: any,
-): x is Web3SigningCredentialPrivateKeyHex {
-  return x?.type && x?.type === Web3SigningCredentialType.PrivateKeyHex;
+export function isWeb3SigningCredentialPrivateKeyHex(x?: {
+  type?: Web3SigningCredentialType;
+}): x is Web3SigningCredentialPrivateKeyHex {
+  return x?.type === Web3SigningCredentialType.PrivateKeyHex;
 }
 
-export function isWeb3SigningCredentialNone(
-  x: any,
-): x is Web3SigningCredentialNone {
-  return x?.type && x?.type === Web3SigningCredentialType.None;
+export function isWeb3SigningCredentialNone(x?: {
+  type?: Web3SigningCredentialType;
+}): x is Web3SigningCredentialNone {
+  return x?.type === Web3SigningCredentialType.None;
 }
 
-export function isWeb3SigningCredentialGethKeychainPassword(
-  x: any,
-): x is Web3SigningCredentialGethKeychainPassword {
-  return x?.type && x?.type === Web3SigningCredentialType.GethKeychainPassword;
+export function isWeb3SigningCredentialGethKeychainPassword(x?: {
+  type?: Web3SigningCredentialType;
+}): x is Web3SigningCredentialGethKeychainPassword {
+  return x?.type === Web3SigningCredentialType.GethKeychainPassword;
 }
 
-export function isWeb3SigningCredentialCactusKeychainRef(
-  x: any,
-): x is Web3SigningCredentialCactusKeychainRef {
+export function isWeb3SigningCredentialCactusKeychainRef(x?: {
+  type?: Web3SigningCredentialType;
+  keychainEntryKey?: string | unknown;
+  keychainId?: string | unknown;
+}): x is Web3SigningCredentialCactusKeychainRef {
   return (
     !!x?.type &&
     x?.type === Web3SigningCredentialType.CactusKeychainRef &&
