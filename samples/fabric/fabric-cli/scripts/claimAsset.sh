838dfed57603f381fdcb81b18c07520458c36c9e
VRamakrishna <vramakr2@in.ibm.com>
2021-10-27 21:41:35 +0000

    Added network arguments to Fabric CLI asset transfer claim and reclaim scripts
    Signed-off-by: VRamakrishna <vramakr2@in.ibm.com>

--- samples/fabric/fabric-cli/scripts/claimAsset.sh
+++ samples/fabric/fabric-cli/scripts/claimAsset.sh
@@ -1,12 +1,12 @@
 # Set the right function in .env
-if [ "$1" == "bond" ]
+if [ "$3" == "bond" ]
 then
 	sed -i "s/DEFAULT_APPLICATION_FUNC=.*/DEFAULT_APPLICATION_FUNC=ClaimRemoteAsset/g" .env
-elif [ "$1" == "token" ]
+elif [ "$3" == "token" ]
 then
 	sed -i "s/DEFAULT_APPLICATION_FUNC=.*/DEFAULT_APPLICATION_FUNC=ClaimRemoteTokenAsset/g" .env
 else
-	echo "Unknown asset category: "$1
+	echo "Unknown asset category: "$3
 	exit 1
 fi
 ./bin/fabric-cli env set-file ./.env
@@ -14,19 +14,19 @@ fi
 cd scripts
 
 # Create a chaincode.json from the template file after replacing user IDs with certificates
-USER_CERT_BASE64=$(node getAssetTransferViewAddress.js getusercert network1 alice)
+USER_CERT_BASE64=$(node getAssetTransferViewAddress.js getusercert $2 alice)
 sed "s/<alice>/$USER_CERT_BASE64/g" ../chaincode.json.template > ../chaincode.json
-sed -i "s/<assettype>/$2/g" ../chaincode.json
-sed -i "s/<assetid>/$3/g" ../chaincode.json
-if [ "$1" == "token" ]
+sed -i "s/<assettype>/$4/g" ../chaincode.json
+sed -i "s/<assetid>/$5/g" ../chaincode.json
+if [ "$3" == "token" ]
 then
-	sed -i "s/<numunits>/$4/g" ../chaincode.json
+	sed -i "s/<numunits>/$6/g" ../chaincode.json
 fi
 
 # Get view address by running JS code
-VIEW_ADDRESS=$(node getAssetTransferViewAddress.js claim $1 network1 alice network2 bob $2 $3)
+VIEW_ADDRESS=$(node getAssetTransferViewAddress.js claim $3 $2 alice $1 bob $4 $5)
 
 cd ..
 
 # Run interop query using view address
-./bin/fabric-cli interop --local-network=network2 --requesting-org=Org1MSP $VIEW_ADDRESS --user=bob
+./bin/fabric-cli interop --local-network=$1 --remote-network=$2 --requesting-org=Org1MSP $VIEW_ADDRESS --user=bob
