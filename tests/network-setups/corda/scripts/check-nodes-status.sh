34ce95ae507020176b609c4f97a1666dcbb9cf1e
VRamakrishna <vramakr2@in.ibm.com>
2021-10-28 07:04:11 +0000

    Added timeouts in Corda testnet startup checks
    Signed-off-by: VRamakrishna <vramakr2@in.ibm.com>

--- tests/network-setups/corda/scripts/check-nodes-status.sh
+++ tests/network-setups/corda/scripts/check-nodes-status.sh
@@ -3,19 +3,39 @@
 echo "Waiting for network node services to start"
 
 # Wait for PartyA services in container to start
+COUNT=0
 while [[ $(docker logs corda_partya_1 | tail -n 1) != "Running P2PMessaging loop" ]]
-do sleep 5
+do
+    sleep 5
+    COUNT=$(( COUNT + 1 ))
+    if [[ $COUNT == 20 ]]
+    then
+        break
+    fi
 done
 echo "PartyA node services started"
 
-# Wait for PartyA services in container to start
+# Wait for PartyB services in container to start
+COUNT=0
 while [[ $(docker logs corda_partyb_1 | tail -n 1) != "Running P2PMessaging loop" ]]
-do sleep 5
+do
+    sleep 5
+    COUNT=$(( COUNT + 1 ))
+    if [[ $COUNT == 20 ]]
+    then
+        break
+    fi
 done
 echo "PartyB node services started"
 
-# Wait for PartyA services in container to start
+# Wait for Notary services in container to start
 while [[ $(docker logs corda_notary_1 | tail -n 1) != "Running P2PMessaging loop" ]]
-do sleep 5
+do
+    sleep 5
+    COUNT=$(( COUNT + 1 ))
+    if [[ $COUNT == 20 ]]
+    then
+        break
+    fi
 done
 echo "Notary node services started"
