1291539a547ec9548ba4a6ce64514014c5848ca0
sandeep.nishad1 <sandeep.nishad1@ibm.com>
2021-11-11 15:56:51 +0530

    added query hash, preimage functions to libs/assetexchange
    Signed-off-by: sandeep.nishad1 <sandeep.nishad1@ibm.com>

--- core/network/fabric-interop-cc/libs/assetexchange/assetSwapContracts.go
+++ core/network/fabric-interop-cc/libs/assetexchange/assetSwapContracts.go
@@ -51,6 +51,8 @@ const (
 	assetKeyPrefix    = "AssetKey_"   // prefix for the map, asset-key --> asset-object
 	assetKeyDelimiter = "_"           // delimiter for the asset-key
 	contractIdPrefix  = "ContractId_" // prefix for the map, contractId --> asset-key
+	claimAssetKeyPrefix = "ClaimAssetKey_"
+	claimContractIdPrefix = "ClaimContractId_"
 )
 
 // helper functions to log and return errors
@@ -73,6 +75,10 @@ func GenerateSHA256HashInBase64Form(preimage string) string {
 func generateContractIdMapKey(contractId string) string {
 	return contractIdPrefix + contractId
 }
+// function to return the key to fetch an element from the map using contractId
+func generateClaimContractIdMapKey(contractId string) string {
+	return claimContractIdPrefix + contractId
+}
 
 /*
  * Function to generate asset-lock key (which is combination of asset-type and asset-id)
@@ -86,6 +92,17 @@ func GenerateAssetLockKeyAndContractId(ctx contractapi.TransactionContextInterfa
 
 	contractId := GenerateSHA256HashInBase64Form(assetLockKey)
 	return assetLockKey, contractId, nil
+}
+/*
+ * Function to generate asset-lock key (which is combination of asset-type and asset-id)
+ * and contract-id (which is a hash on asset-lock key) for the non-fungible asset locking on the ledger
+ */
+func GenerateClaimAssetLockKey(ctx contractapi.TransactionContextInterface, chaincodeId string, assetAgreement *common.AssetExchangeAgreement) (string, error) {
+	assetLockKey, err := ctx.GetStub().CreateCompositeKey("AssetExchangeContract", []string{chaincodeId, assetAgreement.Type, assetAgreement.Id})
+	if err != nil {
+		return "", logThenErrorf("error while creating composite key: %+v", err)
+	}
+	return claimAssetKeyPrefix + assetLockKey, nil
 }
 
 /*
@@ -525,6 +542,21 @@ func ClaimAsset(ctx contractapi.TransactionContextInterface, callerChaincodeID, 
 		if !isCorrectPreimage {
 			return "", logThenErrorf("cannot claim asset of type %s and ID %s as the hash preimage is not matching", assetAgreement.Type, assetAgreement.Id)
 		}
+		
+		// Record Hash Preimage into ledger
+		claimInfoHTLC := &common.AssetClaimHTLC{}
+		err = proto.Unmarshal(claimInfo.ClaimInfo, claimInfoHTLC)
+		if err != nil {
+			return "", logThenErrorf("unmarshal claimInfo.ClaimInfo error: %s", err)
+		}
+		claimAssetLockKey, err := GenerateClaimAssetLockKey(ctx, callerChaincodeID, assetAgreement)
+		if err != nil {
+			return "", logThenErrorf(err.Error())
+		}
+		err = ctx.GetStub().PutState(claimAssetLockKey, []byte(claimInfoHTLC.HashPreimageBase64))
+		if err != nil {
+			return "", logThenErrorf("failed to write to the world state: %+v", err)
+		}
 	}
 
 	err = ctx.GetStub().DelState(assetLockKey)
@@ -646,6 +678,17 @@ func ClaimAssetUsingContractId(ctx contractapi.TransactionContextInterface, cont
 		}
 		if !isCorrectPreimage {
 			return logThenErrorf("cannot claim asset associated with contractId %s as the hash preimage is not matching", contractId)
+		}
+		
+		// Write HashPreimage to the ledger
+		claimInfoHTLC := &common.AssetClaimHTLC{}
+		err = proto.Unmarshal(claimInfo.ClaimInfo, claimInfoHTLC)
+		if err != nil {
+			return logThenErrorf("unmarshal claimInfo.ClaimInfo error: %s", err)
+		}
+		err = ctx.GetStub().PutState(generateClaimContractIdMapKey(contractId), []byte(claimInfoHTLC.HashPreimageBase64))
+		if err != nil {
+			return logThenErrorf("failed to write to the world state: %+v", err)
 		}
 	}
 
@@ -810,6 +853,17 @@ func ClaimFungibleAsset(ctx contractapi.TransactionContextInterface, contractId,
 		if !isCorrectPreimage {
 			return logThenErrorf("cannot claim fungible asset associated with contractId %s as the hash preimage is not matching", contractId)
 		}
+		
+		// Write HashPreimage to the ledger
+		claimInfoHTLC := &common.AssetClaimHTLC{}
+		err = proto.Unmarshal(claimInfo.ClaimInfo, claimInfoHTLC)
+		if err != nil {
+			return logThenErrorf("unmarshal claimInfo.ClaimInfo error: %s", err)
+		}
+		err = ctx.GetStub().PutState(generateClaimContractIdMapKey(contractId), []byte(claimInfoHTLC.HashPreimageBase64))
+		if err != nil {
+			return logThenErrorf("failed to write to the world state: %+v", err)
+		}
 	}
 
 	err = ctx.GetStub().DelState(generateContractIdMapKey(contractId))
@@ -850,4 +904,105 @@ func UnlockFungibleAsset(ctx contractapi.TransactionContextInterface, contractId
 	}
 
 	return nil
+}
+
+func GetHTLCHash(ctx contractapi.TransactionContextInterface, callerChaincodeID, assetAgreementBytesBase64 string) (string, error) {
+
+	assetAgreementBytes, err := base64.StdEncoding.DecodeString(assetAgreementBytesBase64)
+	if err != nil {
+		return "", logThenErrorf("error in base64 decode of asset agreement: %+v", err)
+	}
+
+	assetAgreement := &common.AssetExchangeAgreement{}
+	err = proto.Unmarshal([]byte(assetAgreementBytes), assetAgreement)
+	if err != nil {
+		return "", logThenErrorf(err.Error())
+	}
+	//display the requested asset agreement
+	log.Infof("assetExchangeAgreement: %+v", assetAgreement)
+
+	assetLockKey, _, err := GenerateAssetLockKeyAndContractId(ctx, callerChaincodeID, assetAgreement)
+	if err != nil {
+		return "", logThenErrorf(err.Error())
+	}
+
+	assetLockValBytes, err := ctx.GetStub().GetState(assetLockKey)
+	if err != nil {
+		return "", logThenErrorf(err.Error())
+	}
+
+	if assetLockValBytes == nil {
+		return "", logThenErrorf("no asset of type %s and ID %s is locked", assetAgreement.Type, assetAgreement.Id)
+	}
+
+	assetLockVal := AssetLockValue{}
+	err = json.Unmarshal(assetLockValBytes, &assetLockVal)
+	if err != nil {
+		return "", logThenErrorf("unmarshal error: %s", err)
+	}
+	log.Infof("assetLockVal: %+v", assetLockVal)
+	
+	return getHTLCHashHelper(ctx, assetLockVal.LockInfo)
+}
+
+func GetHTLCHashByContractId(ctx contractapi.TransactionContextInterface, contractId string) (string, error) {
+	assetLockVal, err := fetchFungibleAssetLocked(ctx, contractId)
+	if err != nil {
+		return "", logThenErrorf(err.Error())
+	}
+	
+	return getHTLCHashHelper(ctx, assetLockVal.LockInfo)
+}
+
+func GetHTLCHashPreImageByContractId(ctx contractapi.TransactionContextInterface, contractId string) (string, error) {
+	return getHashPreImageHelper(ctx, generateClaimContractIdMapKey(contractId))
+}
+
+func GetHTLCHashPreImage(ctx contractapi.TransactionContextInterface, callerChaincodeID, assetAgreementBytesBase64 string) (string, error) {
+	assetAgreementBytes, err := base64.StdEncoding.DecodeString(assetAgreementBytesBase64)
+	if err != nil {
+		return "", logThenErrorf("error in base64 decode of asset agreement: %+v", err)
+	}
+
+	assetAgreement := &common.AssetExchangeAgreement{}
+	err = proto.Unmarshal([]byte(assetAgreementBytes), assetAgreement)
+	if err != nil {
+		return "", logThenErrorf(err.Error())
+	}
+	//display the requested asset agreement
+	log.Infof("assetExchangeAgreement: %+v", assetAgreement)
+	
+	claimAssetLockKey, err := GenerateClaimAssetLockKey(ctx, callerChaincodeID, assetAgreement)
+	if err != nil {
+		return "", logThenErrorf(err.Error())
+	}
+	
+	return getHashPreImageHelper(ctx, claimAssetLockKey)
+}
+
+func getHTLCHashHelper(ctx contractapi.TransactionContextInterface, lockInfo interface{}) (string, error) {
+	lockInfoVal := HashLock{}
+	lockInfoBytes, err := json.Marshal(lockInfo)
+	if err != nil {
+		return "", logThenErrorf("marshal lockInfo error: %s", err)
+	}
+	err = json.Unmarshal(lockInfoBytes, &lockInfoVal)
+	if err != nil {
+		return "", logThenErrorf("unmarshal lockInfoBytes error: %s", err)
+	}
+	
+	return lockInfoVal.HashBase64, nil
+}
+
+func getHashPreImageHelper(ctx contractapi.TransactionContextInterface, key string) (string, error) {
+	hashPreImageBase64Bytes, err := ctx.GetStub().GetState(key)
+	if err != nil {
+		return "", logThenErrorf("failed to retrieve from the world state: %+v", err)
+	}
+
+	if hashPreImageBase64Bytes == nil {
+		return "", logThenErrorf("key %s is not associated with any claimed asset", key)
+	}
+	
+	return string(hashPreImageBase64Bytes), nil
 }
