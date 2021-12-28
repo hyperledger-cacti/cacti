af773b0a90cd10a22c79ad7a72f71136c54b0f21
awadhana <awadhana0825@gmail.com>
2021-12-27 20:42:43 -0500

    test: jestify get-node-jws-endpoint-v1 test
    Migrated test from Tap to Jest.
    
    File Path:
    packages/cactus-plugin-consortium-manual/src/test/
    typescript/unit/consortium/get-node-jws-endpoint-v1.test.ts
    
    This is a PARTIAL resolution to issue #238
    
    Signed-off-by: awadhana <awadhana0825@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -51,7 +51,6 @@ module.exports = {
     `./packages/cactus-test-plugin-htlc-eth-besu-erc20/src/test/typescript/integration/plugin-htlc-eth-besu-erc20/get-single-status-endpoint-invalid.test.ts`,
     `./packages/cactus-test-plugin-htlc-eth-besu-erc20/src/test/typescript/integration/plugin-htlc-eth-besu-erc20/openapi/openapi-validation.test.ts`,
     `./packages/cactus-test-plugin-htlc-eth-besu-erc20/src/test/typescript/integration/plugin-htlc-eth-besu-erc20/refund-endpoint.test.ts`,
-    `./packages/cactus-plugin-consortium-manual/src/test/typescript/unit/consortium/get-node-jws-endpoint-v1.test.ts`,
     `./packages/cactus-test-cmd-api-server/src/test/typescript/integration/remote-plugin-imports.test.ts`,
     `./packages/cactus-test-cmd-api-server/src/test/typescript/integration/plugin-import-with-npm-install.test.ts`,
     `./packages/cactus-test-cmd-api-server/src/test/typescript/integration/plugin-import-with-npm-install-version-selection.test.ts`,
