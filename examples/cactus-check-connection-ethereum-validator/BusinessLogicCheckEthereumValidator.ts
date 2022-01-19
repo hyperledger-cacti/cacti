6ddf7e0f68f197adc9058b679b284aa7238e6277
stepniowskip <piotr.stepniowski@fujitsu.com>
2022-01-19 13:41:43 +0100

    test: enhancement to check connection eth validator blp
    - Add tests for sync (read and write) requests to validator.
    - Add tests for async (read and write) requests to validator.
    - Add handling for Monitor Mode in blp.
    - Add tests for config files.
    - Add automatization tools for tests and setup of blp.
    - Updated readme.
    
    Depends on hyperledger#1629
    
    Signed-off-by: stepniowskip <piotr.stepniowski@fujitsu.com>

--- examples/cactus-check-connection-ethereum-validator/BusinessLogicCheckEthereumValidator.ts
+++ examples/cactus-check-connection-ethereum-validator/BusinessLogicCheckEthereumValidator.ts
@@ -7,6 +7,8 @@ import { transactionManagement } from "../../packages/cactus-cmd-socketio-server
 import { verifierFactory } from "../../packages/cactus-cmd-socketio-server/src/main/typescript/routing-interface/routes/index";
 import { BusinessLogicBase } from "../../packages/cactus-cmd-socketio-server/src/main/typescript/business-logic-plugin/BusinessLogicBase";
 import { Verifier } from "../../packages/cactus-cmd-socketio-server/src/main/typescript/verifier/Verifier";
+import { LedgerEvent } from "../../packages/cactus-cmd-socketio-server/src/main/typescript/verifier/LedgerPlugin";
+import { json2str } from "../../packages/cactus-cmd-socketio-server/src/main/typescript/verifier/DriverCommon";
 
 const fs = require("fs");
 const yaml = require("js-yaml");
@@ -29,7 +31,7 @@ export class BusinessLogicCheckEthereumValidator extends BusinessLogicBase {
   }
 
   startTransaction(req: Request, businessLogicID: string, tradeID: string) {
-    logger.debug("called startTransaction()");
+    logger.debug(` ##startTransaction(): called startTransaction()`);
 
     const requestInfo: RequestInfo = new RequestInfo();
     requestInfo.setBusinessLogicID(businessLogicID);
@@ -40,45 +42,74 @@ export class BusinessLogicCheckEthereumValidator extends BusinessLogicBase {
       requestInfo.tradeID,
     );
 
-    this.executeTransaction(tradeInfo);
+    this.startMonitor(tradeInfo);
   }
 
-  executeTransaction(tradeInfo: TradeInfo) {
+  startMonitor(tradeInfo: TradeInfo) {
     logger.debug(
       `##startMonitor(): businessLogicID: ${tradeInfo.businessLogicID}`,
     );
 
+    const validator = this.getValidator(tradeInfo);
+
+    if (config.monitorMode === true) {
+      logger.debug(`##startMonitor(): Creating validator with Monitor Mode`);
+    } else {
+      logger.debug(`##startMonitor(): Creating validator without Monitor Mode`)
+    }
+
+    const verifier = this.getVerifier(validator);
+
+    this.sendTestRequest(verifier);
+  }
+
+  private getValidator(tradeInfo: TradeInfo) {
+    logger.debug(`##getValidator(): Getting validator with tradeInfo: ${tradeInfo}`);
     const validator = JSON.parse(
       transactionManagement.getValidatorToUse(tradeInfo.businessLogicID),
     );
-    const ethereumValidator = validator["validatorID"][0];
-    const verifier = verifierFactory.getVerifier(
-      ethereumValidator,
-      moduleName,
-      {},
-      false,
-    );
-
-    this.sendRequest(verifier);
+    const ethValidator = validator["validatorID"][0];
+    return ethValidator;
   }
 
-  sendRequest(verifier: Verifier) {
+  private getVerifier(validator: any) {
+    logger.debug(`getVerifier(): Getting Verifier with validator: ${validator}`);
+
+    const verifier = verifierFactory.getVerifier(
+      validator,
+      moduleName,
+      {},
+      config.monitorMode
+    );
+
+    return verifier;
+  }
+
+  stopMonitor(tradeInfo: TradeInfo) {
+    logger.debug(`##calling stopMonitor()`);
+
+    // get validator
+    const validator = this.getValidator(tradeInfo);
+    // get verifier
+    const verifier = this.getVerifier(validator);
+    // stop monitoring
+    verifier.stopMonitor(moduleName);
+  }
+
+  sendTestRequest(verifier: Verifier) {
     const contract = {};
     const method = { command: "getBalance", type: "web3Eth" };
     const accountEthereum = "06fc56347d91c6ad2dae0c3ba38eb12ab0d72e97";
     const args = { args: [accountEthereum] };
 
-    logger.debug(`##sendRequest(): calling verifier.sendSyncRequest()`);
+    logger.debug(`##sendTestRequest(): calling verifier.sendSyncRequest()`);
     verifier
       .sendSyncRequest(contract, method, args)
       .then((response) => {
-        logger.debug(
-          `got response: \n status code: ${response.status} \n Ethereum balance: ${response.data}`,
-        );
         if (response.status == 504 && response.data === undefined) {
-          logger.error(`Could not establish connection to ethereum ledger`);
+          logger.error(`##sendTestRequest(): Could not establish connection to ethereum ledger`);
         } else {
-          logger.info(`Successfully connected to ethereum ledger`);
+          logger.info(`##sendTestRequest(): Successfully connected to ethereum ledger`);
         }
         return { status: response.status, data: response.data };
       })
@@ -86,4 +117,101 @@ export class BusinessLogicCheckEthereumValidator extends BusinessLogicBase {
         logger.error(err);
       });
   }
+
+  onEvent(ledgerEvent: LedgerEvent, targetIndex: number): void {
+    logger.debug(`##onEvent(): ${json2str(ledgerEvent)}`);
+
+    const tx = this.getTransactionFromEvent(ledgerEvent, targetIndex);
+    if (tx == null) {
+      logger.error(`##onEvent(): invalid event: ${json2str(ledgerEvent)}`);
+      return;
+    }
+
+    try {
+      const txId = tx["hash"];
+      const status = ledgerEvent.data["status"];
+      logger.debug(`##onEvent(): txId = ${txId}`);
+      logger.debug(`##onEvent(): status = ${status}`);
+
+      if (status !== 200) {
+        logger.error(
+          `##onEvent(): error event, status: ${status}, txId: ${txId}`
+        );
+        return;
+      }
+    } catch (err) {
+      logger.error(`##onEvent(): err: ${err}, event: ${json2str(ledgerEvent)}`);
+    }
+  }
+
+  getTxIDFromEvent(event: LedgerEvent, targetIndex: number): string | null {
+    const tx = this.getTransactionFromEvent(event, targetIndex);
+    console.log(tx)
+    if (tx == null) {
+      logger.warn(`##getTxIDFromEvent(): skip(not found tx)`);
+      return null;
+    }
+
+    try {
+      const txId = tx["hash"];
+      if (typeof txId !== "string") {
+        logger.warn(
+          `##getTxIDFromEvent(): skip(invalid block, not found txId.), event: ${json2str(
+            event
+          )}`
+        );
+        return null;
+      }
+
+      logger.debug(`##getTxIDFromEvent(): txId: ${txId}`);
+      return txId;
+    } catch (err) {
+      logger.error(
+        `##getTxIDFromEvent(): err: ${err}, event: ${json2str(event)}`
+      );
+      return null;
+    }
+  }
+
+  getEventDataNum(ledgerEvent: LedgerEvent): number {
+    logger.debug(
+      `##getEventDataNum(), ledgerEvent.verifierId: ${ledgerEvent.verifierId}`
+    );
+    const event = ledgerEvent.data;
+    let retEventNum = 0;
+
+    try {
+      const validatorID = config.checkEthereumValidator.connector.validatorID;
+
+      retEventNum = event["blockData"]["transactions"].length;
+      logger.debug(
+        `##getEventDataNum(): retEventNum: ${retEventNum}, verifierId: ${ledgerEvent.verifierId}`
+      );
+      return retEventNum;
+    } catch (err) {
+      logger.error(
+        `##getEventDataNum(): invalid even, err: ${err}, event: ${event}`
+      );
+      return 0;
+    }
+  }
+
+  getTransactionFromEvent(
+    event: LedgerEvent,
+    targetIndex: number
+  ): object | null {
+    try {
+      const retTransaction = event.data["blockData"]["transactions"][targetIndex];
+      logger.debug(
+        `##getTransactionFromEvent(), retTransaction: ${retTransaction}`
+      );
+      console.log(retTransaction)
+      return retTransaction;
+    } catch (err) {
+      logger.error(
+        `##getTransactionFromEvent(): invalid even, err:${err}, event:${event}`
+      );
+    }
+  }
 }
+
--- examples/cactus-check-connection-ethereum-validator/check-ethereum-validator.ts
+++ examples/cactus-check-connection-ethereum-validator/check-ethereum-validator.ts
@@ -6,7 +6,8 @@ import {
   BadRequestError,
 } from "../../packages/cactus-cmd-socketio-server/src/main/typescript/routing-interface/RIFError";
 import { ConfigUtil } from "../../packages/cactus-cmd-socketio-server/src/main/typescript/routing-interface/util/ConfigUtil";
-import { TestEthereumBalance } from "./TestEthereumBalance";
+import { TestEthereumVerifier } from "./TestEthereumVerifier";
+import { request } from "http";
 
 const config: any = ConfigUtil.getConfig();
 const moduleName = "check-ethereum-validator";
@@ -27,6 +28,19 @@ function isRifError(err: any, res: Response): boolean {
   return false;
 }
 
+function checkRequestType(requestType: string): string | boolean {
+
+  if (requestType === undefined) {
+    logger.error(`Unspecified type of request in request body. Use <async, sync>`);
+    return false;
+  }
+  if (requestType.localeCompare("async") !== 0 && requestType.localeCompare("sync") !== 0) {
+    logger.error(`Wrong request type specified in request: ${requestType}. Use <async, sync>`);
+    return false;
+  }
+  return requestType;
+}
+
 router.post("/", (req: Request, res: Response, next: NextFunction) => {
   try {
     logger.info("check-ethereum-validator()");
@@ -39,20 +53,87 @@ router.post("/", (req: Request, res: Response, next: NextFunction) => {
   }
 });
 
-const testEthereumBalance: TestEthereumBalance = new TestEthereumBalance();
+const testEthereumVerifier: TestEthereumVerifier = new TestEthereumVerifier();
 
-router.get("/:account", (req: Request, res: Response, next: NextFunction) => {
+router.post("/getBalance", (req: Request, res: Response, next: NextFunction) => {
+  logger.info("check-ethereum-validator()");
+
+  const requestType = checkRequestType(req.body.requestType);
+
+  if (requestType === false) {
+    return res.end();
+  }
+
+  logger.debug(`Sending ${requestType} request on 'getBalance' endpoint`);
+
   try {
-    logger.info("check-ethereum-validator()");
-    testEthereumBalance
-      .getBalance(req.params.account)
-      .then((result) => {
+    testEthereumVerifier.getBalance(req.body.account,
+      requestType.toString())
+      .then((result: any) => {
+        logger.debug(JSON.stringify(result));
+        res.status(200).json(result);
+      })
+      .catch((err: any) => {
+        logger.error(err);
+      });
+  } catch (err) {
+    if (isRifError(err, res)) return;
+    next(err);
+  }
+
+});
+
+router.post("/transferAssets", (req: Request, res: Response, next: NextFunction) => {
+
+  logger.info("check-ethereum-validator()");
+
+  const requestType = checkRequestType(req.body.requestType);
+
+  if (requestType === false) {
+    return res.end();
+  }
+
+  logger.debug(`Sending ${req.body.requestType} request on 'transferAsset' endpoint`);
+
+  try {
+    testEthereumVerifier.transferAsset(req.body.srcAccount,
+      req.body.destAccount,
+      req.body.amount,
+      req.body.requestType)
+      .then((result: any) => {
         logger.debug(JSON.stringify(result));
         res.status(200).json(result);
       })
       .catch((err) => {
         logger.error(err);
       });
+  } catch (err) {
+    if (isRifError(err, res)) return;
+    next(err);
+  }
+});
+
+
+router.get("/stopMonitor", (req: Request, res: Response, next: NextFunction) => {
+
+  logger.info("check-ethereum-validator()");
+
+  try {
+    testEthereumVerifier.stopMonitor()
+    res.status(200).json(true);
+  } catch (err) {
+    if (isRifError(err, res)) return;
+    next(err);
+  }
+});
+
+router.get("/startMonitor", (req: Request, res: Response, next: NextFunction) => {
+
+  logger.info("check-ethereum-validator()");
+
+  try {
+    testEthereumVerifier.startMonitor()
+    res.status(200).json(true);
   } catch (err) {
     if (isRifError(err, res)) return;
     next(err);
