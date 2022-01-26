bad58f6f1b4f891dc2e06dbebe68accc70aa5f59
Michal Bajer <michal.bajer@fujitsu.com>
2022-01-26 12:52:41 +0100

    refactor(indy-testnet): organize indy-testnet components
    Move and adjust indy validator Dockerfile from tools/docker/indy-testnet
    to it's sources in packages-python/cactus_validator_socketio_indy.
    Rename indy validator dir from cactus_validator_socketio to
    cactus_validator_socketio_indy. Rename clientbase image to indy-sdk-cli,
    move it to separate dir in tools/docker. Adjust paths in indy-testnet
    docker-compose, add dependency to start nginx after validator. Commit
    empty dir tools/docker/indy-testnet/indy_pool/sandbox directly to repo,
    remove redundant setup script.
    
    Closes: #1812
    Signed-off-by: Michal Bajer <michal.bajer@fujitsu.com>

--- tools/docker/indy-testnet/run-before-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir indy_pool/sandbox
-chown 1000:1000 indy_pool/sandbox
-
-cp -r ../../../packages-python/cactus_validator_socketio/ validator/
-cp validator/requirements.txt validator/cactus_validator_socketio/validator-python/
