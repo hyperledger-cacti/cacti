4b594b1c755c6e1c62f0124b3bbe93c22ade5f36
Michal Bajer <michal.bajer@fujitsu.com>
2022-02-02 16:31:44 +0100

    chore(contribs): remove unused Fujitsu-ConnectionChain
    Closes: #1831
    Signed-off-by: Michal Bajer <michal.bajer@fujitsu.com>

--- contribs/Fujitsu-ConnectionChain/connection-chain/environment/base/cc_env/servers/cooperation/ecSide/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright 2019 Fujitsu Laboratories Ltd.
-# SPDX-License-Identifier: Apache-2.0
-
-cd /opt/gopath/src/github.com/hyperledger/fabric/work/server/connector
-npm install
-cd /opt/gopath/src/github.com/hyperledger/fabric/work/server/connector/lib/dependent
-npm install
