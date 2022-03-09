86914cf05a6c96d1abef6da90cd3d5366d3736c4
marouani <marouani@mbp-de-marouani.home>
2022-03-09 11:42:55 +0100

    add command parameters to handle account address n addition to account index, add to customize network parameters
    Signed-off-by: Saoussen MAROUANI <saoussen.marouani@ibm.com>

--- samples/besu/besu-cli/src/commands/asset/lock.ts
+++ samples/besu/besu-cli/src/commands/asset/lock.ts
@@ -18,7 +18,7 @@ const command: GluegunCommand = {
 				print,
 				toolbox,
 				`besu-cli asset lock --network=network1 --sender_account=1 --recipient_account=2 --amount=5 --timeout=1000`,
-				'besu-cli asset lock --network=<network1|network2> --sender_account=<1|2> --recipient_account=<2|1> --amount=<lock-amount> --timeout=<lock-duration-seconds> --hash_base64=<hashLock-optional-parameter>',
+				'besu-cli asset lock --network=<network1|network2> --sender_account=<1|2> --recipient_account=<2|1> --amount=<lock-amount> --timeout=<lock-duration-seconds> --hash_base64=<hashLock-optional-parameter> --recipient_account_address=<recipient-account-address> --sender_account_address=<sender-account-address>  --network_port=<port> --network_host=<host> ',
 				[
 					{
 						name: '--network',
@@ -31,9 +31,19 @@ const command: GluegunCommand = {
 							'The index of the account of the sender/owner of the asset from the list obtained through web3.eth.getAccounts(). For example, we can set Alice as accounts[1] and hence value of this parameter for Alice can be 1.'
 					},
 					{
+						name: '--sender_account_address',
+						description:
+							'The address of the sender account. We can set this parameter if we want to use account address instead of account index '
+					},
+					{
 						name: '--recipient_account',
 						description:
 							'The index of the account of the recipient of the asset from the list obtained through web3.eth.getAccounts(). For example, we can set Alice as accounts[1] and hence value of this parameter for Alice can be 1.'
+					},
+					{
+						name: '--recipient_account_address',
+						description:
+							'The address of the recipient account. We can set this parameter if we want to use account address instead of account index '
 					},
 					{
 						name: '--amount',
@@ -49,6 +59,16 @@ const command: GluegunCommand = {
 						name: '--hash_base64',
 						description:
 							'The hash value with which the asset will be locked i.e., providing its pre-image will enable unlocking the asset. This is an optional parameter. If not provided, we will generate a fresh hash pair with a randomly generated pre-image and output the corresponding pre-image.'
+					},
+					{
+						name: '--network_host',
+						description:
+							'The network host. Default value is taken from config.json'
+					},
+					{
+						name: '--network_port',
+						description:
+							'The network port. Default value is taken from config.json'
 					}
 				],
 				command,
@@ -64,7 +84,19 @@ const command: GluegunCommand = {
 			return
 		}
 		const networkConfig = getNetworkConfig(options.network)
-		const provider = new Web3.providers.HttpProvider('http://'+networkConfig.networkHost+':'+networkConfig.networkPort)
+
+        var networkPort = networkConfig.networkPort
+        if(options.network_port){
+            networkPort = options.network_port
+            console.log('Use network port : ', networkPort)
+    	}
+    	var networkHost = networkConfig.networkHost
+    	if(options.network_host){
+    	    networkHost = options.network_host
+    	    console.log('Use network host : ', networkHost)
+        }
+
+		const provider = new Web3.providers.HttpProvider('http://'+networkHost+':'+networkPort)
 		const web3N = new Web3(provider)
 		const interopContract = await getContractInstance(provider, networkConfig.interopContract).catch(function () {
 			console.log("Failed getting interopContract!");
@@ -84,6 +116,9 @@ const command: GluegunCommand = {
 		if(options.sender_account){
 			sender = accounts[options.sender_account]
 		}
+		else if(options.sender_account_address){
+		    sender = '0x'+ options.sender_account_address
+		}
 		else{
 			print.info('Sender account index not provided. Taking from networkConfig..')
 			sender = accounts[networkConfig.senderAccountIndex]
@@ -91,6 +126,9 @@ const command: GluegunCommand = {
 		var recipient
 		if(options.recipient_account){
 			recipient = accounts[options.recipient_account]
+		}
+		else if(options.recipient_account_address){
+		     recipient = '0x'+ options.recipient_account_address
 		}
 		else{
 			print.info('Recipient account index not provided. Taking from networkConfig..')
