051776c3bc9a99c64c1b73be8c85ed60d98c3f99
Youngone Lee <youngone.lee@accenture.com>
2022-05-18 08:18:06 -0500

    test: jestify server test in cactus common
    Migrate Tap to Jest
    
    File Path:
    packages/cactus-common/src/
    test/typescript/
    unit/servers.test.ts
    
    This is a PARTIAL resolution to issue #238
    
    Signed-off-by: Youngone Lee <youngone.lee@accenture.com>

--- packages/cactus-common/src/test/typescript/unit/servers.test.ts
+++ packages/cactus-common/src/test/typescript/unit/servers.test.ts
@@ -1,7 +1,5 @@
 import { createServer } from "http";
 import { AddressInfo } from "net";
-
-// import test, { Test } from "tape-promise/tape";
 import "jest-extended";
 
 import { Servers } from "../../../main/typescript/index";
@@ -9,10 +7,16 @@ import { Servers } from "../../../main/typescript/index";
 const testCase = "Servers";
 
 describe(testCase, () => {
-  // test("Servers", async (tParent: Test) => {
+  const server = createServer();
+  const portBlocker = createServer();
+
+  afterAll(async () => {
+    await Servers.shutdown(server);
+    await portBlocker.close();
+  });
+
   test("Servers#listen()", async () => {
     {
-      const server = createServer();
       await expect(
         Servers.listen({
           hostname: "x",
@@ -23,7 +27,6 @@ describe(testCase, () => {
     }
 
     {
-      const server = createServer();
       await expect(
         Servers.listen({
           hostname: "localhost",
@@ -31,28 +34,16 @@ describe(testCase, () => {
           server,
         }),
       ).toReject();
-      await Servers.shutdown(server);
     }
 
     {
-      const server = createServer();
       await expect(
         Servers.listen({ hostname: "localhost", port: 0, server }),
       ).toResolve();
-      // await t.doesNotReject(
-      //   Servers.listen({ hostname: "localhost", port: 0, server }),
-      //   "Does not rejects when port specified as zero OK",
-      // );
-      await Servers.shutdown(server);
     }
-
-    // t.end();
-    // });
 
     const prefPort = 4123;
     const host = "localhost";
-    const portBlocker = createServer();
-    // test.onFinish(() => portBlocker.close());
 
     const listenOptionsBlocker = {
       server: portBlocker,
@@ -63,34 +54,11 @@ describe(testCase, () => {
 
     await expect(async () => {
       const server = await Servers.startOnPreferredPort(prefPort, host);
-      // test.onFinish(() => server.close());
-      // t.ok(server, "Server returned truthy OK");
       expect(server).toBeTruthy();
       const addressInfo = server.address() as AddressInfo;
-      // t.ok(addressInfo, "AddressInfo returned truthy OK");
       expect(addressInfo).toBeTruthy();
-      // t.ok(addressInfo.port, "AddressInfo.port returned truthy OK");
       expect(addressInfo).toBeTruthy();
       expect(addressInfo.port).not.toBe(prefPort);
-      // t.doesNotEqual(
-      //   addressInfo.port,
-      //   prefPort,
-      //   "Preferred and actually allocated ports are different, therefore fallback is considered successful OK",
-      // );
     });
-
-    // await t.doesNotReject(async () => {
-    //   const server = await Servers.startOnPreferredPort(prefPort, host);
-    //   test.onFinish(() => server.close());
-    //   t.ok(server, "Server returned truthy OK");
-    //   const addressInfo = server.address() as AddressInfo;
-    //   t.ok(addressInfo, "AddressInfo returned truthy OK");
-    //   t.ok(addressInfo.port, "AddressInfo.port returned truthy OK");
-    //   t.doesNotEqual(
-    //     addressInfo.port,
-    //     prefPort,
-    //     "Preferred and actually allocated ports are different, therefore fallback is considered successful OK",
-    //   );
-    // }, "Servers.startOnPreferredPort falls back without throwing OK");
   });
 });
