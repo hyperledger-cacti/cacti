249f63193c3a3aa31237f7d7806783e376e5aac0
Youngone Lee <youngone.lee@accenture.com>
2022-05-22 18:57:17 -0500

    test(test-tooling): jestify containers.test.ts
    File Path:
    packages/cactus-test-
    tooling/src/test/
    typescript/integration/
    common/
    containers.test.ts
    
    Edit from Peter: I updated the ci.yml file as well (this became a
    requirement some time after this PR was opened & reviewed).
    
    This is a PARTIAL resolution to issue #238
    
    Signed-off-by: Youngone Lee <youngone.lee@accenture.com>
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- jest.config.js
+++ jest.config.js
@@ -107,7 +107,6 @@ module.exports = {
     `./packages/cactus-test-tooling/src/test/typescript/integration/substrate/substrate-test-ledger-multiple-concurrent.test.ts`,
     `./packages/cactus-test-tooling/src/test/typescript/integration/iroha/iroha-test-ledger/constructor-validates-options.test.ts`,
     `./packages/cactus-test-tooling/src/test/typescript/integration/rustc-container/rustc-container-target-bundler.test.ts`,
-    `./packages/cactus-test-tooling/src/test/typescript/integration/common/containers.test.ts`,
     `./packages/cactus-test-tooling/src/test/typescript/integration/besu/besu-test-ledger/constructor-validates-options.test.ts`,
     `./packages/cactus-test-plugin-htlc-eth-besu/src/test/typescript/integration/plugin-htlc-eth-besu/get-single-status-endpoint.test.ts`,
     `./packages/cactus-test-plugin-htlc-eth-besu/src/test/typescript/integration/plugin-htlc-eth-besu/get--status-endpoint-invalid.test.ts`,
