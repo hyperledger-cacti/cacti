3518b219481d9fa2d96b9acec0d55b8807ea4cd5
Krishnasuri Narayanam <knaraya3@in.ibm.com>
2022-05-23 08:09:29 +0000

    adding the test file asset_locks_contract_test.go
    Signed-off-by: Krishnasuri Narayanam <knaraya3@in.ibm.com>

--- core/network/fabric-interop-cc/interfaces/asset-mgmt/asset_locks_test.go
+++ core/network/fabric-interop-cc/interfaces/asset-mgmt/asset_locks_test.go
@@ -971,9 +971,9 @@ func TestAssetClaim(t *testing.T) {
     recipient := "Bob"
     locker := clientId
     hash := []byte("MBQGA1UEBxMNU2FuIEZyYW5jaXNjbzEPMA0GA1UECxMGY2xpZW50MSQwIgYDVQQD")
-    hashPreimage := []byte("YW5jaXNjbzEeMBwGA1UE" + "dummy")
+    hashPreimage := []byte("YW5jaXNjbzEeMBwGA1UE")
     claimInfoHTLC := &common.AssetClaimHTLC {
-        HashMechanism: common.HashMechanism_SHA512,
+        HashMechanism: common.HashMechanism_SHA256,
         HashPreimageBase64: nil,
     }
     claimInfoBytes, _ := proto.Marshal(claimInfoHTLC)
@@ -1042,7 +1042,7 @@ func TestAssetClaim(t *testing.T) {
     // Test success
     // First, lock an asset
     lockInfoHTLC := &common.AssetLockHTLC {
-        HashMechanism: common.HashMechanism_SHA512,
+        HashMechanism: common.HashMechanism_SHA256,
         HashBase64: hash,
         ExpiryTimeSecs: 0,
     }
