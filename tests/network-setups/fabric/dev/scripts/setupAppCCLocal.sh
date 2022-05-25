6fc056c123ff32a8bdbbd2fe2bb433bd3e7e91b4
sandeep.nishad1 <sandeep.nishad1@ibm.com>
2022-05-25 17:01:41 +0530

    update fabric cc setup scripts to use vendor for local deployments, removed semi-local workflows now
    Signed-off-by: sandeep.nishad1 <sandeep.nishad1@ibm.com>

--- tests/network-setups/fabric/dev/scripts/setupAppCCLocal.sh
+++ tests/network-setups/fabric/dev/scripts/setupAppCCLocal.sh
@@ -12,30 +12,8 @@ if [ -d "${CHAINCODE_PATH}/${CC_NAME}" ]; then
     echo "Deleting existing ${CC_NAME} chaincode folder to copy the latest..."
     rm -rf ${CHAINCODE_PATH}/${CC_NAME}
 fi
+
+(cd ${APP_CC_PATH} && make run-vendor)
 cp -r ${APP_CC_PATH} ${CHAINCODE_PATH}/${CC_NAME}
-if [ -f ${CHAINCODE_PATH}/${CC_NAME}/go.mod.local ]
-then
-    cp -r ${WEAVER_PATH}/common/protos-go ${CHAINCODE_PATH}/${CC_NAME}/
-    cp -r ${INTEROPCC_CHAINCODE_PATH}/fabric-interop-cc/libs ${CHAINCODE_PATH}/${CC_NAME}/
-    cp -r ${INTEROPCC_CHAINCODE_PATH}/fabric-interop-cc/interfaces ${CHAINCODE_PATH}/${CC_NAME}/
-    cd ${CHAINCODE_PATH}/${CC_NAME}/libs/assetexchange
-    mv go.mod.local go.mod
-    rm go.sum
-    go mod tidy
-    cd ../utils
-    mv go.mod.local go.mod
-    rm go.sum
-    go mod tidy
-    cd ../..
-    cd interfaces/asset-mgmt
-    mv go.mod.local go.mod
-    rm go.sum
-    go mod tidy
-    cd ../..
-    mv go.mod.local go.mod
-    rm go.sum
-    go mod tidy
-    cd ../..
-fi
 
 echo "Done."
