bc4d4599e8d6e41e09f2939127fcdb69a9434235
sandeep.nishad1 <sandeep.nishad1@ibm.com>
2022-09-02 11:52:52 +0530

    renamed networkId with security domain in iin-agent
    Signed-off-by: sandeep.nishad1 <sandeep.nishad1@ibm.com>

--- core/identity-management/iin-agent/src/protocols/localOperations.ts
+++ core/identity-management/iin-agent/src/protocols/localOperations.ts
@@ -8,12 +8,12 @@ import iin_agent_pb from '@hyperledger-labs/weaver-protos-js/identity/agent_pb';
 import { handlePromise} from '../common/utils';
 
 
-// Generates attestations on a foreign network unit's state
+// Generates attestations on a foreign security domain unit's state
 export const requestAttestation = async (attestedSecurityDomain: iin_agent_pb.AttestedSecurityDomain) => {
     console.log('requestAttestation:');
 };
 
-// Processes attestations on a foreign network unit's state received from a local IIN agent
+// Processes attestations on a foreign security domain unit's state received from a local IIN agent
 export const sendAttestation = async (attestedSecurityDomain: iin_agent_pb.AttestedSecurityDomain) => {
     console.log('sendAttestation:');
 };
--- core/identity-management/iin-agent/src/server.ts
+++ core/identity-management/iin-agent/src/server.ts
@@ -21,8 +21,8 @@ console.log('iin agent def', JSON.stringify(iin_agent_pb_grpc));
 
 //@ts-ignore
 iinAgentServer.addService(iin_agent_pb_grpc.IINAgentService, {
-    // Service for syncing foreign network unit's state from its IIN agent. Will communicate with the user/agent triggering this process and respond with an ack to the caller while the sync is occurring.
-    syncExternalState: (call: { request: iin_agent_pb.NetworkUnitIdentity }, callback: (_: any, object: ack_pb.Ack) => void) => {
+    // Service for syncing foreign security domain unit's state from its IIN agent. Will communicate with the user/agent triggering this process and respond with an ack to the caller while the sync is occurring.
+    syncExternalState: (call: { request: iin_agent_pb.SecurityDomainMemberIdentity }, callback: (_: any, object: ack_pb.Ack) => void) => {
         const ack_response = new ack_pb.Ack();
         try {
             syncExternalStateFromIINAgent(call.request);
@@ -42,8 +42,8 @@ iinAgentServer.addService(iin_agent_pb_grpc.IINAgentService, {
             callback(null, ack_response);
         }
     },
-    // Service for receiving requests for one's network unit's state from foreign IIN agents. Will communicate with the IIN agent caller and respond with an ack while the attestation is being generated.
-    requestIdentityConfiguration: (call: { request: iin_agent_pb.NetworkUnitIdentity }, callback: (_: any, object: ack_pb.Ack) => void) => {
+    // Service for receiving requests for one's security domain unit's state from foreign IIN agents. Will communicate with the IIN agent caller and respond with an ack while the attestation is being generated.
+    requestIdentityConfiguration: (call: { request: iin_agent_pb.SecurityDomainMemberIdentity }, callback: (_: any, object: ack_pb.Ack) => void) => {
         const ack_response = new ack_pb.Ack();
         try {
             requestIdentityConfiguration(call.request);
@@ -63,7 +63,7 @@ iinAgentServer.addService(iin_agent_pb_grpc.IINAgentService, {
             callback(null, ack_response);
         }
     },
-    // Service for receiving network unit states from foreign IIN agents. Will communicate with the IIN agent caller and respond with an ack while the attestation is being processed.
+    // Service for receiving security domain unit states from foreign IIN agents. Will communicate with the IIN agent caller and respond with an ack while the attestation is being processed.
     sendIdentityConfiguration: (call: { request: iin_agent_pb.AttestedMembership }, callback: (_: any, object: ack_pb.Ack) => void) => {
         const ack_response = new ack_pb.Ack();
         try {
@@ -84,7 +84,7 @@ iinAgentServer.addService(iin_agent_pb_grpc.IINAgentService, {
             callback(null, ack_response);
         }
     },
-    // Service for receiving requests for attestations on foreign network unit states from local IIN agents. Will communicate with the IIN agent caller and respond with an ack while the attestation is being generated.
+    // Service for receiving requests for attestations on foreign security domain unit states from local IIN agents. Will communicate with the IIN agent caller and respond with an ack while the attestation is being generated.
     requestAttestation: (call: { request: iin_agent_pb.AttestedSecurityDomain }, callback: (_: any, object: ack_pb.Ack) => void) => {
         const ack_response = new ack_pb.Ack();
         try {
@@ -105,7 +105,7 @@ iinAgentServer.addService(iin_agent_pb_grpc.IINAgentService, {
             callback(null, ack_response);
         }
     },
-    // Service for receiving attestations on foreign network unit states from local IIN agents. Will communicate with the IIN agent caller and respond with an ack while the attestation is being processed.
+    // Service for receiving attestations on foreign security domain unit states from local IIN agents. Will communicate with the IIN agent caller and respond with an ack while the attestation is being processed.
     sendAttestation: (call: { request: iin_agent_pb.AttestedSecurityDomain }, callback: (_: any, object: ack_pb.Ack) => void) => {
         const ack_response = new ack_pb.Ack();
         try {
