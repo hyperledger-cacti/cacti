3236903b209c67aa4827aa044911afbb4413edfd
sandeep.nishad1 <sandeep.nishad1@ibm.com>
2022-10-04 15:09:14 +0530

    initialize ledger on start of agent
    Signed-off-by: sandeep.nishad1 <sandeep.nishad1@ibm.com>

--- core/identity-management/iin-agent/src/server.ts
+++ core/identity-management/iin-agent/src/server.ts
@@ -14,6 +14,7 @@ import { Certificate } from '@fidm/x509';
 import * as path from 'path';
 import { syncExternalStateFromIINAgent, requestIdentityConfiguration, sendIdentityConfiguration } from './protocols/externalOperations';
 import { requestAttestation, sendAttestation } from './protocols/localOperations';
+import { getLedgerBase } from './common/utils'
 
 
 const iinAgentServer = new Server();
