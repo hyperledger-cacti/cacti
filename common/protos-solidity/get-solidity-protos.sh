6aa53815c5ce6afc134a67d548242a54799693f3
micky <chanmickyyun@gmail.com>
2022-11-02 11:43:07 +0100

    add .gitignore for protobuf contract besu
    Signed-off-by: micky <chanmickyyun@gmail.com>

--- common/protos-solidity/get-solidity-protos.sh
+++ common/protos-solidity/get-solidity-protos.sh
@@ -13,4 +13,4 @@ fi
 cp ${WEAVER_ROOT}/common/protos/common/asset_locks.proto ${PROTOS_SOLIDITY_DIR}/common/asset_locks.proto
 sed -i '3d' ${PROTOS_SOLIDITY_DIR}/common/asset_locks.proto
 
-protoc --plugin ${BESU_PROTOSDIR}/bin/protoc-gen-sol --sol_out ${PROTOS_SOLIDITY_DIR} --proto_path ${PROTOS_SOLIDITY_DIR}/common/ asset_locks.proto
+protoc --plugin ${BESU_PROTOSDIR}/bin/protoc-gen-sol --sol_out ${PROTOS_SOLIDITY_DIR} --proto_path ${PROTOS_SOLIDITY_DIR}/common/asset_locks.proto
