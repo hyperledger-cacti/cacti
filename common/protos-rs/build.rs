0ddbba487b310948f9a83e9efe60777d735cfce0
sandeep.nishad1 <sandeep.nishad1@ibm.com>
2022-11-23 14:37:07 +0530

    restructure protos-rs folder
    Signed-off-by: sandeep.nishad1 <sandeep.nishad1@ibm.com>

--- common/protos-rs/build.rs
+++ common/protos-rs/build.rs
@@ -1,18 +1,18 @@
 use std::env;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env::set_var("OUT_DIR", "./src/generated/");
+    env::set_var("OUT_DIR", "./pkg/src/generated/");
     tonic_build::configure()
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
         .compile(
             &[
-                "./protos/relay/datatransfer.proto",
-                "./protos/relay/events.proto",
-                "./protos/networks/networks.proto",
-                "./protos/driver/driver.proto",
+                "../protos/relay/datatransfer.proto",
+                "../protos/relay/events.proto",
+                "../protos/networks/networks.proto",
+                "../protos/driver/driver.proto",
             ],
             &[
-                "./protos/",
+                "../protos/",
             ],
         )?;
     Ok(())
---
+++ common/protos-rs/pkg/README.md
@@ -0,0 +1,5 @@
+# Weaver Compiled Protos for Rust
+
+This package contains compiled the gRPC service and protocol buffer rust files for the relay, drivers, other interoperation modules, and common structures used by several Weaver components.
+
+Read more about Weaver protos here: [RFC Formats](../../../rfcs/formats/README.md)
