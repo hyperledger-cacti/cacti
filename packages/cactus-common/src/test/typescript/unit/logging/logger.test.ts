0d1446339527f0bde9e3082ffdba2e8f0cc914fe
Arnab Nandi <arnabnandi2002@gmail.com>
2023-05-14 23:55:25 +0530

    fixe1

--- packages/cactus-common/src/test/typescript/unit/logging/logger.test.ts
+++ packages/cactus-common/src/test/typescript/unit/logging/logger.test.ts
@@ -36,7 +36,7 @@ test.skip("Logger#debug/error writes to stdout/stderr", async (t: Test) => {
       const timeoutMsg = "Timed out waiting for marker to appear on stdout";
       const timerId = setTimeout(() => reject(new Error(timeoutMsg)), 5000);
 
-      stdOutDataHandler = (data: string) => {
+      stdOutDataHandler = (data: Buffer) => {
         const msg = data.toString("utf-8");
         aggregateStdOut = aggregateStdOut.concat(msg);
         if (msg.includes(marker)) {
@@ -45,7 +45,7 @@ test.skip("Logger#debug/error writes to stdout/stderr", async (t: Test) => {
         }
       };
 
-      process.stdout.on("data", stdOutDataHandler);
+      process.stdout.on("data", stdOutDataHandler );
 
       // send the log now that we have hooked into the stream waiting for the marker to appear
       log.info(marker);
@@ -56,7 +56,7 @@ test.skip("Logger#debug/error writes to stdout/stderr", async (t: Test) => {
     didNotThrow = false;
   }
 
-  process.stdout.off("data", stdOutDataHandler as (data: string) => void);
+  process.stdout.off("data", stdOutDataHandler as (data: Buffer) => void);
   t.comment(`Aggregate std out messages: ${aggregateStdOut}`);
   t.true(didNotThrow, "Marker appeared on stdout on time OK");
 
