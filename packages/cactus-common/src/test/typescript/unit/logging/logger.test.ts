bd5333cd9e20162873079d5e817e708f84268afa
Arnab Nandi <97181728+arnabnandikgp@users.noreply.github.com>
2023-05-22 18:54:43 +0530

    Update logger.test.ts
    Signed-off-by: Arnab Nandi <97181728+arnabnandikgp@users.noreply.github.com>

--- packages/cactus-common/src/test/typescript/unit/logging/logger.test.ts
+++ packages/cactus-common/src/test/typescript/unit/logging/logger.test.ts
@@ -18,7 +18,7 @@ test.skip("Logger#debug/error writes to stdout/stderr", async (t: Test) => {
 
   // wait for the marker to appear on stdout OR crash with timeout if it never comes
   let aggregateStdOut = "";
-  let stdOutDataHandler : ((...args: any[]) => void);
+  let stdOutDataHandler;
   let didNotThrow: boolean;
   try {
     // hook up to the stdout data stream and wrap it in a promise that can be awaited
@@ -36,7 +36,7 @@ test.skip("Logger#debug/error writes to stdout/stderr", async (t: Test) => {
       const timeoutMsg = "Timed out waiting for marker to appear on stdout";
       const timerId = setTimeout(() => reject(new Error(timeoutMsg)), 5000);
 
-      stdOutDataHandler = (data: Buffer) => {
+      stdOutDataHandler : ((...args: any[]) => void) = (data: Buffer) => {
         const msg = data.toString("utf-8");
         aggregateStdOut = aggregateStdOut.concat(msg);
         if (msg.includes(marker)) {
