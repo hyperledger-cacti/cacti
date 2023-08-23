1e53b2d1c95dae915730906882a929393b985bf8
outsidethecode <outsidethecode@gmail.com>
2023-08-23 12:20:05 +0100

    Added the required functions to call back the gateway after the asset is locked.

--- weaver/core/drivers/fabric-driver/server/satp.ts
+++ weaver/core/drivers/fabric-driver/server/satp.ts
@@ -1,17 +1,8 @@
-import ack_pb from '@hyperledger/cacti-weaver-protos-js/common/ack_pb';
-import eventsPb from '@hyperledger/cacti-weaver-protos-js/common/events_pb';
-import events_grpc_pb from '@hyperledger/cacti-weaver-protos-js/relay/events_grpc_pb';
-import queryPb from '@hyperledger/cacti-weaver-protos-js/common/query_pb';
-import { InteroperableHelper } from '@hyperledger/cacti-weaver-sdk-fabric';
-import { getDriverKeyCert } from "./walletSetup";
-import { DBConnector, DBKeyNotFoundError, LevelDBConnector } from "./dbConnector";
-import { checkIfArraysAreEqual, handlePromise, relayCallback } from "./utils";
-import { registerListenerForEventSubscription, unregisterListenerForEventSubscription } from "./listener";
-import { getNetworkGateway } from "./fabric-code";
-import { Gateway, Network, Contract } from "fabric-network";
-import state_pb from '@hyperledger/cacti-weaver-protos-js/common/state_pb';
+import satp_pb from '@hyperledger/cacti-weaver-protos-js/relay/satp_pb';
+import satp_grpc_pb from '@hyperledger/cacti-weaver-protos-js/relay/satp_grpc_pb';
 import driverPb from '@hyperledger/cacti-weaver-protos-js/driver/driver_pb';
 import logger from './logger';
+import { credentials } from '@grpc/grpc-js';
 
 import { getNetworkConfig } from './helpers/helpers'
 import { AssetManager, HashFunctions } from '@hyperledger/cacti-weaver-sdk-fabric'
@@ -32,12 +23,19 @@ async function performLockHelper(
     networkName: string
 ): Promise<any> {
 
+    let performLockRequest2 = {};
+    performLockRequest2['target-network'] = 'network1';
+    performLockRequest2['hashBase64'] = 'ivHErp1x4bJDKuRo6L5bApO/DdoyD/dG0mAZrzLZEIs=';
+    performLockRequest2['timeout-duration'] = '3600';
+    performLockRequest2['locker'] = 'alice';
+    performLockRequest2['recipient'] = 'bob';
+    performLockRequest2['param'] = 'bond01:a03';
 
     // Locker and Recipient
-    const locker = performLockRequest['locker'];
-    const recipient = performLockRequest['recipient'];
-    let hashFn = performLockRequest['hash_fn'];
-    let hashBase64 = performLockRequest['hashBase64'];
+    const locker = performLockRequest2['locker'];
+    const recipient = performLockRequest2['recipient'];
+    let hashFn = performLockRequest2['hash_fn'];
+    let hashBase64 = performLockRequest2['hashBase64'];
 
     // Hash
     let hash: HashFunctions.Hash
@@ -57,22 +55,22 @@ async function performLockHelper(
     // Timeout
     var timeout = 0, timeout2 = 0;
     const currTime = Math.floor(Date.now() / 1000);
-    if (performLockRequest['timeout-epoch']) {
-        let duration = performLockRequest['timeout-epoch'] - currTime
-        timeout = performLockRequest['timeout-epoch']
-        timeout2 = performLockRequest['timeout-epoch'] + duration
+    if (performLockRequest2['timeout-epoch']) {
+        let duration = performLockRequest2['timeout-epoch'] - currTime
+        timeout = performLockRequest2['timeout-epoch']
+        timeout2 = performLockRequest2['timeout-epoch'] + duration
     }
-    else if (performLockRequest['timeout-duration']) {
-        timeout = currTime + performLockRequest['timeout-duration']
-        timeout2 = currTime + 2 * performLockRequest['timeout-duration']
+    else if (performLockRequest2['timeout-duration']) {
+        timeout = currTime + performLockRequest2['timeout-duration']
+        timeout2 = currTime + 2 * performLockRequest2['timeout-duration']
     }
 
-    const params = performLockRequest['param'].split(':')
+    const params = performLockRequest2['param'].split(':')
+    const netConfig = getNetworkConfig(performLockRequest2['target-network'])
 
-    const netConfig = getNetworkConfig(performLockRequest['target-network'])
     if (!netConfig.connProfilePath || !netConfig.channelName || !netConfig.chaincode) {
         console.error(
-            `Please use a valid --target-network. No valid environment found for ${performLockRequest['target-network']} `
+            `Please use a valid --target-network. No valid environment found for ${performLockRequest2['target-network']} `
         )
         return
     }
@@ -81,12 +79,14 @@ async function performLockHelper(
         channel: netConfig.channelName,
         contractName: netConfig.chaincode,
         connProfilePath: netConfig.connProfilePath,
-        networkName: performLockRequest['target-network'],
+        networkName: performLockRequest2['target-network'],
         mspId: netConfig.mspId,
         userString: locker
     })
 
+    logger.info(`network details: ${network}`);
     const lockerId = await network.wallet.get(locker)
+
     const lockerCert = Buffer.from((lockerId).credentials.certificate).toString('base64')
 
     const recipientId = await network.wallet.get(recipient)
@@ -94,7 +94,7 @@ async function performLockHelper(
 
     var funcToCall, asset
 
-    if (performLockRequest['fungible']) {
+    if (performLockRequest2['fungible']) {
         funcToCall = AssetManager.createFungibleHTLC
         asset = 'Fungible Asset'
     } else {
@@ -106,89 +106,69 @@ async function performLockHelper(
 
     try {
         console.info(`Trying ${asset} Lock: ${params[0]}, ${params[1]} by ${locker} for ${recipient}`)
-        const res = await funcToCall(network.contract,
-            params[0],
-            params[1],
-            recipientCert,
-            hash,
-            timeout,
-            null)
-        if (!res.result) {
-            throw new Error()
-        }
-        console.info(`${asset} Locked with Contract Id: ${res.result}, preimage: ${res.hash.getPreimage()}, hashvalue: ${res.hash.getSerializedHashBase64()}`)
+        // const res = await funcToCall(network.contract,
+        //     params[0],
+        //     params[1],
+        //     recipientCert,
+        //     hash,
+        //     timeout,
+        //     null)
+        // if (!res.result) {
+        //     throw new Error()
+        // }
+        // console.info(`${asset} Locked with Contract Id: ${res.result}, preimage: ${res.hash.getPreimage()}, hashvalue: ${res.hash.getSerializedHashBase64()}`)
         console.info('Asset Exchange: Lock Complete.')
+
     } catch (error) {
-        console.error(`Could not Lock ${asset} in ${performLockRequest['target-network']}`)
+        console.error(`Could not Lock ${asset} in ${performLockRequest2['target-network']}`)
     }
 
-    // await new Promise(f => setTimeout(f, performLockRequest['timeout-duration'] * 1000 + 3000));
+    // await new Promise(f => setTimeout(f, performLockRequest2['timeout-duration'] * 1000 + 3000));
 
     await network.gateway.disconnect()
-    console.log('Gateways disconnected.')
+    logger.info('Gateways disconnected.')
 
+    const client = getRelayClientForAssetStatusResponse();
+    const request = new satp_pb.SendAssetStatusRequest();
+    request.setSessionId(performLockRequest.getSessionId());
+    request.setStatus("Locked");
+    client.sendAssetStatus(request, relayCallback);
+}
 
-    // const viewPayload: state_pb.ViewPayload = performLockRequest.getViewPayload();
-    // const ctx: eventsPb.ContractTransaction = writeExternalStateMessage.getCtx();
-    // const keyCert = await getDriverKeyCert();
+function getRelayClientForAssetStatusResponse() {
+    let client: satp_grpc_pb.SATPClient;
+    if (process.env.RELAY_TLS === 'true') {
+        if (!process.env.RELAY_TLSCA_CERT_PATH || process.env.RELAY_TLSCA_CERT_PATH == "") {
+            client = new satp_grpc_pb.SATPClient(
+                process.env.RELAY_ENDPOINT,
+                credentials.createSsl()
+            );
+        } else {
+            if (!(process.env.RELAY_TLSCA_CERT_PATH && fs.existsSync(process.env.RELAY_TLSCA_CERT_PATH))) {
+                throw new Error("Missing or invalid RELAY_TLSCA_CERT_PATH: " + process.env.RELAY_TLSCA_CERT_PATH);
+            }
+            const rootCert = fs.readFileSync(process.env.RELAY_TLSCA_CERT_PATH);
+            client = new satp_grpc_pb.SATPClient(
+                process.env.RELAY_ENDPOINT,
+                credentials.createSsl(rootCert)
+            );
+        }
+    } else {
+        client = new satp_grpc_pb.SATPClient(
+            process.env.RELAY_ENDPOINT,
+            credentials.createInsecure()
+        );
+    }
+    return client;
+}
 
-    // const requestId: string = viewPayload.getRequestId();
-    // if (!viewPayload.getError()) {
-
-    //     let interopArgIndices = [], viewsSerializedBase64 = [], addresses = [], viewContentsBase64 = [];
-    //     const view: state_pb.View = viewPayload.getView();
-
-    //     const result = InteroperableHelper.getResponseDataFromView(view, keyCert.key.toBytes());
-    //     if (result.contents) {
-    //         viewContentsBase64.push(result.contents);
-    //     } else {
-    //         viewContentsBase64.push([]);
-    //     }
-
-    //     interopArgIndices.push(ctx.getReplaceArgIndex());
-    //     addresses.push(result.viewAddress);
-    //     viewsSerializedBase64.push(Buffer.from(viewPayload.getView().serializeBinary()).toString("base64"));
-
-    //     let ccArgsB64 = ctx.getArgsList();
-    //     let ccArgsStr = [];
-    //     for (const ccArgB64 of ccArgsB64) {
-    //         ccArgsStr.push(Buffer.from(ccArgB64).toString('utf8'));
-    //     }
-
-    //     let gateway: Gateway = await getNetworkGateway(networkName);
-    //     const network: Network = await gateway.getNetwork(ctx.getLedgerId());
-    //     const interopContract: Contract = network.getContract(process.env.INTEROP_CHAINCODE ? process.env.INTEROP_CHAINCODE : 'interop');
-
-    //     const endorsingOrgs = ctx.getMembersList();
-    //     const invokeObject = {
-    //         channel: ctx.getLedgerId(),
-    //         ccFunc: ctx.getFunc(),
-    //         ccArgs: ccArgsStr,
-    //         contractName: ctx.getContractId()
-    //     }
-    //     logger.info(`writing external state to contract: ${ctx.getContractId()} with function: ${ctx.getFunc()}, and args: ${invokeObject.ccArgs} on channel: ${ctx.getLedgerId()}`);
-
-    //     const [ response, responseError ] = await handlePromise(InteroperableHelper.submitTransactionWithRemoteViews(
-    //         interopContract,
-    //         invokeObject,
-    //         interopArgIndices,
-    //         addresses,
-    //         viewsSerializedBase64,
-    //         viewContentsBase64,
-    //         endorsingOrgs
-    //     ));
-    //     if (responseError) {
-    //         logger.error(`Failed writing to the ledger with error: ${responseError}`);
-    //         gateway.disconnect();
-    //         throw responseError;
-    //     }
-    //     logger.debug(`write successful`);
-    //     gateway.disconnect();
-    // } else {
-    //     const errorString: string = `erroneous viewPayload identified in WriteExternalState processing`;
-    //     logger.error(`error viewPayload.getError(): ${JSON.stringify(viewPayload.getError())}`);
-    //     throw new Error(errorString);
-    // }
+// handle callback
+function relayCallback(err: any, response: any) {
+    if (response) {
+        logger.info(`Relay Callback Response: ${JSON.stringify(response.toObject())}`);
+    } else if (err) {
+        logger.error(`Relay Callback Error: ${err}`);
+    }
 }
 
 export {
