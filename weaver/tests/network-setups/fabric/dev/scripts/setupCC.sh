dceea5c64be2747975c71c683ea44b7454e937fb
VRamakrishna <vramakr2@in.ibm.com>
2023-09-11 19:46:30 +0000

    build(weaver-testnet): refactored Fabric sample network launch script
    Signed-off-by: VRamakrishna <vramakr2@in.ibm.com>

--- weaver/tests/network-setups/fabric/dev/scripts/setupCC.sh
+++ weaver/tests/network-setups/fabric/dev/scripts/setupCC.sh
@@ -6,7 +6,7 @@ CHAINCODE_PATH=$PWD/../shared/chaincode
 rm -rf $CHAINCODE_PATH/interop
 
 # interop cc module
-INTEROPCC_MOD=github.com/hyperledger/cacti/weaver/core/network/fabric-interop-cc/contracts/interop/v2
+INTEROPCC_MOD=github.com/hyperledger/cacti/weaver/core/network/fabric-interop-cc/contracts/interop/${CACTI_VERSION%%"."*}
 
 # custom gopath for convenient downloading
 mkdir $TMP_PATH
