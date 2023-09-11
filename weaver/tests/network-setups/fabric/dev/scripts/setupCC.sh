48967a2c7dc7f8959b483078d2ac9d0b3b846add
VRamakrishna <vramakr2@in.ibm.com>
2023-09-11 13:36:08 +0000

    build(weaver-testnet): refactored script and reverted dependency version change
    Minor refactoring in Fabric testnet launch script.
    Reverting unnecessary version downgrade in Weaver relay Cargo dependencies.
    
    Signed-off-by: VRamakrishna <vramakr2@in.ibm.com>

--- weaver/tests/network-setups/fabric/dev/scripts/setupCC.sh
+++ weaver/tests/network-setups/fabric/dev/scripts/setupCC.sh
@@ -1,6 +1,6 @@
 directory=$(dirname $0)
 
-CACTI_VERSION=v2@v2.0.0-alpha.1
+CACTI_VERSION=v2.0.0-alpha.1
 TMP_PATH=$PWD/../shared/tmp
 CHAINCODE_PATH=$PWD/../shared/chaincode
 rm -rf $CHAINCODE_PATH/interop
@@ -15,7 +15,7 @@ export GOPATH=$TMP_PATH
 
 # Download interopcc and copy it into correct folder
 go install "${INTEROPCC_MOD}@latest"
-cp -r $TMP_PATH/pkg/mod/github.com/hyperledger/cacti/weaver/core/network/fabric-interop-cc/contracts/interop/$CACTI_VERSION $CHAINCODE_PATH/interop
+cp -r $TMP_PATH/pkg/mod/$INTEROPCC_MOD@$CACTI_VERSION $CHAINCODE_PATH/interop
 chmod -R +w $CHAINCODE_PATH/interop
 
 # Clean tmp and Undo gopath
