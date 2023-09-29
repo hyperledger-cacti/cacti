6cd1fab961638366f66e9852f4a8fb50ad140483
Sandeep Nishad <sandeep.nishad1@ibm.com>
2023-09-29 14:30:34 +0530

    fix(relay): move entrypoint script shebang to 1st line
    Signed-off-by: Sandeep Nishad <sandeep.nishad1@ibm.com>

--- weaver/core/relay/docker/entrypoint-driver.sh
+++ weaver/core/relay/docker/entrypoint-driver.sh
@@ -1,8 +1,8 @@
+#!/bin/sh
+
 # Copyright IBM Corp. All Rights Reserved.
 #
 # SPDX-License-Identifier: Apache-2.0
-
-#!/bin/sh
 
 DRIVER_CONFIG_TEMPLATE=${DRIVER_CONFIG_TEMPLATE:-$(pwd)/config/driver.template.toml}
 
