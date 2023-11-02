cca89dd567aaec3763c2facd900dfc20019940e5
Peter Somogyvari <peter.somogyvari@accenture.com>
2023-11-02 13:13:01 -0700

    feat(indy-test-ledger): add helper class for indy ledger
    - Add `IndyTestLedger` class to setup test indy ledger for testing purposes.
    - Add functional tests for new test ledger class.
    - Minor fix in cleaning up sample indy test ledger.
    
    Peter's changes:
    1. Rebased onto upstream/main as of 2024-01-21 which was a bit of a
    challenge on account of this  being quite old (110 commits behind)
    2. Meaning that there's a chance that I messed up some of the conflict
    resolutions in a way that is sub-optimal, please forgive if this is the
    case!
    
    Depends on #2861
    
    Co-authored-by: Peter Somogyvari <peter.somogyvari@accenture.com>
    
    Signed-off-by: Michal Bajer <michal.bajer@fujitsu.com>
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- tools/docker/indy-all-in-one/script-cleanup.sh
+++ tools/docker/indy-all-in-one/script-cleanup.sh
@@ -4,7 +4,7 @@ echo "# Stop running indy"
 docker compose down
 
 echo "# Clean Indy data"
-rm -fr /tmp/indy-all-in-one/*
+rm -fr /tmp/indy-all-in-one
 
 echo "# Remove cacti wallets from '~/.afj' (Aries JS Framework)"
 sudo find ~/.afj/data/wallet/ -iname '*cacti*' -exec rm -fr {} \;
