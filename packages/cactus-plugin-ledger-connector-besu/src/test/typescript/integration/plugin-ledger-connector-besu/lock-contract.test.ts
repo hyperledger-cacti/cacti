9a96b8e6158995c565737a4cccccd91312604976
Peter Somogyvari <peter.somogyvari@accenture.com>
2023-12-07 17:51:47 -0800

    test(connector-besu): migrated lock-contract.test.ts to Jest
    The relative file path of the test case within the project:
    packages/cactus-plugin-ledger-connector-besu/src/test/typescript/
    integration/plugin-ledger-connector-besu/lock-contract.test.ts
    
    No functional change of any kind, just refactored the assertions
    to match how they were before but now executed by Jest.
    
    [skip ci]
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-plugin-ledger-connector-besu/src/test/typescript/integration/plugin-ledger-connector-besu/lock-contract.test.ts
+++ packages/cactus-plugin-ledger-connector-besu/src/test/typescript/integration/plugin-ledger-connector-besu/lock-contract.test.ts
@@ -1,100 +1,115 @@
-import test, { Test } from "tape";
+import "jest-extended";
 import { v4 as uuidv4 } from "uuid";
+import Web3 from "web3";
+import { Account } from "web3-core";
+
 import { PluginRegistry } from "@hyperledger/cactus-core";
+import { PluginKeychainMemory } from "@hyperledger/cactus-plugin-keychain-memory";
+import { BesuTestLedger } from "@hyperledger/cactus-test-tooling";
+import { LogLevelDesc } from "@hyperledger/cactus-common";
+import { PluginImportType } from "@hyperledger/cactus-core-api";
+
+import LockAssetContractJson from "../../../solidity/hello-world-contract/LockAsset.json";
+
 import {
   EthContractInvocationType,
   Web3SigningCredentialType,
   PluginLedgerConnectorBesu,
   PluginFactoryLedgerConnector,
-  //Web3SigningCredentialCactusKeychainRef,
   ReceiptType,
 } from "../../../../main/typescript/public-api";
-import { PluginKeychainMemory } from "@hyperledger/cactus-plugin-keychain-memory";
-import { BesuTestLedger } from "@hyperledger/cactus-test-tooling";
-import { LoggerProvider, LogLevelDesc } from "@hyperledger/cactus-common";
-import LockAssetContractJson from "../../../solidity/hello-world-contract/LockAsset.json";
-import Web3 from "web3";
-import { PluginImportType } from "@hyperledger/cactus-core-api";
-const level = "INFO";
-const label = "lock-contact test";
-const log = LoggerProvider.getOrCreate({ level, label });
-test("deploys contract via .json file", async (t: Test) => {
-  const logLevel: LogLevelDesc = "TRACE";
-  const besuTestLedger = new BesuTestLedger();
-  await besuTestLedger.start();
 
-  test.onFinish(async () => {
+const logLevel: LogLevelDesc = "INFO";
+
+describe("PluginLedgerConnectorBesu", () => {
+  const besuTestLedger = new BesuTestLedger();
+  let besuKeyPair: { privateKey: string };
+  let firstHighNetWorthAccount: string;
+  let keychainPlugin: PluginKeychainMemory;
+  let testEthAccount: Account;
+  let connector: PluginLedgerConnectorBesu;
+
+  beforeAll(async () => {
+    await besuTestLedger.start();
+
+    /**
+     * Constant defining the standard 'dev' Besu genesis.json contents.
+     *
+     * @see https://github.com/hyperledger/besu/blob/1.5.1/config/src/main/resources/dev.json
+     */
+    firstHighNetWorthAccount = besuTestLedger.getGenesisAccountPubKey();
+    besuKeyPair = {
+      privateKey: besuTestLedger.getGenesisAccountPrivKey(),
+    };
+  });
+
+  afterAll(async () => {
     await besuTestLedger.stop();
     await besuTestLedger.destroy();
   });
 
-  const rpcApiHttpHost = await besuTestLedger.getRpcApiHttpHost();
-  const rpcApiWsHost = await besuTestLedger.getRpcApiWsHost();
-
-  /**
-   * Constant defining the standard 'dev' Besu genesis.json contents.
-   *
-   * @see https://github.com/hyperledger/besu/blob/1.5.1/config/src/main/resources/dev.json
-   */
-  const firstHighNetWorthAccount = besuTestLedger.getGenesisAccountPubKey();
-  const besuKeyPair = {
-    privateKey: besuTestLedger.getGenesisAccountPrivKey(),
-  };
-  const contractName = "LockAsset";
-
-  const web3 = new Web3(rpcApiHttpHost);
-  const testEthAccount = web3.eth.accounts.create(uuidv4());
-
-  const keychainEntryKey = uuidv4();
-  const keychainEntryValue = testEthAccount.privateKey;
-  const keychainPlugin = new PluginKeychainMemory({
-    instanceId: uuidv4(),
-    keychainId: uuidv4(),
-    // pre-provision keychain with mock backend holding the private key of the
-    // test account that we'll reference while sending requests with the
-    // signing credential pointing to this keychain entry.
-    backend: new Map([[keychainEntryKey, keychainEntryValue]]),
-    logLevel,
-  });
-  keychainPlugin.set(
-    LockAssetContractJson.contractName,
-    JSON.stringify(LockAssetContractJson),
-  );
-  const factory = new PluginFactoryLedgerConnector({
-    pluginImportType: PluginImportType.Local,
-  });
-  const connector: PluginLedgerConnectorBesu = await factory.create({
-    rpcApiHttpHost,
-    rpcApiWsHost,
-    instanceId: uuidv4(),
-    pluginRegistry: new PluginRegistry({ plugins: [keychainPlugin] }),
+  afterAll(async () => {
+    await connector.shutdown();
   });
 
-  await connector.transact({
-    web3SigningCredential: {
-      ethAccount: firstHighNetWorthAccount,
-      secret: besuKeyPair.privateKey,
-      type: Web3SigningCredentialType.PrivateKeyHex,
-    },
-    consistencyStrategy: {
-      blockConfirmations: 0,
-      receiptType: ReceiptType.NodeTxPoolAck,
-    },
-    transactionConfig: {
-      from: firstHighNetWorthAccount,
-      to: testEthAccount.address,
-      value: 10e9,
-      gas: 1000000,
-    },
+  it("creates test account from scratch with seed money", async () => {
+    const rpcApiHttpHost = await besuTestLedger.getRpcApiHttpHost();
+    const rpcApiWsHost = await besuTestLedger.getRpcApiWsHost();
+
+    const web3 = new Web3(rpcApiHttpHost);
+    testEthAccount = web3.eth.accounts.create(uuidv4());
+
+    const keychainEntryKey = uuidv4();
+    const keychainEntryValue = testEthAccount.privateKey;
+    keychainPlugin = new PluginKeychainMemory({
+      instanceId: uuidv4(),
+      keychainId: uuidv4(),
+      // pre-provision keychain with mock backend holding the private key of the
+      // test account that we'll reference while sending requests with the
+      // signing credential pointing to this keychain entry.
+      backend: new Map([[keychainEntryKey, keychainEntryValue]]),
+      logLevel,
+    });
+    keychainPlugin.set(
+      LockAssetContractJson.contractName,
+      JSON.stringify(LockAssetContractJson),
+    );
+    const factory = new PluginFactoryLedgerConnector({
+      pluginImportType: PluginImportType.Local,
+    });
+    connector = await factory.create({
+      rpcApiHttpHost,
+      rpcApiWsHost,
+      instanceId: uuidv4(),
+      logLevel,
+      pluginRegistry: new PluginRegistry({ plugins: [keychainPlugin] }),
+    });
+
+    await connector.transact({
+      web3SigningCredential: {
+        ethAccount: firstHighNetWorthAccount,
+        secret: besuKeyPair.privateKey,
+        type: Web3SigningCredentialType.PrivateKeyHex,
+      },
+      consistencyStrategy: {
+        blockConfirmations: 0,
+        receiptType: ReceiptType.NodeTxPoolAck,
+      },
+      transactionConfig: {
+        from: firstHighNetWorthAccount,
+        to: testEthAccount.address,
+        value: 10e9,
+        gas: 1000000,
+      },
+    });
+
+    const balance = await web3.eth.getBalance(testEthAccount.address);
+    expect(balance).toBeTruthy();
+
+    expect(parseInt(balance, 10)).toEqual(10e9);
   });
 
-  const balance = await web3.eth.getBalance(testEthAccount.address);
-  t.ok(balance, "Retrieved balance of test account OK");
-  t.equals(parseInt(balance, 10), 10e9, "Balance of test account is OK");
-
-  let contractAddress: string;
-
-  test("deploys contract via .json file", async (t2: Test) => {
+  it("deploys contract via .json file, verifies lock/unlock ops", async () => {
     const deployOut = await connector.deployContract({
       keychainId: keychainPlugin.getKeychainId(),
       contractName: LockAssetContractJson.contractName,
@@ -108,24 +123,16 @@ test("deploys contract via .json file", async (t: Test) => {
       bytecode: LockAssetContractJson.bytecode,
       gas: 1000000,
     });
-    t2.ok(deployOut, "deployContract() output is truthy OK");
-    t2.ok(
-      deployOut.transactionReceipt,
-      "deployContract() output.transactionReceipt is truthy OK",
-    );
-    t2.ok(
-      deployOut.transactionReceipt.contractAddress,
-      "deployContract() output.transactionReceipt.contractAddress is truthy OK",
-    );
+    expect(deployOut).toBeTruthy();
+    expect(deployOut.transactionReceipt).toBeTruthy();
+    expect(deployOut.transactionReceipt.contractAddress).toBeTruthy();
 
-    contractAddress = deployOut.transactionReceipt.contractAddress as string;
-    t2.ok(
-      typeof contractAddress === "string",
-      "contractAddress typeof string OK",
-    );
+    const contractAddress = deployOut.transactionReceipt.contractAddress;
+    expect(contractAddress).toBeString();
+    expect(contractAddress).not.toBeEmpty();
 
     const { success: createRes } = await connector.invokeContract({
-      contractName,
+      contractName: LockAssetContractJson.contractName,
       keychainId: keychainPlugin.getKeychainId(),
       invocationType: EthContractInvocationType.Send,
       methodName: "createAsset",
@@ -137,11 +144,11 @@ test("deploys contract via .json file", async (t: Test) => {
       },
       gas: 1000000,
     });
-    t2.ok(createRes, "create Asset() output is truthy");
-    t2.equals(createRes, true, "create Asset Ok");
-    log.warn("create ok");
+    expect(createRes).toBeTruthy();
+    expect(createRes).toBeTrue();
+
     const { success: lockRes } = await connector.invokeContract({
-      contractName,
+      contractName: LockAssetContractJson.contractName,
       keychainId: keychainPlugin.getKeychainId(),
       invocationType: EthContractInvocationType.Send,
       methodName: "lockAsset",
@@ -153,11 +160,12 @@ test("deploys contract via .json file", async (t: Test) => {
       },
       gas: 1000000,
     });
-    log.warn("checking lock res");
-    t2.ok(lockRes, "lock Asset() output is truthy");
-    t2.equals(lockRes, true, "lock Asset Ok");
+
+    expect(lockRes).toBeTruthy();
+    expect(lockRes).toEqual(true);
+
     const { success: unLockRes } = await connector.invokeContract({
-      contractName,
+      contractName: LockAssetContractJson.contractName,
       keychainId: keychainPlugin.getKeychainId(),
       invocationType: EthContractInvocationType.Send,
       methodName: "unLockAsset",
@@ -169,10 +177,12 @@ test("deploys contract via .json file", async (t: Test) => {
       },
       gas: 1000000,
     });
-    t2.ok(unLockRes, "unlock Asset() output is truthy");
-    t2.equals(unLockRes, true, "unlock Asset Ok");
+
+    expect(unLockRes).toBeTruthy();
+    expect(unLockRes).toBeTrue();
+
     const { success: lockRes2 } = await connector.invokeContract({
-      contractName,
+      contractName: LockAssetContractJson.contractName,
       keychainId: keychainPlugin.getKeychainId(),
       invocationType: EthContractInvocationType.Send,
       methodName: "lockAsset",
@@ -184,11 +194,12 @@ test("deploys contract via .json file", async (t: Test) => {
       },
       gas: 1000000,
     });
-    t2.ok(lockRes2, "lock Asset() again output is truthy");
-    t2.equals(lockRes2, true, "lock Asset again Ok");
-    log.warn("asset is locked again");
+
+    expect(lockRes2).toBeTruthy();
+    expect(lockRes2).toBeTrue();
+
     const { success: deleteRes } = await connector.invokeContract({
-      contractName,
+      contractName: LockAssetContractJson.contractName,
       keychainId: keychainPlugin.getKeychainId(),
       invocationType: EthContractInvocationType.Send,
       methodName: "deleteAsset",
@@ -200,9 +211,8 @@ test("deploys contract via .json file", async (t: Test) => {
       },
       gas: 1000000,
     });
-    console.log(deleteRes);
-    t2.ok(deleteRes, "delete Asset() output is truthy");
-    t2.equals(deleteRes, true, "delete Asset Ok");
+
+    expect(deleteRes).toBeTruthy();
+    expect(deleteRes).toBeTrue();
   });
-  t.end();
 });
