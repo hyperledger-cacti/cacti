7a1cc66237656b2132eda9dca9b40fa57bbd5031
André Augusto <andre.augusto@tecnico.ulisboa.pt>
2024-03-04 18:37:03 +0000

    fix(satp-hermes): fix build bugs and hanging tests
    Co-authored-by: André Augusto <andre.augusto@tecnico.ulisboa.pt>
    Co-authored-by: Peter Somogyvari <peter.somogyvari@accenture.com>
    Co-authored-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    test(satp-hermes): fix hanging tests
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    refactor(SATP-Hermes): fix cspell and lint
    
    Signed-off-by: André Augusto <andre.augusto@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/knex/knexfile-remote.ts
+++ packages/cactus-plugin-satp-hermes/src/knex/knexfile-remote.ts
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: path.resolve(__dirname, ".dev-remote.sqlite3"),
+      filename: path.resolve(__dirname, ".dev.remote-" + uuidv4() + ".sqlite3"),
     },
     migrations: {
       directory: path.resolve(__dirname, "migrations"),
