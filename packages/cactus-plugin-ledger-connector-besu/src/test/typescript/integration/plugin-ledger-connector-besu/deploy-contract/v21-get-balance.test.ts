a97db51a1dada2cb4dcb400f7d5282bc146a3e0c
Peter Somogyvari <peter.somogyvari@accenture.com>
2024-03-06 13:27:07 -0800

    test(connector-besu): migrate v21-get-balance test to Jest from tape
    Part of our ongoing effort to migrate the complete test suite from tape
    to Jest.
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-plugin-ledger-connector-besu/src/test/typescript/integration/plugin-ledger-connector-besu/deploy-contract/v21-get-balance.test.ts
+++ packages/cactus-plugin-ledger-connector-besu/src/test/typescript/integration/plugin-ledger-connector-besu/deploy-contract/v21-get-balance.test.ts
@@ -1,4 +1,4 @@
-import test, { Test } from "tape";
+import "jest-extended";
 import { v4 as uuidv4 } from "uuid";
 import { PluginRegistry } from "@hyperledger/cactus-core";
 import {
@@ -13,62 +13,65 @@ import HelloWorldContractJson from "../../../../solidity/hello-world-contract/He
 import Web3 from "web3";
 import { PluginImportType } from "@hyperledger/cactus-core-api";
 
-test("can get balance of an account", async (t: Test) => {
+describe("PluginLedgerConnectorBesu", () => {
   const logLevel: LogLevelDesc = "TRACE";
   const containerImageVersion = "2021-08-24--feat-1244";
   const containerImageName =
     "ghcr.io/hyperledger/cactus-besu-21-1-6-all-in-one";
   const besuOptions = { containerImageName, containerImageVersion };
   const besuTestLedger = new BesuTestLedger(besuOptions);
-  await besuTestLedger.start();
 
-  test.onFinish(async () => {
+  beforeAll(async () => {
+    await besuTestLedger.start();
+  });
+
+  afterAll(async () => {
     await besuTestLedger.stop();
     await besuTestLedger.destroy();
   });
 
-  const rpcApiHttpHost = await besuTestLedger.getRpcApiHttpHost();
-  const rpcApiWsHost = await besuTestLedger.getRpcApiWsHost();
+  test("can get balance of an ETH account", async () => {
+    const rpcApiHttpHost = await besuTestLedger.getRpcApiHttpHost();
+    const rpcApiWsHost = await besuTestLedger.getRpcApiWsHost();
 
-  /**
-   * Constant defining the standard 'dev' Besu genesis.json contents.
-   *
-   * @see https://github.com/hyperledger/besu/blob/21.1.6/config/src/main/resources/dev.json
-   */
-  const firstHighNetWorthAccount = besuTestLedger.getGenesisAccountPubKey();
-  const web3 = new Web3(rpcApiHttpHost);
-  const testEthAccount = web3.eth.accounts.create(uuidv4());
+    /**
+     * Constant defining the standard 'dev' Besu genesis.json contents.
+     *
+     * @see https://github.com/hyperledger/besu/blob/21.1.6/config/src/main/resources/dev.json
+     */
+    const firstHighNetWorthAccount = besuTestLedger.getGenesisAccountPubKey();
+    const web3 = new Web3(rpcApiHttpHost);
+    const testEthAccount = web3.eth.accounts.create(uuidv4());
 
-  const keychainEntryKey = uuidv4();
-  const keychainEntryValue = testEthAccount.privateKey;
-  const keychainPlugin = new PluginKeychainMemory({
-    instanceId: uuidv4(),
-    keychainId: uuidv4(),
-    // pre-provision keychain with mock backend holding the private key of the
-    // test account that we'll reference while sending requests with the
-    // signing credential pointing to this keychain entry.
-    backend: new Map([[keychainEntryKey, keychainEntryValue]]),
-    logLevel,
-  });
-  keychainPlugin.set(
-    HelloWorldContractJson.contractName,
-    JSON.stringify(HelloWorldContractJson),
-  );
-  const factory = new PluginFactoryLedgerConnector({
-    pluginImportType: PluginImportType.Local,
-  });
-  const connector: PluginLedgerConnectorBesu = await factory.create({
-    rpcApiHttpHost,
-    rpcApiWsHost,
-    instanceId: uuidv4(),
-    pluginRegistry: new PluginRegistry({ plugins: [keychainPlugin] }),
-  });
+    const keychainEntryKey = uuidv4();
+    const keychainEntryValue = testEthAccount.privateKey;
+    const keychainPlugin = new PluginKeychainMemory({
+      instanceId: uuidv4(),
+      keychainId: uuidv4(),
+      // pre-provision keychain with mock backend holding the private key of the
+      // test account that we'll reference while sending requests with the
+      // signing credential pointing to this keychain entry.
+      backend: new Map([[keychainEntryKey, keychainEntryValue]]),
+      logLevel,
+    });
+    keychainPlugin.set(
+      HelloWorldContractJson.contractName,
+      JSON.stringify(HelloWorldContractJson),
+    );
+    const factory = new PluginFactoryLedgerConnector({
+      pluginImportType: PluginImportType.Local,
+    });
+    const connector: PluginLedgerConnectorBesu = await factory.create({
+      rpcApiHttpHost,
+      rpcApiWsHost,
+      instanceId: uuidv4(),
+      pluginRegistry: new PluginRegistry({ plugins: [keychainPlugin] }),
+    });
 
-  const req: GetBalanceV1Request = { address: firstHighNetWorthAccount };
-  const currentBalance = await connector.getBalance(req);
-  t.comment(JSON.stringify(currentBalance));
-  //makes the information in to string
-  t.ok(currentBalance, " Balance response is OK :-)");
-  t.equal(typeof currentBalance, "object", "Balance response type is OK :-)");
-  t.end();
+    const req: GetBalanceV1Request = { address: firstHighNetWorthAccount };
+    const currentBalance = await connector.getBalance(req);
+    expect(currentBalance).toBeTruthy();
+    expect(currentBalance).toBeObject();
+    expect(currentBalance).not.toBeEmptyObject();
+  });
 });
