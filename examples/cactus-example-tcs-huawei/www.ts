45fadcd089bb59cf5f604110030a5e7eba5d67b2
Michal Bajer <michal.bajer@fujitsu.com>
2024-03-29 12:24:28 +0100

    feat(cactus-example-tcs-huawei): remove deprecated sample app
    - `cactus-example-tcs-huawei` is using
        `cactus-plugin-ledger-connector-go-ethereum-socketio` and
        `cactus-plugin-ledger-connector-tcs-huawei-socketio` which will
        be removed as well. Ths sample app can't exist on it's own.
    - This is not permanent - we'd love to bring the package back once the
        necessary refactors are done!
    - See issue #3155 on github for more details on context and reasoning
        of this decision.
    
    Closes: #3157
    
    Signed-off-by: Michal Bajer <michal.bajer@fujitsu.com>

--- examples/cactus-example-tcs-huawei/www.ts
+++
@@ -1,7 +0,0 @@
-import { BusinessLogicElectricityTrade } from "./BusinessLogicTcsElectricityTrade";
-import { startCactusSocketIOServer } from "@hyperledger/cactus-cmd-socketio-server";
-
-startCactusSocketIOServer({
-  id: "h40Q9eMD",
-  plugin: new BusinessLogicElectricityTrade("h40Q9eMD"),
-});
