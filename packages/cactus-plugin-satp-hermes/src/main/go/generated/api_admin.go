28bcb1ebdb78649d4b69db214c0ebf1495c2f011
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2024-03-30 11:48:08 +0000

    feat(satp-hermes): add health check endpoint to BLO spec
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/main/go/generated/api_admin.go
+++ packages/cactus-plugin-satp-hermes/src/main/go/generated/api_admin.go
@@ -51,6 +51,20 @@ type AdminApi interface {
 	GetAuditExecute(r ApiGetAuditRequest) (*GetAudit200Response, *http.Response, error)
 
 	/*
+	GetHealthCheck Health check endpoint
+
+	Responds if SATP Hermes is on
+
+	@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	@return ApiGetHealthCheckRequest
+	*/
+	GetHealthCheck(ctx context.Context) ApiGetHealthCheckRequest
+
+	// GetHealthCheckExecute executes the request
+	//  @return GetHealthCheck200Response
+	GetHealthCheckExecute(r ApiGetHealthCheckRequest) (*GetHealthCheck200Response, *http.Response, error)
+
+	/*
 	GetStatus Get SATP current session data
 
 	Retrieve the status of a SATP session
@@ -275,6 +289,105 @@ func (a *AdminApiService) GetAuditExecute(r ApiGetAuditRequest) (*GetAudit200Res
 	if r.includeProofs != nil {
 		parameterAddToHeaderOrQuery(localVarQueryParams, "includeProofs", r.includeProofs, "")
 	}
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := io.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = io.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.client.decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
+type ApiGetHealthCheckRequest struct {
+	ctx context.Context
+	ApiService AdminApi
+}
+
+func (r ApiGetHealthCheckRequest) Execute() (*GetHealthCheck200Response, *http.Response, error) {
+	return r.ApiService.GetHealthCheckExecute(r)
+}
+
+/*
+GetHealthCheck Health check endpoint
+
+Responds if SATP Hermes is on
+
+ @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+ @return ApiGetHealthCheckRequest
+*/
+func (a *AdminApiService) GetHealthCheck(ctx context.Context) ApiGetHealthCheckRequest {
+	return ApiGetHealthCheckRequest{
+		ApiService: a,
+		ctx: ctx,
+	}
+}
+
+// Execute executes the request
+//  @return GetHealthCheck200Response
+func (a *AdminApiService) GetHealthCheckExecute(r ApiGetHealthCheckRequest) (*GetHealthCheck200Response, *http.Response, error) {
+	var (
+		localVarHTTPMethod   = http.MethodGet
+		localVarPostBody     interface{}
+		formFiles            []formFile
+		localVarReturnValue  *GetHealthCheck200Response
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "AdminApiService.GetHealthCheck")
+	if err != nil {
+		return localVarReturnValue, nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/api/v1/@hyperledger/cactus-plugin-satp-hermes/healthcheck"
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+
 	// to determine the Content-Type header
 	localVarHTTPContentTypes := []string{}
 
