1e4a410f17ff401630c8af9445f5cf7ea3f703bc
Peter Somogyvari <peter.somogyvari@accenture.com>
2024-03-30 09:38:37 -0700

    test(connector-besu): migrate get-past-logs.test.ts to Jest runner
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-plugin-ledger-connector-besu/src/test/typescript/integration/plugin-ledger-connector-besu/deploy-contract/get-past-logs.test.ts
+++ packages/cactus-plugin-ledger-connector-besu/src/test/typescript/integration/plugin-ledger-connector-besu/deploy-contract/get-past-logs.test.ts
@@ -1,4 +1,4 @@
-import test, { Test } from "tape";
+import "jest-extended";
 import { v4 as uuidv4 } from "uuid";
 import { PluginRegistry } from "@hyperledger/cactus-core";
 import {
@@ -13,60 +13,63 @@ import HelloWorldContractJson from "../../../../solidity/hello-world-contract/He
 import Web3 from "web3";
 import { PluginImportType } from "@hyperledger/cactus-core-api";
 
-test("can get past logs of an account", async (t: Test) => {
-  const logLevel: LogLevelDesc = "TRACE";
-  const besuTestLedger = new BesuTestLedger();
-  await besuTestLedger.start();
+const logLevel: LogLevelDesc = "INFO";
 
-  test.onFinish(async () => {
+describe("PluginLedgerConnectorBesu", () => {
+  const besuTestLedger = new BesuTestLedger();
+
+  beforeAll(async () => {
+    await besuTestLedger.start();
+  });
+  afterAll(async () => {
     await besuTestLedger.stop();
     await besuTestLedger.destroy();
   });
+  test("can get past logs of an account", async () => {
+    const rpcApiHttpHost = await besuTestLedger.getRpcApiHttpHost();
+    const rpcApiWsHost = await besuTestLedger.getRpcApiWsHost();
 
-  const rpcApiHttpHost = await besuTestLedger.getRpcApiHttpHost();
-  const rpcApiWsHost = await besuTestLedger.getRpcApiWsHost();
+    /**
+     * Constant defining the standard 'dev' Besu genesis.json contents.
+     *
+     * @see https://github.com/hyperledger/besu/blob/1.5.1/config/src/main/resources/dev.json
+     */
+    const firstHighNetWorthAccount = besuTestLedger.getGenesisAccountPubKey();
 
-  /**
-   * Constant defining the standard 'dev' Besu genesis.json contents.
-   *
-   * @see https://github.com/hyperledger/besu/blob/1.5.1/config/src/main/resources/dev.json
-   */
-  const firstHighNetWorthAccount = besuTestLedger.getGenesisAccountPubKey();
+    const web3 = new Web3(rpcApiHttpHost);
+    const testEthAccount = web3.eth.accounts.create(uuidv4());
 
-  const web3 = new Web3(rpcApiHttpHost);
-  const testEthAccount = web3.eth.accounts.create(uuidv4());
+    const keychainEntryKey = uuidv4();
+    const keychainEntryValue = testEthAccount.privateKey;
+    const keychainPlugin = new PluginKeychainMemory({
+      instanceId: uuidv4(),
+      keychainId: uuidv4(),
+      // pre-provision keychain with mock backend holding the private key of the
+      // test account that we'll reference while sending requests with the
+      // signing credential pointing to this keychain entry.
+      backend: new Map([[keychainEntryKey, keychainEntryValue]]),
+      logLevel,
+    });
+    keychainPlugin.set(
+      HelloWorldContractJson.contractName,
+      JSON.stringify(HelloWorldContractJson),
+    );
+    const factory = new PluginFactoryLedgerConnector({
+      pluginImportType: PluginImportType.Local,
+    });
 
-  const keychainEntryKey = uuidv4();
-  const keychainEntryValue = testEthAccount.privateKey;
-  const keychainPlugin = new PluginKeychainMemory({
-    instanceId: uuidv4(),
-    keychainId: uuidv4(),
-    // pre-provision keychain with mock backend holding the private key of the
-    // test account that we'll reference while sending requests with the
-    // signing credential pointing to this keychain entry.
-    backend: new Map([[keychainEntryKey, keychainEntryValue]]),
-    logLevel,
+    const connector: PluginLedgerConnectorBesu = await factory.create({
+      rpcApiHttpHost,
+      rpcApiWsHost,
+      instanceId: uuidv4(),
+      pluginRegistry: new PluginRegistry({ plugins: [keychainPlugin] }),
+    });
+    await connector.onPluginInit();
+
+    const req: GetPastLogsV1Request = { address: firstHighNetWorthAccount };
+    const pastLogs = await connector.getPastLogs(req);
+    expect(pastLogs).toBeTruthy();
+    expect(pastLogs).toBeObject();
+    expect(pastLogs.logs).toBeTruthy();
   });
-  keychainPlugin.set(
-    HelloWorldContractJson.contractName,
-    JSON.stringify(HelloWorldContractJson),
-  );
-  const factory = new PluginFactoryLedgerConnector({
-    pluginImportType: PluginImportType.Local,
-  });
-
-  const connector: PluginLedgerConnectorBesu = await factory.create({
-    rpcApiHttpHost,
-    rpcApiWsHost,
-    instanceId: uuidv4(),
-    pluginRegistry: new PluginRegistry({ plugins: [keychainPlugin] }),
-  });
-  await connector.onPluginInit();
-
-  const req: GetPastLogsV1Request = { address: firstHighNetWorthAccount };
-  const pastLogs = await connector.getPastLogs(req);
-  t.comment(JSON.stringify(pastLogs));
-  t.ok(pastLogs, "Past logs response is OK :-)");
-  t.equal(typeof pastLogs, "object", "Past logs response type is OK :-)");
-  t.end();
 });
