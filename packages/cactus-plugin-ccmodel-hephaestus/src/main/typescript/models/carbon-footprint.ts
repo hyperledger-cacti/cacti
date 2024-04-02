d6f8147ae68a15232ecba1a9004003dfd7c656d8
Bruno Mateus <brumat315@gmail.com>
2024-04-02 14:32:07 +0100

    refactor(cc-tx-visualization): capture transactional data with RxJS
    The plugin now utilizes RxJS instead of RabbitMQ in transaction monitoring.
    ReplaySubjects store and emit observed transactional data to subscribers.
    
    fix(cc-tx-visualization): rebase latest version
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    squash! - migrate a test case to Fabric v2.5.6 LTS AIO
    
    This is just a snippet of a change from the pair programming
    call with Bruno, it can be safely squashed, no worries.
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>
    Signed-off-by: Bruno Mateus <brumat315@gmail.com>
    
    feat(cc-tx-visualization): ccmodel serialization with PM4PY
    
    The plugin now creates the ccmodel for conformance checking with PM4PY.
    
    Signed-off-by: Bruno Mateus <brumat315@gmail.com>

---
+++ packages/cactus-plugin-ccmodel-hephaestus/src/main/typescript/models/carbon-footprint.ts
@@ -0,0 +1,42 @@
+import { LedgerType } from "@hyperledger/cactus-core-api";
+
+export function calculateGasPriceEth(
+  gasUsed: number,
+  effectiveGasPrice: string | undefined,
+): number {
+  if (!gasUsed || !effectiveGasPrice) {
+    return 0;
+  }
+  const gasPrice = parseFloat(effectiveGasPrice);
+  return gasUsed * gasPrice;
+}
+
+export function calculateCarbonFootPrintFabric(
+  peers: string[] | undefined,
+): number {
+  if (!peers) {
+    return 0;
+  }
+  return peers.length * CarbonFootPrintConstants(LedgerType.Fabric2);
+}
+export function calculateCarbonFootPrintBesu(): number {
+  return CarbonFootPrintConstants(LedgerType.Besu2X);
+}
+
+export const CarbonFootPrintConstants = (ledger: LedgerType): number => {
+  switch (ledger) {
+    case LedgerType.Besu2X:
+      return 0.00018;
+
+    case LedgerType.Besu1X:
+      return 0.00018;
+
+    case LedgerType.Fabric2:
+      return 0.00018;
+
+    case LedgerType.Fabric14X:
+      return 0.00018;
+    default:
+      return 0;
+  }
+};
