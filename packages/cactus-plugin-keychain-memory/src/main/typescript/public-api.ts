17163ef77273ab588598a0cd1c104b7404f36908
Peter Somogyvari <peter.somogyvari@accenture.com>
2024-04-04 09:41:28 -0700

    feat(plugin-keychain-memory): add ConnectRPC support
    1. This leverages the newly introduced methods in core-api that the API
    server is using to probe if a plugin has ConnectRPC support or not.
    2. There is support for both HTTP 1.1 and HTTP 2. The caveat here is that
    HTTP 2 is not supported by ExpressJS so we pulled in Fastify to handle
    those type of requests and that means that HTTP 2 ConnectRPC traffic has
    to go through a different port compared to the HTTP 1.1 ConnectRPC traffic.
    3. The lesson here is that we probably need to migrate away from ExpressJS
    longer term because it does not (and from the looks of it will not ever)
    support HTTP 2 which is probably going to be a bit of technical debt/
    limiting factor in architectural decisions going forward for both Cacti
    maintainers and Cacti users.
    4. A new code generator has been introduced by this commit as well which
    is @buf/build - the tool where ConnectRPC originates from. The scripts are
    structured in such a way that this should be seamlessly integrated into the
    existing `codegen` root level script and therefore also the CI.
    5. There is test coverage for both HTTP 1.1 and HTTP 2 traffic in the file at
    ```sh
    packages/cactus-test-plugin-keychain-memory/src/test/typescript/integration/
    test-keychain-memory-crpc-api-server.test.ts
    ```
    6. The test case referenced above is also the example on how to use the
    ConnectRPC client (very similar to the HTTP client we already had before)
    
    Depends on #3183
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-plugin-keychain-memory/src/main/typescript/public-api.ts
+++ packages/cactus-plugin-keychain-memory/src/main/typescript/public-api.ts
@@ -14,3 +14,26 @@ export async function createPluginFactory(
 ): Promise<PluginFactoryKeychain> {
   return new PluginFactoryKeychain(pluginFactoryOptions);
 }
+
+export { IKeychainMemoryCrpcSvcOpenApiOptions } from "./crpc-services/keychain-memory-crpc-svc-openapi";
+export { KeychainMemoryCrpcSvcOpenApi } from "./crpc-services/keychain-memory-crpc-svc-openapi";
+
+export { DefaultService } from "./generated/crpc/services/default_service_connect";
+
+export { DeleteKeychainEntryRequestV1PB } from "./generated/crpc/models/delete_keychain_entry_request_v1_pb_pb";
+export { DeleteKeychainEntryResponseV1PB } from "./generated/crpc/models/delete_keychain_entry_response_v1_pb_pb";
+
+export { GetKeychainEntryRequestV1PB } from "./generated/crpc/models/get_keychain_entry_request_v1_pb_pb";
+export { GetKeychainEntryResponseV1PB } from "./generated/crpc/models/get_keychain_entry_response_v1_pb_pb";
+
+export { HasKeychainEntryRequestV1PB } from "./generated/crpc/models/has_keychain_entry_request_v1_pb_pb";
+export { HasKeychainEntryResponseV1PB } from "./generated/crpc/models/has_keychain_entry_response_v1_pb_pb";
+
+export { SetKeychainEntryRequestV1PB } from "./generated/crpc/models/set_keychain_entry_request_v1_pb_pb";
+export { SetKeychainEntryResponseV1PB } from "./generated/crpc/models/set_keychain_entry_response_v1_pb_pb";
+
+export { DeleteKeychainEntryV1Request } from "./generated/crpc/services/default_service_pb";
+export { GetKeychainEntryV1Request } from "./generated/crpc/services/default_service_pb";
+export { GetPrometheusMetricsV1Response } from "./generated/crpc/services/default_service_pb";
+export { HasKeychainEntryV1Request } from "./generated/crpc/services/default_service_pb";
+export { SetKeychainEntryV1Request } from "./generated/crpc/services/default_service_pb";
---
+++ packages/cactus-test-plugin-keychain-memory/README.md
@@ -0,0 +1,5 @@
+# `@hyperledger/cactus-test-plugin-keychain-memory`
+
+This is a private package only used for executing tests that cannot live in the
+implementation package due to us trying to avoid circular dependencies between
+the API server and the plugins.
