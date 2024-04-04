5d97af68728afe8cf41d5870401b534cd18129ee
Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
2024-04-04 21:47:35 +0100

    feat(satp-hermes): core stage services message names update
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-services/stage3.ts
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-services/stage3.ts
@@ -1,43 +1,43 @@
 import { ConnectRouter, HandlerContext } from "@connectrpc/connect";
 import { SatpStage3Service } from "../../generated/proto/cacti/satp/v02/stage_3_connect";
 import {
-  CommitFinalAcknowledgementReceiptResponse,
-  CommitFinalAssertionRequest,
-  CommitPreparationRequest,
-  CommitReadyResponse,
-  TransferCompleteRequest,
-  TransferCompleteResponse,
+  CommitFinalAcknowledgementReceiptResponseMessage,
+  CommitFinalAssertionRequestMessage,
+  CommitPreparationRequestMessage,
+  CommitReadyResponseMessage,
+  TransferCompleteRequestMessage,
 } from "../../generated/proto/cacti/satp/v02/stage_3_pb";
+import { Empty } from "@bufbuild/protobuf";
 
 export const sendCommitPreparationRequest: any = (router: ConnectRouter) =>
   router.service(SatpStage3Service, {
     async commitPreparation(
-      req: CommitPreparationRequest,
+      req: CommitPreparationRequestMessage,
       context: HandlerContext,
     ) {
       console.log("Received request", req, context);
-      return new CommitReadyResponse({});
+      return new CommitReadyResponseMessage({});
     },
   });
 
 export const sendCommitFinalAssertionRequest: any = (router: ConnectRouter) =>
   router.service(SatpStage3Service, {
     async commitFinalAssertion(
-      req: CommitFinalAssertionRequest,
+      req: CommitFinalAssertionRequestMessage,
       context: HandlerContext,
     ) {
       console.log("Received request", req, context);
-      return new CommitFinalAcknowledgementReceiptResponse({});
+      return new CommitFinalAcknowledgementReceiptResponseMessage({});
     },
   });
 
 export const sendTransferCompleteRequest: any = (router: ConnectRouter) =>
   router.service(SatpStage3Service, {
     async transferComplete(
-      req: TransferCompleteRequest,
+      req: TransferCompleteRequestMessage,
       context: HandlerContext,
     ) {
       console.log("Received request", req, context);
-      return new TransferCompleteResponse({});
+      return new Empty({});
     },
   });
