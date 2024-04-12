c045b96623c6cd3f74cac5c75c8425fbc44ea7d9
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2024-04-12 17:15:11 +0300

    fixup(satp-hermes): add skeleton for satp-bridge
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

---
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-services/satp-bridge/satp-bridge-manager.ts
@@ -0,0 +1,3 @@
+// this file contains a class that encapsulates the logic for managing the SATP bridge (lock, unlock, etc).
+// should inject satp gateway session data (having parameters/chains for transactions), and processes smart contract output
+
