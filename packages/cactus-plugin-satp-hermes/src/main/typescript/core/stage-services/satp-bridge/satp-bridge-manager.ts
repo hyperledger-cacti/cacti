a8dff061a6c17d6040867a71641bf2b4d2b406fe
Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
2024-04-15 19:08:52 +0100

    feat(satp-hermes): update protos and services
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-services/satp-bridge/satp-bridge-manager.ts
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-services/satp-bridge/satp-bridge-manager.ts
@@ -1,3 +1,21 @@
 // this file contains a class that encapsulates the logic for managing the SATP bridge (lock, unlock, etc).
 // should inject satp gateway session data (having parameters/chains for transactions), and processes smart contract output
 
+export abstract class SATPBridgeManager {
+  public abstract lockAsset(
+    sessionId: string,
+    assetId: string,
+  ): Promise<string>;
+  public abstract unlockAsset(
+    sessionId: string,
+    assetId: string,
+  ): Promise<string>;
+  public abstract mintAsset(
+    sessionId: string,
+    assetId: string,
+  ): Promise<string>;
+  public abstract burnAsset(
+    sessionId: string,
+    assetId: string,
+  ): Promise<string>;
+}
