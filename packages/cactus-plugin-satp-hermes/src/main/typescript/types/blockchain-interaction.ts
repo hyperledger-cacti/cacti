3a6a440bcc36e6deca2f185c4606b7dcb7e0bcd3
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2024-04-26 23:02:41 +0300

    feat(satp-hermes): satp development continuation and implementation satp protocol
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/types/blockchain-interaction.ts
+++
@@ -1,5 +0,0 @@
-// TODO: define alias types for SATPLedgerConnector, which encapsulates IPluginLedgerConnector
-// inject gateway, get connectors
-export type SATPLedgerConnector = string;
-
-// TODO Define lock interfaces and strategy pattern for locking (as function of locking blockchain) (see what smart contract implementations return)
