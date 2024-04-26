1b5783ccf3d52334f94bca92578953d654ff53e9
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2024-04-26 23:02:41 +0300

    fix: refactor services and handlers
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/types/satp-protocol.ts
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/types/satp-protocol.ts
@@ -1,16 +1,46 @@
-import { JsObjectSigner } from "@hyperledger/cactus-common";
+import { JsObjectSigner, LogLevelDesc, Logger } from "@hyperledger/cactus-common";
 import { SupportedGatewayImplementations } from "../core/types";
 import { SessionData } from "../generated/proto/cacti/satp/v02/common/session_pb";
 import { v4 as uuidV4 } from "uuid";
+import { ConnectRouter } from "@connectrpc/connect";
 
-export type SATPService = {
-
+/**
+ * Interface for SATP Services.
+ * Each service implementing this interface must provide one or more function(s) to handle specific stages of the SATP protocol.
+ * Implementations should ensure compliance with the defined asynchronous patterns essential for SATP protocol interactions.
+ */
+export interface SATPService {
+  readonly className: string;
+  readonly stage: string;
+  readonly log: Logger;
+  readonly serviceType: SATPServiceType;
+  getServiceIdentifier(): string;
 }
 
-export type ISATPServerServiceOptions = {
+/**
+ * Represents a handler for various stages of the SATP (Secure Asset Transfer Protocol).
+ * Handlers implementing this interface must provide mechanisms to setup routes and handle
+ * protocol-specific requests based on the stage they are designed for.
+  */
+export interface SATPHandler {
+  setupRouter(router: ConnectRouter): void;
+  getHandlerIdentifier(): string;
+}
+
+export enum SATPServiceType {
+  Server,
+  Client,
+} 
+
+export type ISATPServiceOptions = {
+  logLevel: LogLevelDesc
   signer: JsObjectSigner;
 }
 
-export type ISATPClientServiceOptions = {
-  signer: JsObjectSigner;
+export type ISATPServerServiceOptions  = ISATPServiceOptions & {
+
+}
+
+export type ISATPClientServiceOptions = ISATPServiceOptions & {
+
 }
