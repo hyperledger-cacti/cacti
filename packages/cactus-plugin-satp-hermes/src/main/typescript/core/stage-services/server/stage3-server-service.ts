5298ac1ec89c0870a8eff6d952458545752f859f
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2024-04-26 23:02:41 +0300

    fix: refactor services and handlers
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-services/server/stage3-server-service.ts
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-services/server/stage3-server-service.ts
@@ -20,17 +20,19 @@ import {
   verifySignature,
 } from "../../../gateway-utils";
 import { getMessageHash, saveHash, saveSignature } from "../../session-utils";
-import { SATPService, ISATPServerServiceOptions } from "../../../types/satp-protocol";
+import { SATPService, ISATPServerServiceOptions, SATPServiceType } from "../../../types/satp-protocol";
 import { SATPSession } from "../../../core/satp-session";
 
 export class Stage3ServerService implements SATPService {
-  public static readonly CLASS_NAME = "Stage3ServerService";
+  public static readonly CLASS_NAME = "server-service";
+  public static readonly SATP_STAGE = "stage-3";
+  public static readonly SERVICE_TYPE = SATPServiceType.Server;
   private _log: Logger;
   private signer: JsObjectSigner;
 
   constructor(ops: ISATPServerServiceOptions) {
-    const level = "INFO";
-    const label = Stage3ServerService.CLASS_NAME;
+    const level = ops.logLevel || "INFO";
+    const label = this.getServiceIdentifier();
     this._log = LoggerProvider.getOrCreate({ level, label });
     this.signer = ops.signer;
   }
@@ -39,10 +41,22 @@ export class Stage3ServerService implements SATPService {
     return Stage3ServerService.CLASS_NAME;
   }
 
+  public get stage(): string {
+    return Stage3ServerService.SATP_STAGE;
+  }
+
   public get log(): Logger {
     return this._log;
   }
 
+  public get serviceType(): SATPServiceType {
+    return SATPServiceType.Server;
+  }
+
+  public getServiceIdentifier(): string {
+    return `${this.className}#${this.serviceType}`;
+  }
+ 
   async commitReady(
     request: CommitPreparationRequestMessage,
     session: SATPSession,
