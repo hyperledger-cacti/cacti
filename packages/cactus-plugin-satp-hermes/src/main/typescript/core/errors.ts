48157b4bd23a266e7b872b02683b4c61f45eb83d
Rafael Belchior <rbelchior@blockdaemon.com>
2024-05-12 18:21:44 +0100

    fix: eslint all
    Signed-off-by: Rafael Belchior <rbelchior@blockdaemon.com>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/core/errors.ts
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/core/errors.ts
@@ -1,5 +1,10 @@
 export class SATPError extends Error {
-  constructor(message: string, public code: number = 500, public internalErrorId?: string, public trace?: string) {
+  constructor(
+    message: string,
+    public code: number = 500,
+    public internalErrorId?: string,
+    public trace?: string,
+  ) {
     super(message);
     this.name = this.constructor.name;
     Object.setPrototypeOf(this, new.target.prototype); // make sure prototype chain is set to error
@@ -9,7 +14,12 @@ export class SATPError extends Error {
 
 export class BootstrapError extends SATPError {
   constructor(internalErrorId?: string, trace?: string) {
-    super("Bootstrap already called in this Gateway Manager", 409, internalErrorId, trace);
+    super(
+      "Bootstrap already called in this Gateway Manager",
+      409,
+      internalErrorId,
+      trace,
+    );
   }
 }
 
@@ -20,7 +30,17 @@ export class NonExistantGatewayIdentity extends SATPError {
 }
 
 export class GetStatusError extends SATPError {
-  constructor(sessionID: string, message: string, internalErrorId?: string, trace?: string) {
-    super(`Could not GetStatus at Session: with id ${sessionID}. Reason: ${message}`, 400, internalErrorId, trace);
+  constructor(
+    sessionID: string,
+    message: string,
+    internalErrorId?: string,
+    trace?: string,
+  ) {
+    super(
+      `Could not GetStatus at Session: with id ${sessionID}. Reason: ${message}`,
+      400,
+      internalErrorId,
+      trace,
+    );
   }
 }
