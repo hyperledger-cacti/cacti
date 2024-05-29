6aa3aabce5315a637e900309aa1aa284bb3edce5
github-action-benchmark <github@users.noreply.github.com>
2024-05-29 18:23:48 +0000

    add Benchmark (benchmarkjs) benchmark result for 81da3334d8e638f85e398dd228bcef836a278230

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717001653082,
+  "lastUpdate": 1717007028275,
   "repoUrl": "https://github.com/hyperledger/cacti",
   "entries": {
     "Benchmark": [
@@ -29,6 +29,44 @@ window.BENCHMARK_DATA = {
             "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
             "value": 771,
             "range": "±2.96%",
+            "unit": "ops/sec",
+            "extra": "180 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zondervan.v.calvez@accenture.com",
+            "name": "zondervancalvez",
+            "username": "zondervancalvez"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "81da3334d8e638f85e398dd228bcef836a278230",
+          "message": "fix(cmd-api-server): address CVE-2022-25881\n\nPrimary Changes:\n\tUpdated the Dockerfile & https-cache-semantics inside the cmd-api-server package\n\nFixes: #2862\n\nSigned-off-by: zondervancalvez <zondervan.v.calvez@accenture.com>\nSigned-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>",
+          "timestamp": "2024-05-29T11:12:31-07:00",
+          "tree_id": "150f754ac29b731c5aafa0fe70fe64afdec1aca2",
+          "url": "https://github.com/hyperledger/cacti/commit/81da3334d8e638f85e398dd228bcef836a278230"
+        },
+        "date": 1717007025069,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 565,
+            "range": "±1.81%",
+            "unit": "ops/sec",
+            "extra": "177 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 349,
+            "range": "±1.39%",
             "unit": "ops/sec",
             "extra": "180 samples"
           }
