96a3865ce5404e0fd3bf5a6471eb5a653e579d33
aldousalvarez <aldousss.alvarez@gmail.com>
2024-06-11 17:37:04 +0800

    fix(ci): deprecationWarning in yarn_custom_checks
    Primary Changes
    ----------------
    1. Updated the main field to use index.js to fix the Deprecation Warning
    
    Peter's Fixes:
    ----------------
    1. I fixed the file paths in the require statements to match the new location
    after we've remapped the bundle location in the package.json files.
    
    Fixes #2956
    
    Co-Authored-by: Peter Somogyvari <peter.somogyvari@accenture.com>
    
    Signed-off-by: aldousalvarez <aldousss.alvarez@gmail.com>
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- weaver/sdks/fabric/interoperation-node-sdk/index.js
+++ weaver/sdks/fabric/interoperation-node-sdk/index.js
@@ -12,10 +12,10 @@
 // because jsdoc3 generator seems to not able to find them in the api.js module
 // likely due to that module containing multiple classes
 
-module.exports.RelayHelper = require("./src/Relay.js");
-module.exports.InteroperableHelper = require("./src/InteroperableHelper.js");
-module.exports.AssetManager = require("./src/AssetManager.js");
-module.exports.SatpAssetManager = require("./src/SatpAssetManager.js");
-module.exports.HashFunctions = require("./src/HashFunctions.js");
-module.exports.EventsManager = require("./src/EventsManager.js");
-module.exports.MembershipManager = require("./src/MembershipManager.js");
+module.exports.RelayHelper = require("./build/src/Relay.js");
+module.exports.InteroperableHelper = require("./build/src/InteroperableHelper.js");
+module.exports.AssetManager = require("./build/src/AssetManager.js");
+module.exports.SatpAssetManager = require("./build/src/SatpAssetManager.js");
+module.exports.HashFunctions = require("./build/src/HashFunctions.js");
+module.exports.EventsManager = require("./build/src/EventsManager.js");
+module.exports.MembershipManager = require("./build/src/MembershipManager.js");
