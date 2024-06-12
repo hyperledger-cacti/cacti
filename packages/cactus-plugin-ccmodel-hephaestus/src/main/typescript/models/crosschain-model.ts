11982c2f2e76add4101b2fa5a4e1ca9ddd063f2c
Bruno Mateus <brumat315@gmail.com>
2024-06-12 16:05:38 +0100

    feat(hephaestus): analyse transactions using hephaestus
    The plugin is now able to:
    Create a Cross-Chain Model based on monitored transactions
    Compare new transactions against the model and detect non-conformance
    
    Signed-off-by: Bruno Mateus <brumat315@gmail.com>

--- packages/cactus-plugin-ccmodel-hephaestus/src/main/typescript/models/crosschain-model.ts
+++ packages/cactus-plugin-ccmodel-hephaestus/src/main/typescript/models/crosschain-model.ts
@@ -1,3 +1,4 @@
+import { LedgerType } from "@hyperledger/cactus-core-api";
 import { v4 as uuidv4 } from "uuid";
 
 export class CrossChainModel {
@@ -8,6 +9,12 @@ export class CrossChainModel {
   private models = new Map<CrossChainModelType, string>();
   private id: string;
   private lastAggregationDate: Date;
+  private crossChainState: Map<
+    string,
+    [AssetState | undefined, AssetState | undefined]
+  >;
+  private sourceLedgerMethods: string[] = [];
+  private targetLedgerMethods: string[] = [];
 
   constructor() {
     this.id = uuidv4();
@@ -16,6 +23,10 @@ export class CrossChainModel {
       CrossChainTransactionSchema
     >();
     this.lastAggregationDate = new Date();
+    this.crossChainState = new Map<
+      string,
+      [AssetState | undefined, AssetState | undefined]
+    >();
   }
 
   get lastAggregation(): Date {
@@ -62,6 +73,99 @@ export class CrossChainModel {
   ): void {
     this.crossChainTransactions?.set(key, mapDefintion);
   }
+
+  public setSourceLedgerMethod(methodName: string): void {
+    this.sourceLedgerMethods.push(methodName);
+  }
+
+  public sourceLedgerIncludesMethod(methodName: string): boolean {
+    return this.sourceLedgerMethods.includes(methodName);
+  }
+
+  public setTargetLedgerMethod(methodName: string): void {
+    this.targetLedgerMethods.push(methodName);
+  }
+
+  public targetLedgerIncludesMethod(methodName: string): boolean {
+    return this.targetLedgerMethods.includes(methodName);
+  }
+
+  public setAssetStateSourceLedger(ccTxID: string, details: AssetState): void {
+    const prevState = this.crossChainState.get(ccTxID);
+    if (!prevState) {
+      this.crossChainState.set(ccTxID, [details, undefined]);
+      return;
+    }
+    this.crossChainState.set(ccTxID, [details, prevState[1]]);
+  }
+
+  public setAssetStateTargetLedger(ccTxID: string, details: AssetState): void {
+    const prevState = this.crossChainState.get(ccTxID);
+    if (!prevState) {
+      this.crossChainState.set(ccTxID, [undefined, details]);
+      return;
+    }
+    this.crossChainState.set(ccTxID, [prevState[0], details]);
+  }
+
+  public getCrossChainState(): string | undefined {
+    let ccState: string = "";
+    for (const [
+      ccTxID,
+      [assetStateSource, assetStateTarget],
+    ] of this.crossChainState.entries()) {
+      let txData: string;
+      if (assetStateSource && assetStateTarget) {
+        txData =
+          ccTxID +
+          "\n" +
+          assetStateSource.assetID +
+          ";" +
+          assetStateSource.assetState +
+          ";" +
+          assetStateSource.ledger +
+          ";" +
+          assetStateSource.lastStateUpdate +
+          "\n" +
+          assetStateTarget.assetID +
+          ";" +
+          assetStateTarget.assetState +
+          ";" +
+          assetStateTarget.ledger +
+          ";" +
+          assetStateTarget.lastStateUpdate +
+          "\n";
+      } else if (assetStateSource) {
+        txData =
+          ccTxID +
+          "\n" +
+          assetStateSource.assetID +
+          ";" +
+          assetStateSource.assetState +
+          ";" +
+          assetStateSource.ledger +
+          ";" +
+          assetStateSource.lastStateUpdate +
+          "\n";
+      } else if (assetStateTarget) {
+        txData =
+          ccTxID +
+          "\n" +
+          assetStateTarget.assetID +
+          ";" +
+          assetStateTarget.assetState +
+          ";" +
+          assetStateTarget.ledger +
+          ";" +
+          assetStateTarget.lastStateUpdate +
+          "\n";
+      } else {
+        continue;
+      }
+      ccState = ccState + txData + "\n";
+    }
+    return ccState;
+  }
 }
 
 export enum CrossChainModelType {
@@ -70,7 +174,6 @@ export enum CrossChainModelType {
 }
 
 export type CrossChainTransactionSchema = {
-  ccTxID: string;
   // the IDs of all cross chain events of the cross chain transaction
   processedCrossChainEvents: string[];
   latency: number;
@@ -78,4 +181,11 @@ export type CrossChainTransactionSchema = {
   cost: number | undefined;
   throughput: number;
   latestUpdate: Date;
+};
+
+export type AssetState = {
+  assetID: string;
+  assetState: string;
+  ledger: LedgerType;
+  lastStateUpdate: Date;
 };
