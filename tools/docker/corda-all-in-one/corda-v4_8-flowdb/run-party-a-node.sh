de0530276e26713a1a3705412821f71f12d3eada
Peter Somogyvari <peter.somogyvari@accenture.com>
2024-07-08 13:23:38 -0700

    test(tools): fix Corda AIO flowdb image: JVM upgrade, flow permissions
    1. Also removed the party B, C and notary because none of these are present
    in the ledger definition that we get handed down from the official kotlin
    samples Corda GitHub repository.
    2. The above change also saves on resources to have only party A which
    makes this image boot very fast compared to the other Corda AIO images.
    3. Also refactored the corda sample app enum so that the file-system paths
    declared in it are matching reality instead of pointing to non-existent
    directories.
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- tools/docker/corda-all-in-one/corda-v4_8-flowdb/run-party-a-node.sh
+++ tools/docker/corda-all-in-one/corda-v4_8-flowdb/run-party-a-node.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$PARTY_A_NODE_ENABLED" = "true" ]
 then
-  java -Dcapsule.jvm.args="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -javaagent:drivers/jolokia-jvm-1.6.0-agent.jar=port=7005,logHandlerClass=net.corda.node.JolokiaSlf4jAdapter" -Dname=ParticipantA -jar /samples-kotlin/Basic/flow-database-access/build/nodes/ParticipantA/corda.jar
+  java -Dcapsule.jvm.args="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -javaagent:drivers/jolokia-jvm-1.6.2-agent.jar=port=7005,logHandlerClass=net.corda.node.JolokiaSlf4jAdapter" -Dname=PartyA -jar /samples-kotlin/Basic/flow-database-access/build/nodes/PartyA/corda.jar
 else
   sleep infinity
 fi
