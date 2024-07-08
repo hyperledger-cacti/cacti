19941280469a3f66cb678525a4088f86b9cacee3
Peter Somogyvari <peter.somogyvari@accenture.com>
2024-07-08 14:06:52 -0700

    feat(connector-corda): support JVM 17 Cordapps
    1. The runner stage of the corda connector JVM image now uses an up to
    date JRE instead of the very old JDK 8 which the builder stage is still
    stuck on.
    2. This enables the connector to load and use contract .jar files which
    were built with the newer JDK as well.
    3. This is important because we would like to support as wide range of
    contract/flow invocation use cases as possible and also recently there's
    been some changes in the Corda official samples repositories where the
    contracts are being upgraded to JVM 17 as well so this will become more
    and more relevant as a use-case in the near future.
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-plugin-ledger-connector-corda/src/main-server/start-app.sh
+++ packages/cactus-plugin-ledger-connector-corda/src/main-server/start-app.sh
@@ -2,6 +2,14 @@
 
 # Without these we get crashes on JDK 17 an above since the introduction of the
 # Java Modules system.
-EXTRA_JVM_ARGS=""
+
+EXTRA_JVM_ARGS="--add-exports java.base/sun.security.provider.certpath=ALL-UNNAMED"
+EXTRA_JVM_ARGS="--add-exports java.base/sun.security.util=ALL-UNNAMED ${EXTRA_JVM_ARGS}"
+EXTRA_JVM_ARGS="--add-exports java.base/sun.security.rsa=ALL-UNNAMED ${EXTRA_JVM_ARGS}"
+EXTRA_JVM_ARGS="--add-exports jdk.crypto.ec/sun.security.ec=ALL-UNNAMED ${EXTRA_JVM_ARGS}"
+EXTRA_JVM_ARGS="--add-exports java.base/sun.security.x509=ALL-UNNAMED ${EXTRA_JVM_ARGS}"
+EXTRA_JVM_ARGS="--add-opens java.base/java.util=ALL-UNNAMED ${EXTRA_JVM_ARGS}"
+EXTRA_JVM_ARGS="--add-opens java.base/java.time=ALL-UNNAMED ${EXTRA_JVM_ARGS}"
+EXTRA_JVM_ARGS="--add-opens java.base/java.io=ALL-UNNAMED ${EXTRA_JVM_ARGS}"
 
 for i in 1 2 3; do java $EXTRA_JVM_ARGS -jar ${APP}/kotlin-spring/build/libs/cactus-connector-corda-server-2.0.0-rc.2.jar && break || sleep 5; done
