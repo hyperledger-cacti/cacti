b065cbc40dd344aaf02e2947652c59d3dffbc5d9
Peter Somogyvari <peter.somogyvari@accenture.com>
2024-07-12 08:10:33 -0700

    test(supply-chain-app-backend): fix via-npm-script.test.ts hanging
    1. The test case was hanging at the end because it was waiting  for a
    magic string to appear in the logs to indicate that the booting has finished.
    2. The problems began when we changed the supply chain app's log message
    from mentioning Cactus to Cacti which then invalidated the condition forever.
    3. This left it hanging indefinitely and the test case was broken.
    4. Now to fix the problem and avoid this happening again in the future
    the supply chain app exports the log message pattern as a variable and the
    test case imports that directly so if we change the log message in the
    future it will automatically make sure that the test is also waiting for
    the updated log message pattern to show up in the logs making this class
    of bugs impossible to happen.
    5. Also sneaking in a hot-fix for a missing test coverage environment
    variable for the test package of the manual consortium plugin which was
    uncovered by this pull request's execution by chance.
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- examples/cactus-example-supply-chain-backend/src/main/typescript/public-api.ts
+++ examples/cactus-example-supply-chain-backend/src/main/typescript/public-api.ts
@@ -1,3 +1,4 @@
+export { SUPPLY_CHAIN_APP_OK_LOG_MSG_PATTERN } from "./supply-chain-app";
 export { SupplyChainApp } from "./supply-chain-app";
 export { ISupplyChainAppOptions } from "./supply-chain-app";
 export { launchApp } from "./supply-chain-app-cli";
