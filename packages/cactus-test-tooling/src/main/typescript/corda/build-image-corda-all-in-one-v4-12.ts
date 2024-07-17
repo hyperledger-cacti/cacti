5528e292b1386c3aedd7fab1c13798407dfb0e7b
Peter Somogyvari <peter.somogyvari@accenture.com>
2024-07-16 18:59:08 -0700

    test(test-tooling): add container image builder utilities
    1. Currently our integration tests depend on pre-published container
    images to be on the official registry (ghcr.io). This has pros and cons.
    The pro is that we can pin the tests to a specific ledger version and
    then have confidence that the test code works with that specific image.
    On the other hand if the image itself has problems we won't know it until
    after it was published and then tests were executed with it (unless we
    perform manual testing which is a lot of effrot as it requires the
    manual modification of the test cases).
    2. In order to gives us the ability to test against the container image
    definitions as they are in the current revision of the source code,
    we are adding here a couple of utility functions to streamline writing
    test cases that build the container images for themselves as part of the
    test case.
    
    An example of how to use it in a test case:
    
    ```typescript
    const imgConnectorJvm = await buildImageConnectorCordaServer({
        logLevel,
    });
    
    // ...
    
    connector = new CordaConnectorContainer({
        logLevel,
        imageName: imgConnectorJvm.imageName,
        imageVersion: imgConnectorJvm.imageVersion,
        envVars: [envVarSpringAppJson],
    });
    
    ```
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

---
+++ packages/cactus-test-tooling/src/main/typescript/corda/build-image-corda-all-in-one-v4-12.ts
@@ -0,0 +1,49 @@
+import path from "node:path";
+import { buildContainerImage } from "../public-api";
+import { LoggerProvider, LogLevelDesc } from "@hyperledger/cactus-common";
+
+export interface IBuildImageCordaAllInOneV412Response {
+  readonly imageName: Readonly<string>;
+  readonly imageVersion: Readonly<string>;
+  /**
+   * The concatenation of `imageName` a colon character and `imageVersion`.
+   */
+  readonly imageTag: Readonly<string>;
+}
+
+export interface IBuildImageCordaAllInOneV412Request {
+  readonly logLevel?: Readonly<LogLevelDesc>;
+}
+
+export async function buildImageCordaAllInOneV412(
+  req: IBuildImageCordaAllInOneV412Request,
+): Promise<IBuildImageCordaAllInOneV412Response> {
+  if (!req) {
+    throw new Error("Expected arg req to be truthy.");
+  }
+  const logLevel: LogLevelDesc = req.logLevel || "WARN";
+  const log = LoggerProvider.getOrCreate({
+    level: logLevel,
+    label: "build-image-connector-corda-server.ts",
+  });
+  const projectRoot = path.join(__dirname, "../../../../../../../");
+
+  const buildDirRel = "./tools/docker/corda-all-in-one/corda-v4_12/";
+
+  const buildDirAbs = path.join(projectRoot, buildDirRel);
+
+  log.info("Invoking container build with build dir: %s", buildDirAbs);
+
+  const imageName = "caio412";
+  const imageVersion = "latest";
+  const imageTag = `${imageName}:${imageVersion}`;
+
+  await buildContainerImage({
+    buildDir: buildDirAbs,
+    imageFile: "Dockerfile",
+    imageTag,
+    logLevel: logLevel,
+  });
+
+  return { imageName, imageVersion, imageTag };
+}
