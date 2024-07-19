49826f567285d9d03e535d18bbc712c26ca6780d
Peter Somogyvari <peter.somogyvari@accenture.com>
2024-07-19 11:53:28 -0700

    test(plugin-keychain-google-sm): migrate remaining tape tests to Jest
    1. Also turned off tape test execution completely for the package in ci.yaml
    This would've been a no-op anyway just wasting resources since we have no
    tests left that need tape at all.
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-plugin-keychain-google-sm/src/test/typescript/integration/plugin-keychain-google-sm.test.ts
+++ packages/cactus-plugin-keychain-google-sm/src/test/typescript/integration/plugin-keychain-google-sm.test.ts
@@ -1,14 +1,12 @@
-import test, { Test } from "tape-promise/tape";
+import { AddressInfo } from "node:net";
+import { randomUUID } from "node:crypto";
+import "jest-extended";
 import express from "express";
 import bodyParser from "body-parser";
 import http from "http";
-import { AddressInfo } from "net";
 import { StatusCodes } from "http-status-codes";
 
 import { IListenOptions, Servers } from "@hyperledger/cactus-common";
-
-import { v4 as uuidv4 } from "uuid";
-
 import { LogLevelDesc } from "@hyperledger/cactus-common";
 
 import {
@@ -25,10 +23,10 @@ import { SecretManagerServiceClientMock } from "../mock/plugin-keychain-google-s
 
 const logLevel: LogLevelDesc = "TRACE";
 
-test("get,set,has,delete alters state as expected", async (t: Test) => {
+describe("IPluginKeychainGoogleSm", () => {
   const options: IPluginKeychainGoogleSmOptions = {
-    instanceId: uuidv4(),
-    keychainId: uuidv4(),
+    instanceId: randomUUID(),
+    keychainId: randomUUID(),
     logLevel: logLevel,
     backend: new SecretManagerServiceClientMock({
       logLevel: logLevel,
@@ -44,93 +42,93 @@ test("get,set,has,delete alters state as expected", async (t: Test) => {
     port: 0,
     server,
   };
-  const addressInfo = (await Servers.listen(listenOptions)) as AddressInfo;
-  test.onFinish(async () => await Servers.shutdown(server));
-  const { address, port } = addressInfo;
-  const apiHost = `http://${address}:${port}`;
-  const config = new Configuration({ basePath: apiHost });
-  const apiClient = new KeychainGoogleSmApi(config);
 
-  await plugin.registerWebServices(expressApp);
+  let apiClient: KeychainGoogleSmApi;
 
-  t.equal(plugin.getKeychainId(), options.keychainId, "Keychain ID set OK");
-  t.equal(plugin.getInstanceId(), options.instanceId, "Instance ID set OK");
+  afterAll(async () => await Servers.shutdown(server));
 
-  /**
-   * To implement this the key should consist of parent and secretId seperated by ?
-   * For example, key = "projects/my-project?my-secret"
-   */
-  const key = `${uuidv4()}?${uuidv4()}`;
-  const value = uuidv4();
-  console.log(key);
-  console.log(value);
-
-  const res1 = await apiClient.hasKeychainEntryV1({ key });
-  t.true(res1.status >= 200, "res1.status >= 200 OK");
-  t.true(res1.status < 300, "res1.status < 300");
-
-  t.ok(res1.data, "res1.data truthy OK");
-  t.false(res1.data.isPresent, "res1.data.isPresent === false OK");
-  t.ok(res1.data.checkedAt, "res1.data.checkedAt truthy OK");
-  t.equal(res1.data.key, key, "res1.data.key === key OK");
-
-  const res2 = await apiClient.setKeychainEntryV1({
-    key: key,
-    value: value,
+  beforeAll(async () => {
+    const addressInfo = (await Servers.listen(listenOptions)) as AddressInfo;
+    const { address, port } = addressInfo;
+    const apiHost = `http://${address}:${port}`;
+    const config = new Configuration({ basePath: apiHost });
+    apiClient = new KeychainGoogleSmApi(config);
+    await plugin.registerWebServices(expressApp);
   });
-  t.true(res2.status >= 200, "res2.status >= 200 OK");
-  t.true(res2.status < 300, "res2.status < 300 OK");
-  t.notOk(res2.data, "res2.data truthy OK");
 
-  const res3 = await apiClient.hasKeychainEntryV1({ key });
-  t.true(res3.status >= 200, "res3.status >= 200 OK");
-  t.true(res3.status < 300, "res3.status < 300 OK");
-  t.ok(res3.data, "res3.data truthy OK");
-  t.true(res3.data.isPresent, "res3.data.isPresent === true OK");
-  t.ok(res3.data.checkedAt, "res3.data.checkedAt truthy OK");
-  t.equal(res3.data.key, key, "res3.data.key === key OK");
+  it("get,set,has,delete alters state as expected", async () => {
+    expect(plugin.getKeychainId()).toEqual(options.keychainId);
+    expect(plugin.getInstanceId()).toEqual(options.instanceId);
 
-  const res4 = await apiClient.getKeychainEntryV1({
-    key: key,
+    /**
+     * To implement this the key should consist of parent and secretId seperated by ?
+     * For example, key = "projects/my-project?my-secret"
+     */
+    const key = `${randomUUID()}?${randomUUID()}`;
+    const value = randomUUID();
+    console.log(key);
+    console.log(value);
+
+    const res1 = await apiClient.hasKeychainEntryV1({ key });
+    expect(res1.status >= 200).toBeTrue();
+    expect(res1.status < 300).toBeTrue();
+
+    expect(res1.data).toBeTruthy();
+    expect(res1.data.isPresent).toBeFalse();
+    expect(res1.data.checkedAt).toBeTruthy();
+    expect(res1.data.key).toEqual(key);
+
+    const res2 = await apiClient.setKeychainEntryV1({
+      key: key,
+      value: value,
+    });
+    expect(res2.status >= 200).toBeTrue();
+    expect(res2.status < 300).toBeTrue();
+    expect(res2.data).toBeFalsy();
+
+    const res3 = await apiClient.hasKeychainEntryV1({ key });
+    expect(res3.status >= 200).toBeTrue();
+    expect(res3.status < 300).toBeTrue();
+    expect(res3.data).toBeTruthy();
+    expect(res3.data.isPresent).toBeTrue();
+    expect(res3.data.checkedAt).toBeTruthy();
+    expect(res3.data.key).toEqual(key);
+
+    const res4 = await apiClient.getKeychainEntryV1({
+      key: key,
+    });
+    expect(res4.status >= 200).toBeTrue();
+    expect(res4.status < 300).toBeTrue();
+    expect(res4.data).toBeTruthy();
+    //t.equal(res4.data.value, value, "res4.data.value === value OK");
+    //add try catch code and then add what the error message should be -- because of the exception wrapping
+    //t.equals currently does not pass the test case
+
+    const res5 = await apiClient.deleteKeychainEntryV1({ key });
+    expect(res5.status >= 200).toBeTrue();
+    expect(res5.status < 300).toBeTrue();
+    expect(res5.data).toBeFalsy();
+
+    const res6 = await apiClient.hasKeychainEntryV1({ key });
+    expect(res6.status >= 200).toBeTrue();
+    expect(res6.status < 300).toBeTrue();
+    expect(res6.data).toBeTruthy();
+    expect(res6.data.isPresent).toBeFalse();
+    expect(res6.data.checkedAt).toBeTruthy();
+    expect(res6.data.key).toEqual(key);
+    try {
+      await apiClient.getKeychainEntryV1({ key });
+    } catch (ex) {
+      expect(ex).toBeTruthy();
+      expect(ex.response).toBeTruthy();
+      expect(ex.response.data).toBeTruthy();
+      expect(ex.response.data.error).toBeTruthy();
+      expect(
+        ex.response.data.error.includes(`${key} secret not found`),
+      ).toBeTrue();
+
+      expect(ex.response.status).toEqual(StatusCodes.NOT_FOUND);
+      expect(ex.response.data.success).toBeFalsy();
+    }
   });
-  t.true(res4.status >= 200, "res4.status >= 200 OK");
-  t.true(res4.status < 300, "res4.status < 300 OK");
-  t.ok(res4.data, "res4.data truthy OK");
-  //t.equal(res4.data.value, value, "res4.data.value === value OK");
-  //add try catch code and then add what the error message should be -- because of the exception wrapping
-  //t.equals currently does not pass the test case
-
-  const res5 = await apiClient.deleteKeychainEntryV1({ key });
-  t.true(res5.status >= 200, "res5.status >= 200 OK");
-  t.true(res5.status < 300, "res5.status < 300 OK");
-  t.notOk(res5.data, "res5.data falsy OK");
-
-  const res6 = await apiClient.hasKeychainEntryV1({ key });
-  t.true(res6.status >= 200, "res6.status >= 200 OK");
-  t.true(res6.status < 300, "res6.status < 300 OK");
-  t.ok(res6.data, "res6.data truthy OK");
-  t.false(res6.data.isPresent, "res6.data.isPresent === false OK");
-  t.ok(res6.data.checkedAt, "res6.data.checkedAt truthy OK");
-  t.equal(res6.data.key, key, "res6.data.key === key OK");
-  try {
-    await apiClient.getKeychainEntryV1({ key });
-  } catch (out) {
-    t.ok(out, "error thrown for not found endpoint truthy OK");
-    t.ok(out.response, "deploy contract response truthy OK");
-    t.ok(out.response.data, "out.response.data truthy OK");
-    t.ok(out.response.data.error, "out.response.data.error truthy OK");
-    t.true(
-      out.response.data.error.includes(`${key} secret not found`),
-      "HTTP 404 response for non-existent key contains legible error message OK",
-    );
-
-    t.equal(
-      out.response.status,
-      StatusCodes.NOT_FOUND,
-      "deploy contract response status === 404 OK",
-    );
-    t.notok(out.response.data.success, "out.response.data.success falsy OK");
-  }
-
-  t.end();
 });
