4ee2bad01095d47883984c57b755ea36b36ef3a8
Carlos Amaro <64661289+LordKubaya@users.noreply.github.com>
2024-07-24 17:45:00 +0100

    feat(satp-hermes): implement satp stages 1, 2, 3
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    feat(satp-hermes): fabric and besu bridges, wrapper contracts with ontology (#3382)
    
    feat(satp-hermes): added forge to compile solidity contracts
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    fix(satp-hermes): added generated code (#3443)
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    fix(satp-hermes): run sequencial instead of paralel (#3445)
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-handlers/stage2-handler.ts
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-handlers/stage2-handler.ts
@@ -1,58 +1,110 @@
-/*
 import { ConnectRouter, HandlerContext } from "@connectrpc/connect";
 import { SatpStage2Service } from "../../generated/proto/cacti/satp/v02/stage_2_connect";
-import { LockAssertionRequestMessage } from "../../generated/proto/cacti/satp/v02/stage_2_pb";
-import { SATPGateway } from "../../plugin-satp-hermes-gateway";
 import { Stage2ServerService } from "../stage-services/server/stage2-server-service";
-import { TimestampType, saveTimestamp } from "../session-utils";
-import { MessageType } from "../../generated/proto/cacti/satp/v02/common/message_pb";
 import { SATPSession } from "../satp-session";
-import { ServiceType } from "@bufbuild/protobuf";
 import { SupportedChain } from "../types";
+import { SATPHandler, SATPHandlerOptions } from "../../types/satp-protocol";
+import { Logger, LoggerProvider } from "@hyperledger/cactus-common";
+import {
+  LockAssertionReceiptMessage,
+  LockAssertionRequestMessage,
+} from "../../generated/proto/cacti/satp/v02/stage_2_pb";
+import { Stage2ClientService } from "../stage-services/client/stage2-client-service";
+import { TransferCommenceResponseMessage } from "../../generated/proto/cacti/satp/v02/stage_1_pb";
+export class Stage2SATPHandler implements SATPHandler {
+  public static readonly CLASS_NAME = "Stage2SATPHandler";
+  private session: SATPSession;
+  private serverService: Stage2ServerService;
+  private clientService: Stage2ClientService;
+  private supportedDLTs: SupportedChain[];
+  private logger: Logger;
 
-export const Stage2Handler =
-  (
-    session: SATPSession,
-    service: Stage2ServerService,
-    connectClients: ServiceType[],
-    supportedDLTs: SupportedChain[],
-  ) =>
-  (router: ConnectRouter) =>
-    router.service(SatpStage2Service, {
-      async lockAssertion(
-        req: LockAssertionRequestMessage,
-        context: HandlerContext,
-      ) {
-        console.log("Received LockAssertionRequest", req, context);
-        const recvTimestamp: string = Date.now().toString();
+  constructor(ops: SATPHandlerOptions) {
+    this.session = ops.session;
+    this.serverService = ops.serverService as Stage2ServerService;
+    this.clientService = ops.clientService as Stage2ClientService;
+    this.supportedDLTs = ops.supportedDLTs;
+    this.logger = LoggerProvider.getOrCreate(ops.loggerOptions);
+    this.logger.trace(`Initialized ${Stage2SATPHandler.CLASS_NAME}`);
+  }
 
-        const sessionData = await service.checkLockAssertionRequestMessage(
+  public get Log(): Logger {
+    return this.logger;
+  }
+
+  getHandlerIdentifier(): string {
+    return Stage2SATPHandler.CLASS_NAME;
+  }
+
+  getSessionId(): string {
+    return this.session.getSessionData().id;
+  }
+
+  async LockAssertionImplementation(
+    req: LockAssertionRequestMessage,
+    context: HandlerContext,
+  ): Promise<LockAssertionReceiptMessage> {
+    const stepTag = `LockAssertionImplementation()`;
+    const fnTag = `${this.getHandlerIdentifier()}#${stepTag}`;
+    try {
+      this.Log.debug(`${fnTag}, Lock Assertion...`);
+      this.Log.debug(`${fnTag}, Request: ${req}, Context: ${context}`);
+      const sessionData =
+        await this.serverService.checkLockAssertionRequestMessage(
           req,
-          session,
+          this.session,
         );
+      const message = await this.serverService.lockAssertionResponse(
+        req,
+        this.session,
+      );
+      this.Log.debug(`${fnTag}, Returning response: ${message}`);
+      this.Log.debug(`${fnTag}, Session Data: ${sessionData}`);
 
-        saveTimestamp(
-          sessionData,
-          MessageType.LOCK_ASSERT,
-          TimestampType.RECEIVED,
-          recvTimestamp,
-        );
+      if (!message) {
+        throw new Error(`${fnTag}, Failed to create LockAssertionReceipt`);
+      }
+      return message;
+    } catch (error) {
+      throw new Error(`${fnTag}, Failed to process LockAssertion ${error}`);
+    }
+  }
 
-        const message = await service.lockAssertionResponse(req, session);
-
-        if (!message) {
-          throw new Error("No message returned from lockAssertionResponse");
-        }
-
-        saveTimestamp(
-          sessionData,
-          MessageType.ASSERTION_RECEIPT,
-          TimestampType.PROCESSED,
-        );
-
-        console.log("Returning response", message);
-
-        return message;
-      },
+  setupRouter(router: ConnectRouter): void {
+    router.service(SatpStage2Service, {
+      lockAssertion: this.LockAssertionImplementation,
     });
-*/
+  }
+
+  //client side
+  async LockAssertionRequest(
+    response: TransferCommenceResponseMessage,
+  ): Promise<LockAssertionRequestMessage> {
+    const stepTag = `LockAssertionRequest()`;
+    const fnTag = `${this.getHandlerIdentifier()}#${stepTag}`;
+    try {
+      this.Log.debug(`${fnTag}, Lock Assertion Request Message...`);
+      this.Log.debug(`${fnTag}, Response: ${response}`);
+      await this.clientService.checkTransferCommenceResponseMessage(
+        response,
+        this.session,
+      );
+
+      await this.clientService.lockAsset(this.session);
+
+      const request = await this.clientService.lockAssertionRequest(
+        response,
+        this.session,
+      );
+
+      if (!request) {
+        throw new Error(`${fnTag}, Failed to create LockAssertionRequest`);
+      }
+      return request;
+    } catch (error) {
+      throw new Error(
+        `${fnTag}, Failed to process LockAssertionRequest ${error}`,
+      );
+    }
+  }
+}
