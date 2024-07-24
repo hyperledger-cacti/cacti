5042c50cad436647a2e181edec174ce7a8c85907
github-action-benchmark <github@users.noreply.github.com>
2024-07-24 12:42:33 +0000

    add Benchmark (benchmarkjs) benchmark result for 1ce7c605778ef4cc53e78e480e70dc48669ebf5b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721824518651,
+  "lastUpdate": 1721824952927,
   "repoUrl": "https://github.com/hyperledger/cacti",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,37 @@ window.BENCHMARK_DATA = {
             "range": "±1.41%",
             "unit": "ops/sec",
             "extra": "181 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "peter.somogyvari@accenture.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "committer": {
+            "email": "sandeepn.official@gmail.com",
+            "name": "Sandeep Nishad",
+            "username": "sandeepnRES"
+          },
+          "distinct": true,
+          "id": "1ce7c605778ef4cc53e78e480e70dc48669ebf5b",
+          "message": "chore(release): publish v2.0.0-rc.3\n\nSigned-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>",
+          "timestamp": "2024-07-24T17:47:00+05:30",
+          "tree_id": "c4097ca89262a035e7a7f101e17e3da0282158a2",
+          "url": "https://github.com/hyperledger/cacti/commit/1ce7c605778ef4cc53e78e480e70dc48669ebf5b"
+        },
+        "date": 1721824950235,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 726,
+            "range": "±2.48%",
+            "unit": "ops/sec",
+            "extra": "179 samples"
           }
         ]
       }
