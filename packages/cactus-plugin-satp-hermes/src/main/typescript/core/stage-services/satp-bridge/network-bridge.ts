75d39e120915e0a9a4b4ef67abaf7938107e6515
Carlos Amaro <64661289+LordKubaya@users.noreply.github.com>
2024-07-24 17:45:00 +0100

    feat(satp-hermes): implement satp stages
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    Co-authored-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>

---
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-services/satp-bridge/network-bridge.ts
@@ -0,0 +1,51 @@
+import { TransactionResponse } from "../../../types/blockchain-interaction";
+import { Asset } from "./types/asset";
+
+export abstract class NetworkBridge {
+  network!: string;
+
+  public networkName(): string {
+    return this.network;
+  }
+
+  public abstract wrapAsset(asset: Asset): Promise<TransactionResponse>;
+
+  public abstract unwrapAsset(assetId: string): Promise<TransactionResponse>;
+
+  public abstract lockAsset(
+    assetId: string,
+    amount: number,
+  ): Promise<TransactionResponse>;
+
+  public abstract unlockAsset(
+    assetId: string,
+    amount: number,
+  ): Promise<TransactionResponse>;
+
+  public abstract mintAsset(
+    assetId: string,
+    amount: number,
+  ): Promise<TransactionResponse>;
+
+  public abstract burnAsset(
+    assetId: string,
+    amount: number,
+  ): Promise<TransactionResponse>;
+
+  public abstract assignAsset(
+    assetId: string,
+    to: string,
+    amount: number,
+  ): Promise<TransactionResponse>;
+
+  public abstract runTransaction(
+    methodName: string,
+    params: string[],
+    invocationType: unknown,
+  ): Promise<TransactionResponse>;
+
+  public abstract getReceipt(
+    assetId: string,
+    transactionId: string,
+  ): Promise<string>;
+}
