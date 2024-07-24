afaa5122a668a55662c04775e3418ce05369302a
Carlos Amaro <64661289+LordKubaya@users.noreply.github.com>
2024-07-24 17:45:00 +0100

    feat(satp-hermes): implement satp stages 1, 2, 3
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    feat(satp-hermes): fabric and besu bridges,
    wrapper contracts with ontology (#3382)
    feat(satp-hermes): added forge to compile solidity
    contracts
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    fix(satp-hermes): added generated code (#3443)
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    fix(satp-hermes): run sequencial instead of
    paralel (#3445)
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-services/satp-bridge/satp-bridge-manager.ts
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-services/satp-bridge/satp-bridge-manager.ts
@@ -1,26 +1,200 @@
 // this file contains a class that encapsulates the logic for managing the SATP bridge (lock, unlock, etc).
 // should inject satp gateway session data (having parameters/chains for transactions), and processes smart contract output
+import { BridgeManager } from "./bridge-manager";
+import { Logger, LoggerProvider } from "@hyperledger/cactus-common";
+import { SATPBridgeConfig } from "../../types";
+import { Asset } from "./types/asset";
 
-import { SATPLedgerConnector } from "../../../types/blockchain-interaction";
+export class SatpBridgeManager implements BridgeManager {
+  public static readonly CLASS_NAME = "FabricBridgeManager";
 
-export abstract class SATPBridgeManager {
-  // Instantiate connectors based on supported implementations, at bridge level
-  private connectors: SATPLedgerConnector[] = [];
+  private _log: Logger;
 
-  public abstract lockAsset(
-    sessionId: string,
+  public get log(): Logger {
+    return this._log;
+  }
+
+  constructor(private config: SATPBridgeConfig) {
+    const level = "INFO";
+    const label = SatpBridgeManager.CLASS_NAME;
+    this._log = LoggerProvider.getOrCreate({ level, label });
+  }
+
+  public async wrapAsset(asset: Asset): Promise<string> {
+    const fnTag = `${this.className}#wrap()`;
+
+    const response = await this.config.network.wrapAsset(asset);
+
+    if (response.transactionId == undefined) {
+      throw new Error(`${fnTag}, transactionId is undefined`);
+    }
+
+    const receipt = this.config.network.getReceipt(
+      asset.tokenId,
+      response.transactionId,
+    );
+
+    this.log.info(`${fnTag}, proof of the asset wrapping: ${receipt}`);
+
+    return receipt;
+  }
+  public async unwrapAsset(assetId: string): Promise<string> {
+    const fnTag = `${this.className}#unwrap()`;
+
+    const response = await this.config.network.unwrapAsset(assetId);
+
+    if (response.transactionId == undefined) {
+      throw new Error(`${fnTag}, transactionId is undefined`);
+    }
+
+    const receipt = this.config.network.getReceipt(
+      assetId,
+      response.transactionId,
+    );
+
+    this.log.info(`${fnTag}, proof of the asset unwrapping: ${receipt}`);
+
+    return receipt;
+  }
+
+  public get className(): string {
+    return SatpBridgeManager.CLASS_NAME;
+  }
+
+  public async lockAsset(assetId: string, amount: number): Promise<string> {
+    const fnTag = `${this.className}#lockAsset()`;
+
+    const response = await this.config.network.lockAsset(assetId, amount);
+
+    if (response.transactionId == undefined) {
+      throw new Error(`${fnTag}, transactionId is undefined`);
+    }
+
+    const receipt = await this.config.network.getReceipt(
+      assetId,
+      response.transactionId,
+    );
+
+    this.log.info(`${fnTag}, proof of the asset lock: ${receipt}`);
+
+    return receipt;
+  }
+
+  public async unlockAsset(assetId: string, amount: number): Promise<string> {
+    const fnTag = `${this.className}#unlockAsset()`;
+
+    const response = await this.config.network.unlockAsset(assetId, amount);
+
+    if (response.transactionId == undefined) {
+      throw new Error(`${fnTag}, transactionId is undefined`);
+    }
+
+    const receipt = await this.config.network.getReceipt(
+      assetId,
+      response.transactionId,
+    );
+
+    this.log.info(`${fnTag}, proof of the asset unlock: ${receipt}`);
+
+    return receipt;
+  }
+
+  public async mintAsset(assetId: string, amount: number): Promise<string> {
+    const fnTag = `${this.className}#mintAsset()`;
+
+    const transaction = await this.config.network.mintAsset(assetId, amount);
+
+    if (transaction.transactionId == undefined) {
+      throw new Error(`${fnTag}, transactionId is undefined`);
+    }
+
+    const receipt = await this.config.network.getReceipt(
+      assetId,
+      transaction.transactionId,
+    );
+
+    this.log.info(`${fnTag}, proof of the asset creation: ${receipt}`);
+
+    return receipt;
+  }
+
+  public async burnAsset(assetId: string, amount: number): Promise<string> {
+    const fnTag = `${this.className}#burnAsset()`;
+
+    const transaction = await this.config.network.burnAsset(assetId, amount);
+
+    if (transaction.transactionId == undefined) {
+      throw new Error(`${fnTag}, transactionId is undefined`);
+    }
+
+    const receipt = await this.config.network.getReceipt(
+      assetId,
+      transaction.transactionId,
+    );
+
+    this.log.info(`${fnTag}, proof of the asset deletion: ${receipt}`);
+
+    return receipt;
+  }
+
+  public async assignAsset(
     assetId: string,
-  ): Promise<string>;
-  public abstract unlockAsset(
-    sessionId: string,
+    recipient: string,
+    amount: number,
+  ): Promise<string> {
+    const fnTag = `${this.className}#assignAsset()`;
+
+    const response = await this.config.network.assignAsset(
+      assetId,
+      recipient,
+      amount,
+    );
+
+    if (response.transactionId == undefined) {
+      throw new Error(`${fnTag}, transactionId is undefined`);
+    }
+
+    const receipt = await this.config.network.getReceipt(
+      assetId,
+      response.transactionId,
+    );
+
+    this.log.info(`${fnTag}, proof of the asset assignment: ${receipt}`);
+
+    return receipt;
+  }
+  public async verifyAssetExistence(
     assetId: string,
-  ): Promise<string>;
-  public abstract mintAsset(
-    sessionId: string,
+    invocationType: unknown,
+  ): Promise<boolean | undefined> {
+    //todo: implement this
+    const assetExists = await this.config.network.runTransaction(
+      "AssetExists",
+      [assetId],
+      invocationType,
+    );
+
+    if (assetExists == undefined) {
+      return false;
+    }
+
+    return true;
+  }
+  public async verifyLockAsset(
     assetId: string,
-  ): Promise<string>;
-  public abstract burnAsset(
-    sessionId: string,
-    assetId: string,
-  ): Promise<string>;
+    invocationType: unknown,
+  ): Promise<boolean | undefined> {
+    //todo: implement this
+    const lockAsset = await this.config.network.runTransaction(
+      "LockAsset",
+      [assetId],
+      invocationType,
+    );
+
+    if (lockAsset.output == undefined) {
+      return false;
+    }
+
+    return true;
+  }
 }
