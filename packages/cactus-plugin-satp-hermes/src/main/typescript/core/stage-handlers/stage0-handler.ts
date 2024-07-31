8de69e924c06b5affe875ce62e404138de335e53
Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
2024-07-31 17:16:44 +0100

    test(satp-hermes): add service tests
    bug correction
    
    session functionalities
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    feat(satp-hermes): errors file and other fixes
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    feat(satp-hermes): fix packages versions
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-handlers/stage0-handler.ts
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-handlers/stage0-handler.ts
@@ -14,78 +14,77 @@ import { Stage0ClientService } from "../stage-services/client/stage0-client-serv
 
 export class Stage0SATPHandler implements SATPHandler {
   public static readonly CLASS_NAME = "Stage0SATPHandler";
-  private session: SATPSession;
+  private sessions: Map<string, SATPSession>;
   private serverService: Stage0ServerService;
   private clientService: Stage0ClientService;
   private logger: Logger;
   constructor(ops: SATPHandlerOptions) {
-    this.session = ops.session;
+    this.sessions = ops.sessions;
     this.serverService = ops.serverService as Stage0ServerService;
     this.clientService = ops.clientService as Stage0ClientService;
     this.logger = LoggerProvider.getOrCreate(ops.loggerOptions);
     this.logger.trace(`Initialized ${Stage0SATPHandler.CLASS_NAME}`);
+  }
+  getHandlerSessions(): string[] {
+    return Array.from(this.sessions.keys());
   }
 
   getHandlerIdentifier(): string {
     return Stage0SATPHandler.CLASS_NAME;
   }
 
-  getSessionId(): string {
-    return this.session.getSessionData().id;
-  }
+  // async PreTransferProposalImplementation(
+  //   req: PreTransferVerificationAndContextEstablishmentRequest,
+  // ): Promise<PreTransferVerificationAndContextEstablishmentResponse> {
+  //   try {
+  //     console.log("Received TransferProposalRequest", req);
+  //     const sessionData =
+  //       await this.serverService.checkPreTransferProposalRequestMessage(
+  //         req,
+  //         this.session,
+  //       );
+  //     const message = await this.serverService.preTransferProposalResponse(
+  //       req,
+  //       this.session,
+  //     );
+  //     console.log("message", message);
+  //     console.log("Returning response", sessionData);
+  //     const response =
+  //       new PreTransferVerificationAndContextEstablishmentResponse();
+  //     return response;
+  //   } catch (error) {
+  //     console.error("Error handling TransferProposalRequest:", error);
+  //     throw new Error("Failed to process TransferProposalRequest");
+  //   }
+  // }
 
-  async PreTransferProposalImplementation(
-    req: PreTransferVerificationAndContextEstablishmentRequest,
-  ): Promise<PreTransferVerificationAndContextEstablishmentResponse> {
-    try {
-      console.log("Received TransferProposalRequest", req);
-      const sessionData =
-        await this.serverService.checkPreTransferProposalRequestMessage(
-          req,
-          this.session,
-        );
-      const message = await this.serverService.preTransferProposalResponse(
-        req,
-        this.session,
-      );
-      console.log("message", message);
-      console.log("Returning response", sessionData);
-      const response =
-        new PreTransferVerificationAndContextEstablishmentResponse();
-      return response;
-    } catch (error) {
-      console.error("Error handling TransferProposalRequest:", error);
-      throw new Error("Failed to process TransferProposalRequest");
-    }
-  }
-
-  async PreTransferCommenceImplementation(
-    req: PreTransferCommenceRequestMessage,
-  ): Promise<PreTransferCommenceResponseMessage> {
-    try {
-      console.log("Received TransferCommenceRequest", req);
-      const sessionData =
-        await this.serverService.checkTransferCommenceRequestMessage(
-          req,
-          this.session,
-        );
-      const message = await this.serverService.transferCommenceResponse(
-        req,
-        this.session,
-      );
-      console.log("Returning response", message);
-      console.log("Returning response", sessionData);
-      const response = new PreTransferCommenceResponseMessage();
-      return response;
-    } catch (error) {
-      console.error("Error handling TransferCommenceRequest:", error);
-      throw new Error("Failed to process TransferCommenceRequest");
-    }
-  }
+  // async PreTransferCommenceImplementation(
+  //   req: PreTransferCommenceRequestMessage,
+  // ): Promise<PreTransferCommenceResponseMessage> {
+  //   try {
+  //     console.log("Received TransferCommenceRequest", req);
+  //     const sessionData =
+  //       await this.serverService.checkTransferCommenceRequestMessage(
+  //         req,
+  //         this.session,
+  //       );
+  //     const message = await this.serverService.transferCommenceResponse(
+  //       req,
+  //       this.session,
+  //     );
+  //     console.log("Returning response", message);
+  //     console.log("Returning response", sessionData);
+  //     const response = new PreTransferCommenceResponseMessage();
+  //     return response;
+  //   } catch (error) {
+  //     console.error("Error handling TransferCommenceRequest:", error);
+  //     throw new Error("Failed to process TransferCommenceRequest");
+  //   }
+  // }
   setupRouter(router: ConnectRouter): void {
-    router.service(SatpStage0Service, {
-      preTransferProposalClaims: this.PreTransferProposalImplementation,
-      preTransferCommence: this.PreTransferCommenceImplementation,
-    });
+    //   router.service(SatpStage0Service, {
+    //     preTransferProposalClaims: this.PreTransferProposalImplementation,
+    //     preTransferCommence: this.PreTransferCommenceImplementation,
+    //   });
   }
 }
