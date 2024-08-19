29a461db361eb5763b675761e44a0414a965bcd7
Bruno Mateus <brumat315@gmail.com>
2024-08-19 12:43:30 +0100

    feat(satp-hermes): add satp runner
    Signed-off-by: Bruno Mateus <brumat315@gmail.com>
    
    refactor(satp-hermes): gateway container image definition with bundler
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>
    
    refactor(satp-hermes): merge policies type guard example
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>
    
    refactor(satp-hermes): type guards for SATP env variables
    
    Signed-off-by: Bruno Mateus <brumat315@gmail.com>
    
    squash! - peter's fixes for besu connectivity use LAN IP
    
    Instead of hardcoded localhost use the LAN IP of machine so that the
    gateway container can access it too.
    
    Why though? Because if you tell the gateway container that it should
    access the besu ledger via localhost then it will try to do that through
    the container's own localhost, which is different from the host machine's
    localhost (where the besu ledger is actually running).
    
    Using the actual IP address of the host machine's primary network interface
    resolves the ambiguity between the two differnet localhosts.
    
    Co-authored-by: Peter Somogyvari <peter.somogyvari@accenture.com>
    Co-authored-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

---
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/config-validating-functions/bridges-config-validating-functions/validatePluginRegistryOptions.ts
@@ -0,0 +1,123 @@
+import { LogLevelDesc } from "@hyperledger/cactus-common";
+import { isLogLevelDesc } from "../validateSatpLogLevel";
+import { PluginRegistry } from "@hyperledger/cactus-core";
+import { PluginKeychainMemory } from "@hyperledger/cactus-plugin-keychain-memory";
+
+export interface KeychainBackendEntry {
+  keychainEntry: string;
+  keychainEntryValue: string;
+}
+
+export interface KeychainOptionsJSON {
+  instanceId: string;
+  keychainId: string;
+  logLevel?: LogLevelDesc;
+  backend?: KeychainBackendEntry[];
+  contractName?: string;
+  contractString?: string;
+}
+
+export interface PluginRegistryOptionsJSON {
+  logLevel?: LogLevelDesc;
+  plugins?: KeychainOptionsJSON[];
+}
+
+// Type guard for the KeychainBackendEntry
+function isKeychainBackendEntry(obj: unknown): obj is KeychainBackendEntry {
+  if (typeof obj !== "object" || obj === null) {
+    return false;
+  }
+  const objRecord = obj as Record<string, unknown>;
+  return (
+    "keychainEntry" in obj &&
+    typeof objRecord.keychainEntry === "string" &&
+    "keychainEntryValue" in obj &&
+    typeof objRecord.keychainEntryValue === "string"
+  );
+}
+
+// Type guard for an array of KeychainBackendEntry
+function isKeychainBackendEntryArray(
+  input: unknown,
+): input is Array<KeychainBackendEntry> {
+  return Array.isArray(input) && input.every(isKeychainBackendEntry);
+}
+
+// Type guard for KeychainOptionsJSON
+function isKeychainOptionsJSON(obj: unknown): obj is KeychainOptionsJSON {
+  if (typeof obj !== "object" || obj === null) {
+    return false;
+  }
+  const objRecord = obj as Record<string, unknown>;
+  return (
+    "instanceId" in obj &&
+    typeof objRecord.instanceId === "string" &&
+    "keychainId" in obj &&
+    typeof objRecord.keychainId === "string" &&
+    (!("logLevel" in obj) || isLogLevelDesc(objRecord.logLevel)) &&
+    (!("backend" in obj) || isKeychainBackendEntryArray(objRecord.backend)) &&
+    (!("contractName" in obj) || typeof objRecord.contractName === "string") &&
+    (!("contractString" in obj) || typeof objRecord.contractString === "string")
+  );
+}
+
+// Type guard for an array of KeychainOptionsJSON
+function isKeychainOptionsJSONArray(
+  input: unknown,
+): input is Array<KeychainOptionsJSON> {
+  return Array.isArray(input) && input.every(isKeychainOptionsJSON);
+}
+
+// Type guard for PluginRegistryOptionsJSON
+export function isPluginRegistryOptionsJSON(
+  obj: unknown,
+): obj is PluginRegistryOptionsJSON {
+  if (typeof obj !== "object" || obj === null) {
+    return false;
+  }
+  const objRecord = obj as Record<string, unknown>;
+  return (
+    (!("logLevel" in obj) || isLogLevelDesc(objRecord.logLevel)) &&
+    (!("plugins" in obj) || isKeychainOptionsJSONArray(objRecord.plugins))
+  );
+}
+
+type KeyValuePair = [string, string];
+
+// Function to create PluginRegistry from PluginRegistryOptionsJSON
+export function createPluginRegistry(
+  pluginRegistryOptions?: PluginRegistryOptionsJSON,
+  logLevel?: LogLevelDesc,
+): PluginRegistry {
+  if (
+    pluginRegistryOptions === undefined ||
+    pluginRegistryOptions.plugins === undefined
+  ) {
+    return new PluginRegistry();
+  }
+
+  const plugins: PluginKeychainMemory[] = [];
+  pluginRegistryOptions.plugins.forEach(async (pluginJSON) => {
+    const entryValuesArray: KeyValuePair[] = [];
+    pluginJSON.backend?.forEach((entry) => {
+      entryValuesArray.push([entry.keychainEntry, entry.keychainEntryValue]);
+    });
+    const backend: Map<string, string> = new Map(entryValuesArray);
+    const newPluginKeychainMemory = new PluginKeychainMemory({
+      instanceId: pluginJSON.instanceId,
+      keychainId: pluginJSON.keychainId,
+      logLevel,
+      backend,
+    });
+    if (pluginJSON.contractName && pluginJSON.contractString) {
+      await newPluginKeychainMemory.set(
+        pluginJSON.contractName,
+        pluginJSON.contractString,
+      );
+    }
+    plugins.push(newPluginKeychainMemory);
+  });
+  return new PluginRegistry({
+    plugins: plugins,
+  });
+}
