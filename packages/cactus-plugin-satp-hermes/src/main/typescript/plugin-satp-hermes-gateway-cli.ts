224183528e919604b7de1441e58cbd7ef3c83e9b
Bruno Mateus <brumat315@gmail.com>
2024-08-19 12:43:30 +0100

    feat(satp-hermes): docker and gateway runner for SATP
    Signed-off-by: Bruno Mateus <brumat315@gmail.com>
    
    refactor(satp-hermes): gateway container image definition with bundler
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>
    
    refactor(satp-hermes): merge policies type guard example
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>
    
    refactor(satp-hermes): type guards for SATP env variables
    
    Signed-off-by: Bruno Mateus <brumat315@gmail.com>
    
    squash! - peter's fixes for besu connectivity use LAN IP
    
    Instead of hardcoded localhost use the LAN IP of machine so that the
    gateway container can access it too.
    
    Why though? Because if you tell the gateway container that it should
    access the besu ledger via localhost then it will try to do that through
    the container's own localhost, which is different from the host machine's
    localhost (where the besu ledger is actually running).
    
    Using the actual IP address of the host machine's primary network interface
    resolves the ambiguity between the two differnet localhosts.
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>
    
    Co-authored-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/plugin-satp-hermes-gateway-cli.ts
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/plugin-satp-hermes-gateway-cli.ts
@@ -1,63 +1,140 @@
 #!/usr/bin/env node
 
-import { LoggerProvider, LogLevelDesc } from "@hyperledger/cactus-common";
+import { LoggerProvider } from "@hyperledger/cactus-common";
 import { SATPGateway, SATPGatewayConfig } from "./plugin-satp-hermes-gateway";
-import { SupportedChain, DraftVersions, CurrentDrafts } from "./core/types";
-import dotenv from "dotenv";
+import fs from "fs-extra";
 
-export async function launchGateway(env?: NodeJS.ProcessEnv): Promise<void> {
-  dotenv.config();
+import { validateSatpGatewayIdentity } from "./config-validating-functions/validateSatpGatewayIdentity";
+import { validateSatpCounterPartyGateways } from "./config-validating-functions/validateSatpCounterPartyGateways";
+import { validateSatpLogLevel } from "./config-validating-functions/validateSatpLogLevel";
+import { validateSatpEnvironment } from "./config-validating-functions/validateSatpEnvironment";
+import { validateSatpEnableOpenAPI } from "./config-validating-functions/validateSatpEnableOpenAPI";
+import { validateSatpValidationOptions } from "./config-validating-functions/validateSatpValidationOptions";
+import { validateSatpPrivacyPolicies } from "./config-validating-functions/validateSatpPrivacyPolicies";
+import { validateSatpMergePolicies } from "./config-validating-functions/validateSatpMergePolicies";
+import { validateSatpKeyPairJSON } from "./config-validating-functions/validateKeyPairJSON";
+import { validateSatpBridgesConfig } from "./config-validating-functions/validateSatpBridgesConfig";
+import path from "path";
 
-  const logLevel: LogLevelDesc =
-    (env?.SATP_LOG_LEVEL as LogLevelDesc) || "INFO";
+export async function launchGateway(): Promise<void> {
   const logger = LoggerProvider.getOrCreate({
-    level: logLevel,
+    level: "DEBUG",
     label: "SATP-Gateway",
   });
 
-  // Parse the version string into DraftVersions object
-  const parseVersion = (versionString: string): DraftVersions => {
-    const [Core, Architecture, Crash] = versionString.split(",");
-    return {
-      [CurrentDrafts.Core]: Core,
-      [CurrentDrafts.Architecture]: Architecture,
-      [CurrentDrafts.Crash]: Crash,
-    };
-  };
+  logger.debug("Checking for configuration file...");
+  let configFilePath: string | undefined;
 
+  const possiblePaths = [
+    "/opt/cacti/satp-hermes/gateway-config.json",
+    "/gateway-config.json",
+    path.join(process.cwd(), "gateway-config.json"),
+  ];
+
+  for (const path of possiblePaths) {
+    if (fs.existsSync(path)) {
+      configFilePath = path;
+      logger.debug(`Found gateway-config.json at: ${path}`);
+      break;
+    }
+  }
+
+  if (!configFilePath) {
+    throw new Error(
+      `Could not find gateway-config.json in any of the expected locations: ${possiblePaths.join(", ")}`,
+    );
+  }
+
+  logger.debug(`Reading configuration from: ${configFilePath}`);
+  const config = await fs.readJson(configFilePath);
+  logger.debug(`Configuration read OK`);
+
+  // validating gateway-config.json
+
+  logger.debug("Validating SATP Gateway Identity...");
+  const gid = validateSatpGatewayIdentity({
+    configValue: config.gid,
+  });
+  logger.debug("Valid SATP Gateway Identity");
+
+  logger.debug("Validating SATP Counter Party Gateways...");
+  const counterPartyGateways = validateSatpCounterPartyGateways({
+    configValue: config.counterPartyGateways,
+  });
+  logger.debug("Valid SATP Counter Party Gateways");
+
+  logger.debug("Validating SATP Log Level...");
+  const logLevel = validateSatpLogLevel({
+    configValue: config.logLevel,
+  });
+  logger.debug("SATP Log Level is valid.");
+
+  logger.debug("Validating SATP Environment...");
+  const environment = validateSatpEnvironment({
+    configValue: config.environment,
+  });
+  logger.debug("SATP Environment is valid.");
+
+  logger.debug("Validating SATP Enable OpenAPI...");
+  const enableOpenAPI = validateSatpEnableOpenAPI({
+    configValue: config.enableOpenAPI,
+  });
+  logger.debug("SATP Enable OpenAPI is valid.");
+
+  logger.debug("Validating SATP Validation Options...");
+  const validationOptions = validateSatpValidationOptions({
+    configValue: config.validationOptions,
+  });
+  logger.debug("SATP Validation Options is valid.");
+
+  logger.debug("Validating SATP Privacy Policies...");
+  const privacyPolicies = validateSatpPrivacyPolicies({
+    configValue: config.validationOptions,
+  });
+  logger.debug("SATP Privacy Policies is valid.");
+  privacyPolicies.forEach((p, i) =>
+    logger.debug("Privacy Policy #%d => %o", i, p),
+  );
+
+  logger.debug("Validating SATP Merge Policies...");
+  const mergePolicies = validateSatpMergePolicies({
+    configValue: config.mergePolicies,
+  });
+  logger.debug("SATP Merge Policies is valid.");
+  mergePolicies.forEach((p, i) => logger.debug("Merge Policy #%d => %o", i, p));
+
+  logger.debug("Validating SATP KeyPair...");
+  const keyPair = validateSatpKeyPairJSON({
+    configValue: config.gatewayKeyPair,
+  });
+  logger.debug("SATP KeyPair is valid.");
+
+  logger.debug("Validating SATP Bridges Config...");
+  const bridgesConfig = validateSatpBridgesConfig({
+    configValue: config.bridgesConfig,
+  });
+  logger.debug("SATP Bridges Config is valid.");
+
+  logger.debug("Creating SATPGatewayConfig...");
   const gatewayConfig: SATPGatewayConfig = {
-    gid: {
-      id: env?.SATP_GATEWAY_ID || "",
-      name: env?.SATP_GATEWAY_NAME,
-      version: env?.SATP_GATEWAY_VERSION
-        ? [parseVersion(env.SATP_GATEWAY_VERSION)]
-        : [],
-      supportedDLTs:
-        env?.SATP_SUPPORTED_DLTS?.split(",").map(
-          (dlt) => dlt as SupportedChain,
-        ) || [],
-      proofID: env?.SATP_PROOF_ID,
-      gatewayServerPort: parseInt(env?.SATP_GATEWAY_SERVER_PORT || "0", 10),
-      gatewayClientPort: parseInt(env?.SATP_GATEWAY_CLIENT_PORT || "0", 10),
-      gatewayOpenAPIPort: parseInt(env?.DEFAULT_PORT_GATEWAY_API || "0", 10),
-      address: env?.SATP_GATEWAY_ADDRESS as
-        | `http://${string}`
-        | `https://${string}`
-        | undefined,
-    },
-    counterPartyGateways: JSON.parse(env?.SATP_COUNTER_PARTY_GATEWAYS || "[]"),
+    gid,
+    counterPartyGateways,
     logLevel,
-    keyPair: {
-      privateKey: Buffer.from(env?.SATP_PRIVATE_KEY || "", "hex"),
-      publicKey: Buffer.from(env?.SATP_PUBLIC_KEY || "", "hex"),
-    },
-    environment:
-      (env?.SATP_NODE_ENV as "development" | "production") || "development",
-    enableOpenAPI: env?.SATP_ENABLE_OPEN_API === "true",
-    validationOptions: JSON.parse(env?.SATP_VALIDATION_OPTIONS || "{}"),
-    privacyPolicies: JSON.parse(env?.SATP_PRIVACY_POLICIES || "[]"),
-    mergePolicies: JSON.parse(env?.SATP_MERGE_POLICIES || "[]"),
+    keyPair:
+      keyPair === undefined
+        ? undefined
+        : {
+            publicKey: Buffer.from(keyPair.publicKey, "hex"),
+            privateKey: Buffer.from(keyPair.privateKey, "hex"),
+          },
+    environment,
+    enableOpenAPI,
+    validationOptions,
+    privacyPolicies,
+    mergePolicies,
+    bridgesConfig,
   };
+  logger.debug("SATPGatewayConfig created successfully");
 
   const gateway = new SATPGateway(gatewayConfig);
   try {
@@ -72,5 +149,5 @@ export async function launchGateway(env?: NodeJS.ProcessEnv): Promise<void> {
 }
 
 if (require.main === module) {
-  launchGateway(process.env);
+  launchGateway();
 }
