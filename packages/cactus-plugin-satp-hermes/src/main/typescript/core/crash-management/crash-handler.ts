20870629238dbfcc184b485817f4fedf3cc587bd
Yogesh01000100 <yogeshone678@gmail.com>
2024-08-20 21:08:54 +0000

    feat(satp-hermes): add crash recovery & rollback protocol
    1. Implemented recovery & rollback using RPC-based message handlers.
    2. Added rollback strategies for all SATP stages.
    3. Integrated database log management for recovery and rollback.
    4. Added cron jobs for scheduled crash detection and recovery initiation.
    
    Co-authored-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    Co-authored-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    chore(satp-hermes): improve DB management
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    chore(satp-hermes): crash recovery architecture
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    fix(recovery): enhance crash recovery and rollback implementation
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    refactor(recovery): consolidate logic and improve SATP message handling
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    feat(recovery): add rollback implementations
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    fix: correct return types and inits
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    fix: add unit tests and resolve rollbackstate
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    feat: add function processing logs from g2
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    feat: add cron schedule for periodic crash checks
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    fix: resolve rollback condition and add tests
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    feat: add orchestrator communication layer using connect-RPC
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    feat: add rollback protocol rpc
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    fix: handle server log synchronization
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    fix: resolve gol errors, add unit tests
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    fix: handle server-side rollback
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    fix: resolve networkId in rollback strategies
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>

---
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/core/crash-management/crash-handler.ts
@@ -0,0 +1,146 @@
+import { ConnectRouter } from "@connectrpc/connect";
+import { Logger } from "@hyperledger/cactus-common";
+import {
+  CrashRecovery,
+  RecoverSuccessMessageResponse,
+} from "../../generated/proto/cacti/satp/v02/crash_recovery_pb";
+import { CrashRecoveryServerService } from "./server-service";
+import { CrashRecoveryClientService } from "./client-service";
+import {
+  RecoverMessage,
+  RecoverUpdateMessage,
+  RecoverSuccessMessage,
+  RollbackMessage,
+  RollbackAckMessage,
+  RollbackState,
+} from "../../generated/proto/cacti/satp/v02/crash_recovery_pb";
+import { SATPHandler, SATPHandlerType } from "../../types/satp-protocol";
+import { SessionData } from "../../generated/proto/cacti/satp/v02/common/session_pb";
+
+export class CrashRecoveryHandler implements SATPHandler {
+  private readonly log: Logger;
+
+  constructor(
+    private readonly serverService: CrashRecoveryServerService,
+    private readonly clientService: CrashRecoveryClientService,
+    log: Logger,
+  ) {
+    this.log = log;
+    this.log.trace(`Initialized ${CrashRecoveryHandler.name}`);
+  }
+
+  public getHandlerIdentifier(): SATPHandlerType {
+    return SATPHandlerType.CRASH;
+  }
+
+  public getHandlerSessions(): string[] {
+    return [];
+  }
+
+  public getStage(): string {
+    return "crash";
+  }
+
+  // Server-side
+
+  private async recoverV2MessageImplementation(
+    req: RecoverMessage,
+  ): Promise<RecoverUpdateMessage> {
+    const fnTag = `${CrashRecoveryHandler.name}#recoverV2MessageImplementation`;
+    this.log.debug(`${fnTag} - Handling RecoverMessage: ${req}`);
+    try {
+      return await this.serverService.handleRecover(req);
+    } catch (error) {
+      this.log.error(`${fnTag} - Error:`, error);
+      throw error;
+    }
+  }
+
+  private async recoverV2SuccessMessageImplementation(
+    req: RecoverSuccessMessage,
+  ): Promise<RecoverSuccessMessageResponse> {
+    const fnTag = `${CrashRecoveryHandler.name}#recoverV2SuccessMessageImplementation`;
+    this.log.debug(`${fnTag} - Handling RecoverSuccessMessage:${req}`);
+    try {
+      return await this.serverService.handleRecoverSuccess(req);
+    } catch (error) {
+      this.log.error(`${fnTag} - Error:`, error);
+      throw error;
+    }
+  }
+
+  private async rollbackV2MessageImplementation(
+    req: RollbackMessage,
+  ): Promise<RollbackAckMessage> {
+    const fnTag = `${CrashRecoveryHandler.name}#rollbackV2MessageImplementation`;
+    this.log.debug(`${fnTag} - Handling RollbackMessage: ${req}`);
+    try {
+      return await this.serverService.handleRollback(req);
+    } catch (error) {
+      this.log.error(`${fnTag} - Error:`, error);
+      throw error;
+    }
+  }
+
+  public setupRouter(router: ConnectRouter): void {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const that = this;
+    router.service(CrashRecovery, {
+      async recoverV2Message(req) {
+        return await that.recoverV2MessageImplementation(req);
+      },
+      async recoverV2SuccessMessage(req) {
+        return await that.recoverV2SuccessMessageImplementation(req);
+      },
+      async rollbackV2Message(req) {
+        return await that.rollbackV2MessageImplementation(req);
+      },
+    });
+
+    this.log.info("Router setup completed for CrashRecoveryHandler");
+  }
+
+  // Client-side
+
+  public async sendRecoverMessage(
+    session: SessionData,
+  ): Promise<RecoverMessage> {
+    const fnTag = `${this.constructor.name}#createRecoverMessage`;
+    try {
+      return this.clientService.createRecoverMessage(session);
+    } catch (error) {
+      this.log.error(`${fnTag} - Failed to create RecoverMessage: ${error}`);
+      throw new Error(`Error in createRecoverMessage: ${error}`);
+    }
+  }
+
+  public async sendRecoverSuccessMessage(
+    session: SessionData,
+  ): Promise<RecoverSuccessMessage> {
+    const fnTag = `${this.constructor.name}#createRecoverSuccessMessage`;
+    try {
+      return await this.clientService.createRecoverSuccessMessage(session);
+    } catch (error) {
+      this.log.error(
+        `${fnTag} - Failed to create RecoverSuccessMessage: ${error}`,
+      );
+      throw new Error(`Error in createRecoverSuccessMessage: ${error}`);
+    }
+  }
+
+  public async sendRollbackMessage(
+    session: SessionData,
+    rollbackState: RollbackState,
+  ): Promise<RollbackMessage> {
+    const fnTag = `${this.constructor.name}#createRollbackMessage`;
+    try {
+      return await this.clientService.createRollbackMessage(
+        session,
+        rollbackState,
+      );
+    } catch (error) {
+      this.log.error(`${fnTag} - Failed to create RollbackMessage: ${error}`);
+      throw new Error(`Error in createRollbackMessage: ${error}`);
+    }
+  }
+}
