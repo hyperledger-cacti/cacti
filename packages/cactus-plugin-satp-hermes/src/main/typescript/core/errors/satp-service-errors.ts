f56c32561b1fca4b333d0ac100e6dea8976e32a3
Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
2024-08-23 17:12:08 +0100

    test(satp-hermes): add e2e tests
    Co-authored-by: eduv09 <eduardovasques10@tecnico.ulisboa.pt>
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    fix(satp-hermes): error messages in stage0
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    feat(satp-hermes): service tests added stage0 and error handling
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    feat(satp-hermes): test e2e
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    fix(satp-hermes): fix bugs and test e2e working transfer
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    fix(stap-hermes): bugs and 2 gateway test
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/core/errors/satp-service-errors.ts
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/core/errors/satp-service-errors.ts
@@ -1,168 +1,299 @@
 import { SATPInternalError } from "./satp-errors";
 
 export class SatpCommonBodyError extends SATPInternalError {
-  constructor(fnTag: string, data: string) {
+  constructor(tag: string, data: string) {
     super(
-      `${fnTag}, message satp common body is missing or is missing required fields \n ${data}`,
+      `${tag}, message satp common body is missing or is missing required fields \n ${data}`,
       400,
     );
   }
 }
 export class SessionError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, session undefined`, 500);
+  constructor(tag: string) {
+    super(`${tag}, session undefined`, 500);
+  }
+}
+
+export class SessionIdError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, session id undefined`, 500);
+  }
+}
+
+export class SessionMissMatchError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, session missmatch`, 500);
   }
 }
 
 export class SessionDataNotLoadedCorrectlyError extends SATPInternalError {
-  constructor(fnTag: string, data: string) {
-    super(`${fnTag}, session data was not loaded correctly \n ${data}`, 500);
-  }
-}
-
-export class SessionCompletedError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, session data already completed`, 500);
-  }
-}
-
-export class SATPVersionError extends SATPInternalError {
-  constructor(fnTag: string, unsupported: string, supported: string) {
+  constructor(tag: string, data: string, stack?: Error) {
     super(
-      `${fnTag}, unsupported SATP version \n received: ${unsupported}, supported: ${supported}`,
-      400,
+      `${tag}, session data was not loaded correctly \n ${data} \n stack: ${stack} `,
+      500,
     );
   }
 }
 
+export class SessionDataNotAvailableError extends SATPInternalError {
+  constructor(tag: string, type: string) {
+    super(`${tag}, ${type} session data not available`, 500);
+  }
+}
+
+export class SessionCompletedError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, session data already completed`, 500);
+  }
+}
+
+export class SATPVersionError extends SATPInternalError {
+  constructor(tag: string, unsupported?: string, supported?: string) {
+    if (!supported) {
+      super(`${tag}, SATP version is missing`, 400);
+    } else {
+      super(
+        `${tag}, unsupported SATP version \n received: ${unsupported}, supported: ${supported}`,
+        400,
+      );
+    }
+  }
+}
+
+export class SignatureAlgorithmError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, signature algorithm is missing`, 400);
+  }
+}
+
 export class SignatureVerificationError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, message signature verification failed`, 400);
+  constructor(tag: string) {
+    super(`${tag}, message signature verification failed`, 400);
   }
 }
 
 export class SignatureMissingError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, message signature missing`, 400);
+  constructor(tag: string) {
+    super(`${tag}, message signature missing`, 400);
+  }
+}
+
+export class LockTypeError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, lock type missing`, 400);
+  }
+}
+
+export class lockExpirationTimeError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, lock expiration time missing`, 400);
+  }
+}
+
+export class CredentialProfileError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, credential profile missing`, 400);
+  }
+}
+
+export class LoggingProfileError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, logging profile missing`, 400);
+  }
+}
+
+export class AccessControlProfileError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, access control profile missing`, 400);
   }
 }
 
 export class MessageTypeError extends SATPInternalError {
-  constructor(fnTag: string, received: string, expected: string) {
+  constructor(tag: string, received: string, expected: string) {
     super(
-      `${fnTag}, message type miss match \n received: ${received} \n expected: ${expected}`,
+      `${tag}, message type miss match \n received: ${received} \n expected: ${expected}`,
       400,
     );
   }
 }
 
 export class TransferInitClaimsError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, transferInitClaims missing or faulty`, 400);
+  constructor(tag: string) {
+    super(`${tag}, transferInitClaims missing or faulty`, 400);
   }
 }
 
 export class TransferInitClaimsHashError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, transferInitClaims hash missing or missmatch`, 400);
+  constructor(tag: string) {
+    super(`${tag}, transferInitClaims hash missing or missmatch`, 400);
   }
 }
 
 export class NetworkCapabilitiesError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, NetworkCapabilitiesError missing or faulty`, 400);
+  constructor(tag: string) {
+    super(`${tag}, NetworkCapabilitiesError missing or faulty`, 400);
   }
 }
 
 export class DLTNotSupportedError extends SATPInternalError {
-  constructor(fnTag: string, dlt: string) {
-    super(`${fnTag}, DLT not supported \n received: ${dlt}`, 400);
+  constructor(tag: string, dlt: string) {
+    super(`${tag}, DLT not supported \n received: ${dlt}`, 400);
   }
 }
 
 export class ServerGatewayPubkeyError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, serverGatewayPubkey missing or missmatch`, 400);
+  constructor(tag: string) {
+    super(`${tag}, serverGatewayPubkey missing or missmatch`, 400);
   }
 }
 
 export class ClientGatewayPubkeyError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, clientGatewayPubkey missing or missmatch`, 400);
+  constructor(tag: string) {
+    super(`${tag}, clientGatewayPubkey missing or missmatch`, 400);
   }
 }
 
 export class SequenceNumberError extends SATPInternalError {
-  constructor(fnTag: string, received: bigint, expected: bigint) {
+  constructor(tag: string, received: bigint, expected: bigint) {
     super(
-      `${fnTag}, sequence number missmatch \n received: ${received} \n expected: ${expected}`,
+      `${tag}, sequence number missmatch \n received: ${received} \n expected: ${expected}`,
       400,
     );
   }
 }
 
 export class HashError extends SATPInternalError {
-  constructor(fnTag: string, received: string, expected: string) {
+  constructor(tag: string, received: string, expected: string) {
     super(
-      `${fnTag}, hash missmatch \n received: ${received} \n expected: ${expected}`,
+      `${tag}, hash missmatch \n received: ${received} \n expected: ${expected}`,
       400,
     );
   }
 }
 
 export class TransferContextIdError extends SATPInternalError {
-  constructor(fnTag: string, received: string, expected: string) {
-    super(
-      `${fnTag}, transferContextId missing or missmatch \n received: ${received} \n expected: ${expected}`,
-      400,
-    );
+  constructor(tag: string, received?: string, expected?: string) {
+    if (!received || !expected) {
+      super(`${tag}, transferContextId missing or missmatch`, 400);
+    } else {
+      super(
+        `${tag}, transferContextId missing or missmatch \n received: ${received} \n expected: ${expected}`,
+        400,
+      );
+    }
   }
 }
 
 export class MissingBridgeManagerError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, bridge manager missing`, 400);
+  constructor(tag: string) {
+    super(`${tag}, bridge manager missing`, 400);
   }
 }
 
 export class LockAssertionClaimError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, lockAssertionClaim missing or faulty`, 400);
+  constructor(tag: string) {
+    super(`${tag}, lockAssertionClaim missing or faulty`, 400);
   }
 }
 
 export class LockAssertionClaimFormatError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, lockAssertionClaimFormat missing`, 400);
+  constructor(tag: string) {
+    super(`${tag}, lockAssertionClaimFormat missing`, 400);
   }
 }
 
 export class LockAssertionExpirationError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, lockAssertionExpiration missing or faulty`, 400);
+  constructor(tag: string) {
+    super(`${tag}, lockAssertionExpiration missing or faulty`, 400);
   }
 }
 
 export class BurnAssertionClaimError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, burnAssertionClaim missing or faulty`, 400);
+  constructor(tag: string) {
+    super(`${tag}, burnAssertionClaim missing or faulty`, 400);
   }
 }
 
 export class MintAssertionClaimError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, mintAssertionClaim missing or faulty`, 400);
+  constructor(tag: string) {
+    super(`${tag}, mintAssertionClaim missing or faulty`, 400);
   }
 }
 
 export class AssignmentAssertionClaimError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, assignmentAssertionClaim missing or faulty`, 400);
+  constructor(tag: string) {
+    super(`${tag}, assignmentAssertionClaim missing or faulty`, 400);
   }
 }
 
 export class ResourceUrlError extends SATPInternalError {
-  constructor(fnTag: string) {
-    super(`${fnTag}, resourceUrl missing or missmatch`, 400);
+  constructor(tag: string) {
+    super(`${tag}, resourceUrl missing or missmatch`, 400);
+  }
+}
+
+export class GatewayNetworkIdError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, gatewayNetworkId missing or missmatch`, 400);
+  }
+}
+
+export class OntologyContractError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, ontologyContract missing or has problems`, 400);
+  }
+}
+
+export class LedgerAssetIdError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, ledgerAssetId missing`, 400);
+  }
+}
+
+export class LedgerAssetError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, ledgerAsset missing`, 400);
+  }
+}
+
+export class NetworkIdError extends SATPInternalError {
+  constructor(tag: string, type: string) {
+    super(`${tag}, ${type} networkId missing or missmatch`, 400);
+  }
+}
+
+export class AssetMissing extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, asset missing`, 400);
+  }
+}
+
+export class WrapAssertionClaimError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, Wrap Assertion Claim missing or faulty`, 400);
+  }
+}
+
+export class TokenIdMissingError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, tokenId missing`, 400);
+  }
+}
+
+export class MissingRecipientError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, Recipient is missing`, 400);
+  }
+}
+
+export class DigitalAssetIdError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, DigitalAssetId is missing`, 400);
+  }
+}
+
+export class PubKeyError extends SATPInternalError {
+  constructor(tag: string) {
+    super(`${tag}, PubKey is missing`, 400);
   }
 }
