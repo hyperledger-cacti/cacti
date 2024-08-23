817575684fed9bfebadd0542a7a16f61b8a80f78
Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
2024-08-23 17:12:08 +0100

    test(satp-hermes): add e2e tests
    Co-authored-by: eduv09 <eduardovasques10@tecnico.ulisboa.pt>
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    fix(satp-hermes): error messages in stage0
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    feat(satp-hermes): service tests added stage0 and error handling
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    feat(satp-hermes): test e2e
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    fix(satp-hermes): fix bugs and test e2e working transfer
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    
    fix(stap-hermes): bugs and 2 gateway test
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-handlers/stage0-handler.ts
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/core/stage-handlers/stage0-handler.ts
@@ -5,29 +5,42 @@ import {
   SATPHandler,
   SATPHandlerOptions,
   SATPHandlerType,
+  Stage,
 } from "../../types/satp-protocol";
-import { ConnectRouter } from "@connectrpc/connect";
+import { ConnectRouter, HandlerContext } from "@connectrpc/connect";
 import { SatpStage0Service } from "../../generated/proto/cacti/satp/v02/stage_0_connect";
 import {
-  PreTransferCommenceRequestMessage,
-  PreTransferCommenceResponseMessage,
-  PreTransferVerificationAndContextEstablishmentRequest,
-  PreTransferVerificationAndContextEstablishmentResponse,
+  CheckRequest,
+  CheckResponse,
+  NewSessionRequest,
+  NewSessionResponse,
+  PreSATPTransferRequest,
+  PreSATPTransferResponse,
 } from "../../generated/proto/cacti/satp/v02/stage_0_pb";
 import { Stage0ClientService } from "../stage-services/client/stage0-client-service";
-
+import {
+  FailedToCreateMessageError,
+  FailedToProcessError,
+  PubKeyError,
+  SenderGatewayNetworkIdError,
+  SessionNotFoundError,
+} from "../errors/satp-handler-errors";
 export class Stage0SATPHandler implements SATPHandler {
   public static readonly CLASS_NAME = SATPHandlerType.STAGE0;
   private sessions: Map<string, SATPSession>;
   private serverService: Stage0ServerService;
   private clientService: Stage0ClientService;
   private logger: Logger;
+  private pubKeys: Map<string, string>;
+  private gatewayId: string;
   constructor(ops: SATPHandlerOptions) {
     this.sessions = ops.sessions;
     this.serverService = ops.serverService as Stage0ServerService;
     this.clientService = ops.clientService as Stage0ClientService;
     this.logger = LoggerProvider.getOrCreate(ops.loggerOptions);
     this.logger.trace(`Initialized ${Stage0SATPHandler.CLASS_NAME}`);
+    this.pubKeys = ops.pubkeys;
+    this.gatewayId = ops.gatewayId;
   }
   getHandlerSessions(): string[] {
     return Array.from(this.sessions.keys());
@@ -37,58 +50,179 @@ export class Stage0SATPHandler implements SATPHandler {
     return Stage0SATPHandler.CLASS_NAME;
   }
 
-  // async PreTransferProposalImplementation(
-  //   req: PreTransferVerificationAndContextEstablishmentRequest,
-  // ): Promise<PreTransferVerificationAndContextEstablishmentResponse> {
-  //   try {
-  //     console.log("Received TransferProposalRequest", req);
-  //     const sessionData =
-  //       await this.serverService.checkPreTransferProposalRequestMessage(
-  //         req,
-  //         this.session,
-  //       );
-  //     const message = await this.serverService.preTransferProposalResponse(
-  //       req,
-  //       this.session,
-  //     );
-  //     console.log("message", message);
-  //     console.log("Returning response", sessionData);
-  //     const response =
-  //       new PreTransferVerificationAndContextEstablishmentResponse();
-  //     return response;
-  //   } catch (error) {
-  //     console.error("Error handling TransferProposalRequest:", error);
-  //     throw new Error("Failed to process TransferProposalRequest");
-  //   }
-  // }
+  getStage(): string {
+    return Stage.STAGE0;
+  }
 
-  // async PreTransferCommenceImplementation(
-  //   req: PreTransferCommenceRequestMessage,
-  // ): Promise<PreTransferCommenceResponseMessage> {
-  //   try {
-  //     console.log("Received TransferCommenceRequest", req);
-  //     const sessionData =
-  //       await this.serverService.checkTransferCommenceRequestMessage(
-  //         req,
-  //         this.session,
-  //       );
-  //     const message = await this.serverService.transferCommenceResponse(
-  //       req,
-  //       this.session,
-  //     );
-  //     console.log("Returning response", message);
-  //     console.log("Returning response", sessionData);
-  //     const response = new PreTransferCommenceResponseMessage();
-  //     return response;
-  //   } catch (error) {
-  //     console.error("Error handling TransferCommenceRequest:", error);
-  //     throw new Error("Failed to process TransferCommenceRequest");
-  //   }
-  // }
+  public get Log(): Logger {
+    return this.logger;
+  }
+
+  private async NewSessionImplementation(
+    req: NewSessionRequest,
+    context: HandlerContext,
+  ): Promise<NewSessionResponse> {
+    const stepTag = `NewSessionImplementation()`;
+    const fnTag = `${this.getHandlerIdentifier()}#${stepTag}`;
+    try {
+      this.Log.debug(`${fnTag}, New Session...`);
+      this.Log.debug(`${fnTag}, Request: ${req}, Context: ${context}`);
+
+      let session = this.sessions.get(req.sessionId);
+
+      if (req.gatewayId == "") {
+        throw new SenderGatewayNetworkIdError(fnTag);
+      }
+
+      if (!this.pubKeys.has(req.gatewayId)) {
+        throw new PubKeyError(fnTag);
+      }
+
+      session = await this.serverService.checkNewSessionRequest(
+        req,
+        session,
+        this.pubKeys.get(req.gatewayId)!,
+      );
+
+      this.sessions.set(session.getSessionId(), session);
+
+      const message = await this.serverService.newSessionResponse(req, session);
+
+      if (!message) {
+        throw new FailedToCreateMessageError(fnTag, "NewSessionResponse");
+      }
+
+      this.Log.debug(`${fnTag}, Returning response: ${message}`);
+
+      return message;
+    } catch (error) {
+      throw new FailedToCreateMessageError(fnTag, "NewSessionResponse", error);
+    }
+  }
+
+  private async PreSATPTransferImplementation(
+    req: PreSATPTransferRequest,
+    context: HandlerContext,
+  ): Promise<PreSATPTransferResponse> {
+    const stepTag = `PreSATPTransferImplementation()`;
+    const fnTag = `${this.getHandlerIdentifier()}#${stepTag}`;
+    try {
+      this.Log.debug(`${fnTag}, PreSATPTransfer...`);
+      this.Log.debug(`${fnTag}, Request: ${req}, Context: ${context}`);
+
+      const session = this.sessions.get(req.sessionId);
+
+      if (!session) {
+        throw new SessionNotFoundError(fnTag);
+      }
+
+      await this.serverService.checkPreSATPTransferRequest(req, session);
+
+      await this.serverService.wrapToken(session);
+
+      const message = await this.serverService.preSATPTransferResponse(
+        req,
+        session,
+      );
+
+      if (!message) {
+        throw new FailedToCreateMessageError(fnTag, "PreSATPTransferResponse");
+      }
+
+      this.Log.debug(`${fnTag}, Returning response: ${message}`);
+
+      return message;
+    } catch (error) {
+      throw new FailedToCreateMessageError(fnTag, "NewSessionResponse", error);
+    }
+  }
+
   setupRouter(router: ConnectRouter): void {
-    //   router.service(SatpStage0Service, {
-    //     preTransferProposalClaims: this.PreTransferProposalImplementation,
-    //     preTransferCommence: this.PreTransferCommenceImplementation,
-    //   });
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const that = this;
+    router.service(SatpStage0Service, {
+      async check(req: CheckRequest): Promise<CheckResponse> {
+        return new CheckResponse({ check: req.check });
+      },
+      async newSession(req, context): Promise<NewSessionResponse> {
+        return await that.NewSessionImplementation(req, context);
+      },
+      async preSATPTransfer(req, context): Promise<PreSATPTransferResponse> {
+        return await that.PreSATPTransferImplementation(req, context);
+      },
+      // newSession: this.NewSessionImplementation,
+      //preSATPTransfer: this.PreSATPTransferImplementation,
+    });
+  }
+
+  //client side
+
+  public async NewSessionRequest(
+    sessionId: string,
+  ): Promise<NewSessionRequest> {
+    const stepTag = `NewSessionRequest()`;
+    const fnTag = `${this.getHandlerIdentifier()}#${stepTag}`;
+    try {
+      this.Log.debug(`${fnTag}, New Session Request...`);
+
+      const session = this.sessions.get(sessionId);
+
+      if (!session) {
+        throw new SessionNotFoundError(fnTag);
+      }
+
+      const message = await this.clientService.newSessionRequest(
+        session,
+        this.gatewayId,
+      );
+
+      if (!message) {
+        throw new FailedToCreateMessageError(fnTag, "NewSessionRequest");
+      }
+
+      return message;
+    } catch (error) {
+      throw new FailedToProcessError(fnTag, "NewSessionRequest", error);
+    }
+  }
+
+  public async PreSATPTransferRequest(
+    response: NewSessionResponse,
+    sessionId: string,
+  ): Promise<PreSATPTransferRequest> {
+    const stepTag = `PreSATPTransferRequest()`;
+    const fnTag = `${this.getHandlerIdentifier()}#${stepTag}`;
+    try {
+      this.Log.debug(`${fnTag}, Pre SATP Transfer Request...`);
+
+      const session = this.sessions.get(sessionId);
+
+      if (!session) {
+        throw new SessionNotFoundError(fnTag);
+      }
+
+      const newSession = await this.clientService.checkNewSessionResponse(
+        response,
+        session,
+        Array.from(this.sessions.keys()),
+      );
+
+      if (newSession.getSessionId() != session.getSessionId()) {
+        this.sessions.set(newSession.getSessionId(), newSession);
+        this.sessions.delete(session.getSessionId());
+      }
+
+      await this.clientService.wrapToken(session);
+
+      const message = await this.clientService.preSATPTransferRequest(session);
+
+      if (!message) {
+        throw new FailedToCreateMessageError(fnTag, "PreSATPTransferRequest");
+      }
+
+      return message;
+    } catch (error) {
+      throw new FailedToProcessError(fnTag, "PreSATPTransferRequest", error);
+    }
   }
 }
