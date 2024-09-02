a861df71915eae6341e3b4e0b1c9f1dc186be53c
jagpreetsinghsasan <jagpreetsinghsasan@accenture.com>
2024-09-02 07:53:04 +0000

    ci(github): commit parity check based on similarity
        Primary Changes
        ---------------
        1. Added a commit-pr similarity ratio to lessen the check strictness
        2. Added an env var to the workflow to easily control the similarity
           ratio
    
        Changes required to incorporate 1)
        ---------------------------------
        3. Added string-similarity-js package in package.json
    
    Fixes #3470
    
    Signed-off-by: jagpreetsinghsasan <jagpreetsinghsasan@accenture.com>

--- tools/pr-commit-parity.js
+++ tools/pr-commit-parity.js
@@ -1,20 +1,30 @@
+import { stringSimilarity } from "string-similarity-js";
+
 export async function fetchJsonFromUrl(url) {
   const fetchResponse = await fetch(url);
   return fetchResponse.json();
 }
 
-//regex expressions
+// regex expressions
 const PULL_REQ_REQUIREMENTS_REGEX = /\*\*Pull\sRequest\sRequirements(.|\n)*/gim;
 const FIXES_OR_DEPENDS_REGEX = /(Fixes|Depends)(.|\n)*/gim;
 const SIGNED_OFF_REGEX = /(")*Signed-off-by:(.|\s)*/gim;
+const COMMIT_TITLE_REGEX = /.*\n/m;
 const HYPHEN_REGEX = /(-)+/gm;
 const BACKTICK_REGEX = /`+/gm;
-const SPACE_REGEX = / +/gm;
 const COMMIT_TO_BE_REVIEWED_REGEX = /("#*\s*Commit\sto\sbe\sreviewed)/gim;
-const WHITESPACES_HARDCODED_REGEX = /(\r\n|\n|\r|\\r|\\n)/gm;
+const WHITESPACES_HARDCODED_REGEX = /(\r\n|\\r)/gm;
+const NEWLINE_HARDCODED_REGEX = /\\n/gm;
 
 const args = process.argv.slice(2);
+
+// The following 2 lines should be commented to test this script locally
 const pullReqUrl = args[0];
+const ACCEPTABLE_SIMILARITY_RATIO = parseFloat(args[1]);
+
+// These following 2 lines should be un-commented to test this script locally
+// const pullReqUrl = "https://api.github.com/repos/hyperledger/cactus/pulls/3338";
+// const ACCEPTABLE_SIMILARITY_RATIO = 0.9;
 
 const prMetadata = await fetchJsonFromUrl(pullReqUrl);
 const prBodyRaw = JSON.stringify(prMetadata.body);
@@ -29,33 +39,44 @@ commitMessagesMetadata.forEach((commitMessageMetadata) => {
       .replace(HYPHEN_REGEX, "")
       .replace(BACKTICK_REGEX, "")
       .replace(WHITESPACES_HARDCODED_REGEX, "")
-      .replace(FIXES_OR_DEPENDS_REGEX, "")
-      .replace(SPACE_REGEX, ""),
+      .replace(FIXES_OR_DEPENDS_REGEX, ""),
   );
 });
 
 let prBodyStriped = prBodyRaw
   .replace(PULL_REQ_REQUIREMENTS_REGEX, "")
   .replace(FIXES_OR_DEPENDS_REGEX, "")
-  .replace(WHITESPACES_HARDCODED_REGEX, "")
+  .replace(WHITESPACES_HARDCODED_REGEX, "\n")
   .replace(SIGNED_OFF_REGEX, "")
   .replace(HYPHEN_REGEX, "")
   .replace(BACKTICK_REGEX, "")
   .replace(COMMIT_TO_BE_REVIEWED_REGEX, "")
-  .replace(SPACE_REGEX, "");
+  .replace(NEWLINE_HARDCODED_REGEX, "");
 
-let PR_BODY_IN_COMMIT_MESSAGES = false;
+let PR_COMMIT_PARITY = false;
 for (let commitMessageListIndex in commitMessageList) {
   let commitMessage = commitMessageList[commitMessageListIndex];
-  if (commitMessage == prBodyStriped) PR_BODY_IN_COMMIT_MESSAGES = true;
+  if (
+    stringSimilarity(commitMessage, prBodyStriped) >=
+      ACCEPTABLE_SIMILARITY_RATIO ||
+    stringSimilarity(
+      commitMessage.replace(COMMIT_TITLE_REGEX, ""),
+      prBodyStriped,
+    ) >= ACCEPTABLE_SIMILARITY_RATIO
+  )
+    PR_COMMIT_PARITY = true;
 }
 
-if (!PR_BODY_IN_COMMIT_MESSAGES) {
+if (!PR_COMMIT_PARITY) {
   console.error(
-    "PR Body does not match any existing commit message\n" +
-      "Please make sure that the PR Body matches to minimum one of the commit messages\n" +
+    "PR message and commit message are not similar. A general solution for this is to have PR message exactly same as the commit message\n" +
       "Please refer the following PR for reference: https://github.com/hyperledger/cacti/pull/3338\n" +
       "And the commit message here: https://github.com/hyperledger/cacti/pull/3338/commits/47ebdec442d30fa48c8518b876c47c38097cf028\n",
+    "-----------------------------------------------\n\n",
+    "Commit Message List (ignore extra white spaces and new lines)\n" +
+      commitMessageList +
+      "\n----------------------------------------------\nRelevant Pr Description (ignore extra white spaces and new lines)\n" +
+      prBodyStriped,
   );
   process.exit(-1);
 }
