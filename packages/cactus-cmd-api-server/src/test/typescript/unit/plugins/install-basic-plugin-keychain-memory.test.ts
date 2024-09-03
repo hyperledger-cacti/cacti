7fbded802022f0c65145a50f3141d01135047552
Peter Somogyvari <peter.somogyvari@accenture.com>
2024-09-03 10:57:46 -0700

    test(cmd-api-server): jestify install-basic-plugin-keychain-memory.test
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-cmd-api-server/src/test/typescript/unit/plugins/install-basic-plugin-keychain-memory.test.ts
+++ packages/cactus-cmd-api-server/src/test/typescript/unit/plugins/install-basic-plugin-keychain-memory.test.ts
@@ -1,7 +1,10 @@
-import test, { Test } from "tape-promise/tape";
-import { v4 as uuidv4 } from "uuid";
+import path from "node:path";
 
-import { LogLevelDesc } from "@hyperledger/cactus-common";
+import "jest-extended";
+import { v4 as uuidv4 } from "uuid";
+import { StatusCodes } from "http-status-codes";
+
+import { LoggerProvider, LogLevelDesc } from "@hyperledger/cactus-common";
 
 import {
   Configuration,
@@ -15,91 +18,104 @@ import {
   ConfigService,
 } from "../../../../main/typescript/public-api";
 
+import { ICactusApiServerOptions } from "../../../../main/typescript/config/config-service";
+
 import { K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS } from "../../../../main/typescript/prometheus-exporter/metrics";
 
 import { DefaultApi as ApiServerApi } from "../../../../main/typescript/public-api";
-import path from "path";
+import convict from "convict";
 
-const logLevel: LogLevelDesc = "TRACE";
+const logLevel: LogLevelDesc = "INFO";
 
-test("can import plugins at runtime (CLI)", async (t: Test) => {
-  // const pluginsPath = path.join(
-  //   "/tmp/org/hyperledger/cactus/cmd-api-server/install-basic-plugin-keychain-memory_test", // the dir path from the root
-  //   uuidv4(), // then a random directory to ensure proper isolation
-  // );
-  const pluginsPath = path.join(
-    __dirname, // start at the current file's path
-    "../../../../../../../", // walk back up to the project root
-    ".tmp/test/cmd-api-server/runtime-plugin-imports_test", // the dir path from the root
-    uuidv4(), // then a random directory to ensure proper isolation
-  );
-  const pluginManagerOptionsJson = JSON.stringify({ pluginsPath });
-
-  const configService = new ConfigService();
-  const apiServerOptions = await configService.newExampleConfig();
-  apiServerOptions.authorizationProtocol = AuthorizationProtocol.NONE;
-  apiServerOptions.pluginManagerOptionsJson = pluginManagerOptionsJson;
-  apiServerOptions.configFile = "";
-  apiServerOptions.apiCorsDomainCsv = "*";
-  apiServerOptions.apiPort = 0;
-  apiServerOptions.cockpitPort = 0;
-  apiServerOptions.grpcPort = 0;
-  apiServerOptions.crpcPort = 0;
-  apiServerOptions.apiTlsEnabled = false;
-  apiServerOptions.plugins = [
-    {
-      packageName: "@hyperledger/cactus-plugin-keychain-memory",
-      type: PluginImportType.Local,
-      action: PluginImportAction.Install,
-      options: {
-        instanceId: uuidv4(),
-        keychainId: uuidv4(),
-        logLevel,
-      },
-    },
-  ];
-  const config = await configService.newExampleConfigConvict(apiServerOptions);
-
-  const apiServer = new ApiServer({
-    config: config.getProperties(),
+describe("ApiServer", () => {
+  const log = LoggerProvider.getOrCreate({
+    label: "install-basic-plugin-keychain-memory.test.ts",
+    level: logLevel,
   });
 
-  const startResponse = apiServer.start();
-  await t.doesNotReject(startResponse, "started API server dynamic imports OK");
-  t.ok(startResponse, "startResponse truthy OK");
+  let config: convict.Config<ICactusApiServerOptions>;
+  let apiServer: ApiServer;
+  let apiSrvOpts: ICactusApiServerOptions;
 
-  const addressInfoApi = (await startResponse).addressInfoApi;
-  const protocol = apiServerOptions.apiTlsEnabled ? "https" : "http";
-  const { address, port } = addressInfoApi;
-  const apiHost = `${protocol}://${address}:${port}`;
-  t.comment(
-    `Metrics URL: ${apiHost}/api/v1/api-server/get-prometheus-exporter-metrics`,
-  );
-
-  const apiConfig = new Configuration({ basePath: apiHost });
-  const apiClient = new ApiServerApi(apiConfig);
-
-  {
-    const res = await apiClient.getPrometheusMetricsV1();
-    const promMetricsOutput =
-      "# HELP " +
-      K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS +
-      " Total number of plugins imported\n" +
-      "# TYPE " +
-      K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS +
-      " gauge\n" +
-      K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS +
-      '{type="' +
-      K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS +
-      '"} 1';
-    t.ok(res);
-    t.ok(res.data);
-    t.equal(res.status, 200);
-    t.true(
-      res.data.includes(promMetricsOutput),
-      "Total 1 plugins imported as expected. RESULT OK",
+  beforeAll(async () => {
+    const pluginsPath = path.join(
+      __dirname, // start at the current file's path
+      "../../../../../../../", // walk back up to the project root
+      ".tmp/test/cmd-api-server/install-basic-plugin-keychain-memory_test", // the dir path from the root
+      uuidv4(), // then a random directory to ensure proper isolation
     );
-  }
+    log.info("Plugin install dir: %s", pluginsPath);
 
-  test.onFinish(() => apiServer.shutdown());
+    const pluginManagerOptionsJson = JSON.stringify({ pluginsPath });
+
+    const configService = new ConfigService();
+    apiSrvOpts = await configService.newExampleConfig();
+    apiSrvOpts.authorizationProtocol = AuthorizationProtocol.NONE;
+    apiSrvOpts.pluginManagerOptionsJson = pluginManagerOptionsJson;
+    apiSrvOpts.configFile = "";
+    apiSrvOpts.apiCorsDomainCsv = "*";
+    apiSrvOpts.apiPort = 0;
+    apiSrvOpts.cockpitPort = 0;
+    apiSrvOpts.grpcPort = 0;
+    apiSrvOpts.crpcPort = 0;
+    apiSrvOpts.apiTlsEnabled = false;
+    apiSrvOpts.plugins = [
+      {
+        packageName: "@hyperledger/cactus-plugin-keychain-memory",
+        type: PluginImportType.Local,
+        action: PluginImportAction.Install,
+        options: {
+          instanceId: uuidv4(),
+          keychainId: uuidv4(),
+          logLevel,
+        },
+      },
+    ];
+    config = await configService.newExampleConfigConvict(apiSrvOpts);
+  });
+
+  afterAll(async () => {
+    await apiServer.shutdown();
+  });
+
+  it("can import plugins at runtime (CLI)", async () => {
+    apiServer = new ApiServer({
+      config: config.getProperties(),
+    });
+
+    const startResponse = apiServer.start();
+    expect(startResponse).toResolve();
+
+    const addressInfoApi = (await startResponse).addressInfoApi;
+    const protocol = apiSrvOpts.apiTlsEnabled ? "https" : "http";
+    const { address, port } = addressInfoApi;
+    const apiHost = `${protocol}://${address}:${port}`;
+    log.debug(
+      `Metrics URL: ${apiHost}/api/v1/api-server/get-prometheus-exporter-metrics`,
+    );
+
+    const apiConfig = new Configuration({ basePath: apiHost });
+    const apiClient = new ApiServerApi(apiConfig);
+
+    {
+      const res = await apiClient.getPrometheusMetricsV1();
+      const promMetricsOutput =
+        "# HELP " +
+        K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS +
+        " Total number of plugins imported\n" +
+        "# TYPE " +
+        K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS +
+        " gauge\n" +
+        K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS +
+        '{type="' +
+        K_CACTUS_API_SERVER_TOTAL_PLUGIN_IMPORTS +
+        '"} 1';
+
+      expect(res).toMatchObject({
+        data: expect.stringContaining(promMetricsOutput),
+        status: StatusCodes.OK,
+      });
+      expect(res.data.includes(promMetricsOutput)).toBeTrue();
+    }
+  });
 });
