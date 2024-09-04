2a68f45eca95862a67d6ffd4c19cdbcd796d72b1
Peter Somogyvari <peter.somogyvari@accenture.com>
2024-09-04 14:15:12 -0700

    test(api-client): jestify default-consortium-provider.test.ts
    This was the final test case that was still using TAP so now we got to
    turn off the tape invocation in the ci completely which is also a performance
    gain worth mentioning.
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-api-client/src/test/typescript/integration/default-consortium-provider.test.ts
+++ packages/cactus-api-client/src/test/typescript/integration/default-consortium-provider.test.ts
@@ -1,50 +1,61 @@
-import { AddressInfo } from "net";
+import http from "node:http";
+import { AddressInfo } from "node:net";
 
-import test, { Test } from "tape";
+import "jest-extended";
+import { StatusCodes } from "http-status-codes";
 
 import { DefaultApi as ConsortiumManualApi } from "@hyperledger/cactus-plugin-consortium-manual";
-import { LogLevelDesc, Servers } from "@hyperledger/cactus-common";
+import {
+  LoggerProvider,
+  LogLevelDesc,
+  Servers,
+} from "@hyperledger/cactus-common";
 import { DefaultConsortiumProvider } from "../../../main/typescript";
 import { Configuration } from "@hyperledger/cactus-core-api";
 
-test("Reports failures with meaningful information", async (t: Test) => {
-  const logLevel: LogLevelDesc = "TRACE";
+describe("DefaultConsortiumProvider", () => {
+  const logLevel: LogLevelDesc = "SILENT";
 
-  test("Handles timeout/connection refusal transparently", async (t2: Test) => {
-    const httpServer1 = await Servers.startOnPreferredPort(4050);
-    test.onFinish(() => Servers.shutdown(httpServer1));
-    const addressInfo1 = httpServer1.address() as AddressInfo;
-    const apiHost = `http://${addressInfo1.address}:${addressInfo1.port}`;
+  const log = LoggerProvider.getOrCreate({
+    label: "default-consortium-provider.test.ts",
+    level: logLevel,
+  });
 
-    const config = new Configuration({
+  let httpServer1: http.Server;
+  let apiClientConfig: Configuration;
+
+  beforeAll(async () => {
+    httpServer1 = await Servers.startOnPreferredPort(4050);
+    const { address, port } = httpServer1.address() as AddressInfo;
+    const apiHost = `http://${address}:${port}`;
+    log.info("API host: %s", apiHost);
+
+    apiClientConfig = new Configuration({
       basePath: apiHost,
       baseOptions: {
         timeout: 2000,
       },
     });
-
-    const provider = new DefaultConsortiumProvider({
-      logLevel,
-      apiClient: new ConsortiumManualApi(config),
-    });
-
-    try {
-      await provider.get();
-      t2.fail("Provider.get() did not throw despite API errors.");
-    } catch (ex) {
-      t2.ok(ex, "Thrown error truthy OK");
-      t2.ok(ex.message, "Thrown error.message truthy OK");
-      t2.equal(
-        typeof ex.message,
-        "string",
-        "Thrown error.message type string OK",
-      );
-      t2.true(ex.message.includes("timeout"), "Has timeout in msg OK");
-    }
-    t2.end();
   });
 
-  test("Handles 4xx transparently", async (t2: Test) => {
+  afterAll(async () => {
+    await Servers.shutdown(httpServer1);
+  });
+
+  it("Handles timeout/connection refusal transparently", async () => {
+    const provider = new DefaultConsortiumProvider({
+      logLevel,
+      apiClient: new ConsortiumManualApi(apiClientConfig),
+    });
+
+    expect(provider.get()).rejects.toMatchObject({
+      message: expect.stringMatching(
+        new RegExp(`connect ECONNREFUSED (.*):4050`),
+      ),
+    });
+  });
+
+  it("Handles 4xx transparently", async () => {
     const config = new Configuration({
       basePath: "https://httpbin.org/status/400",
     });
@@ -53,24 +64,9 @@ test("Reports failures with meaningful information", async (t: Test) => {
       apiClient: new ConsortiumManualApi(config),
     });
 
-    try {
-      await provider.get();
-      t2.fail("Provider.get() did not throw despite API errors.");
-    } catch (ex) {
-      t2.ok(ex, "Thrown error truthy OK");
-      t2.ok(ex.message, "Thrown error.message truthy OK");
-      t2.equal(
-        typeof ex.message,
-        "string",
-        "Thrown error.message type string OK",
-      );
-      t2.true(
-        ex.message.includes("status code 404"),
-        "Has Status Code in msg OK",
-      );
-    }
-    t2.end();
+    expect(provider.get()).rejects.toMatchObject({
+      code: "ERR_BAD_REQUEST",
+      message: expect.stringContaining("status code " + StatusCodes.NOT_FOUND),
+    });
   });
-
-  t.end();
 });
