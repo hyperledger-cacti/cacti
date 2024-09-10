c2c1fe71a28b5de958ed60a84bcf234c9c1d17ba
github-action-benchmark <github@users.noreply.github.com>
2024-09-10 19:40:50 +0000

    add Benchmark (benchmarkjs) benchmark result for 68d50ba270911d030e3aca8b3fdc891f43267c6b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725877627776,
+  "lastUpdate": 1725997249820,
   "repoUrl": "https://github.com/hyperledger/cacti",
   "entries": {
     "Benchmark": [
@@ -69,6 +69,37 @@ window.BENCHMARK_DATA = {
             "range": "±2.92%",
             "unit": "ops/sec",
             "extra": "180 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "peter.somogyvari@accenture.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "68d50ba270911d030e3aca8b3fdc891f43267c6b",
+          "message": "test(connector-besu): jest migrate get-past-logs-endpoint.test.ts\n\nDepends on #3528 => test(test-tooling): add new besu AIO image builder utility\n\nFixes #3527\n\nSigned-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>",
+          "timestamp": "2024-09-10T12:13:59-07:00",
+          "tree_id": "dc7ad978cb183e18be8943d2d5b906cc45b2270b",
+          "url": "https://github.com/hyperledger/cacti/commit/68d50ba270911d030e3aca8b3fdc891f43267c6b"
+        },
+        "date": 1725997248069,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 690,
+            "range": "±2.69%",
+            "unit": "ops/sec",
+            "extra": "178 samples"
           }
         ]
       }
