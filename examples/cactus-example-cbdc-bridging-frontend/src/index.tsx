f68182356c44489225806163c605e529a7092729
André Augusto <andre.augusto@tecnico.ulisboa.pt>
2024-09-16 21:13:55 +0100

    docs(example-cbdc): update frontend to match new functionality * Separated Transfer functionality into local blockchain transfers and cross-chain transfers * Added new table with the state of token approvals made to the bridge * Highlight cross-chain functionality with red coloring * add helper page with instructions on how to use app
    Signed-off-by: André Augusto <andre.augusto@tecnico.ulisboa.pt>

--- examples/cactus-example-cbdc-bridging-frontend/src/index.tsx
+++ examples/cactus-example-cbdc-bridging-frontend/src/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -8,6 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
 );
