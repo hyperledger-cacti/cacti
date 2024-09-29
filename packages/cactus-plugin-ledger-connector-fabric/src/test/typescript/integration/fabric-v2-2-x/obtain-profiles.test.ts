321eb4e712472596f8efef9e26be4f8adb87b741
adrianbatuto <adrian.batuto@accenture.com>
2024-09-29 08:50:39 +0800

    test(connector-fabric): jestify all remaining test cases
    Primary Changes
    ----------------
    1. Jestified remaining tests for the connector-fabric plugin,
    excluding add-orgs.test.ts (currently skipped).
    2. Removed the tests from taprc and jest.config.js
    
    Fixes #3547
    
    Signed-off-by: adrianbatuto <adrian.batuto@accenture.com>

--- packages/cactus-plugin-ledger-connector-fabric/src/test/typescript/integration/fabric-v2-2-x/obtain-profiles.test.ts
+++ packages/cactus-plugin-ledger-connector-fabric/src/test/typescript/integration/fabric-v2-2-x/obtain-profiles.test.ts
@@ -1,7 +1,4 @@
-// The TAP runner has a bug and prevents the test from passing in the CI when yarn "tap --ts ..."" is used
-// if test is run with ts-node, it successfully executes
-import test, { Test } from "tape-promise/tape";
-
+import "jest-extended";
 import {
   DEFAULT_FABRIC_2_AIO_IMAGE_NAME,
   FABRIC_25_LTS_AIO_FABRIC_VERSION,
@@ -15,54 +12,38 @@ import { LogLevelDesc } from "@hyperledger/cactus-common";
 const testCase = "obtains configuration profiles from Fabric 2.x ledger";
 const logLevel: LogLevelDesc = "TRACE";
 
-test("BEFORE " + testCase, async (t: Test) => {
-  const pruning = pruneDockerAllIfGithubAction({ logLevel });
-  await t.doesNotReject(pruning, "Pruning did not throw OK");
-  t.end();
-});
-
-test(testCase, async (t: Test) => {
-  const ledger = new FabricTestLedgerV1({
-    emitContainerLogs: true,
-    publishAllPorts: true,
-    imageName: DEFAULT_FABRIC_2_AIO_IMAGE_NAME,
-    imageVersion: FABRIC_25_LTS_AIO_IMAGE_VERSION,
-    envVars: new Map([["FABRIC_VERSION", FABRIC_25_LTS_AIO_FABRIC_VERSION]]),
+describe("Obtain configuration profiles test", () => {
+  let ledger: FabricTestLedgerV1;
+  beforeAll(async () => {
+    const pruning = pruneDockerAllIfGithubAction({ logLevel });
+    await expect(pruning).resolves.not.toThrow();
+    ledger = new FabricTestLedgerV1({
+      emitContainerLogs: true,
+      publishAllPorts: true,
+      imageName: DEFAULT_FABRIC_2_AIO_IMAGE_NAME,
+      imageVersion: FABRIC_25_LTS_AIO_IMAGE_VERSION,
+      envVars: new Map([["FABRIC_VERSION", FABRIC_25_LTS_AIO_FABRIC_VERSION]]),
+    });
+    await ledger.start({ omitPull: false });
   });
-  const tearDown = async () => {
+  afterAll(async () => {
     await ledger.stop();
     await ledger.destroy();
-  };
+  });
+  test(`${testCase}`, async () => {
+    const connectionProfile = await ledger.getConnectionProfileOrg1();
+    expect(connectionProfile).toBeTruthy();
+    const connectionProfileOrg1 = await ledger.getConnectionProfileOrgX("org1");
+    expect(connectionProfile).toEqual(connectionProfileOrg1);
 
-  test.onFinish(tearDown);
+    const connectionProfileOrg2 = await ledger.getConnectionProfileOrgX("org2");
+    expect(connectionProfileOrg2).toBeTruthy();
 
-  await ledger.start({ omitPull: false });
+    expect(connectionProfileOrg1).not.toEqual(connectionProfileOrg2);
 
-  const connectionProfile = await ledger.getConnectionProfileOrg1();
-
-  t.ok(connectionProfile, "getConnectionProfileOrg1() out truthy OK");
-
-  const connectionProfileOrg1 = await ledger.getConnectionProfileOrgX("org1");
-  t.isEquivalent(connectionProfile, connectionProfileOrg1);
-
-  const connectionProfileOrg2 = await ledger.getConnectionProfileOrgX("org2");
-  t.ok(connectionProfileOrg2, "getConnectionProfileOrg2() out truthy OK");
-
-  t.notDeepEqual(
-    connectionProfileOrg1,
-    connectionProfileOrg2,
-    "connection profiles are different",
-  );
-
-  //Should return error, as there is no Org3 in the default deployment of Fabric AIO image
-  const error = "/no such container - Could not find the file.*/";
-  await t.rejects(ledger.getConnectionProfileOrgX("org3"), error);
-
-  t.end();
-});
-
-test("AFTER " + testCase, async (t: Test) => {
-  const pruning = pruneDockerAllIfGithubAction({ logLevel });
-  await t.doesNotReject(pruning, "Pruning did not throw OK");
-  t.end();
+    //Should return error, as there is no Org3 in the default deployment of Fabric AIO image
+    const error = "getConnectionProfileOrgX() crashed.";
+    const promise = ledger.getConnectionProfileOrgX("org3");
+    await expect(promise).rejects.toThrow(error);
+  });
 });
