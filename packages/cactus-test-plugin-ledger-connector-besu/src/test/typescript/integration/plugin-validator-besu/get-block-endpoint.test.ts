ffd77be91d12aa755d224a9d77b98a4b62354dfd
krriscode <85664362+krriscode@users.noreply.github.com>
2024-10-28 10:40:43 +0000

    test(test-plugin-ledger-connector-besu): jestify get-block-endpoint
    Migration of get-block-endpoint.ts from tape-promise to Jest.
    Update tests for improved reliability and performance.
    
    ```
    packages/cactus-test-plugin-ledger-connector-besu/src/test/typescript/
    integration/plugin-validator-besu/get-block-endpoint.test.ts
    ```
    
    Fixes: #3500
    
    Co-authored-by: Peter Somogyvari <peter.somogyvari@accenture.com>
    
    Signed-off-by: krriscode <85664362+krriscode@users.noreply.github.com>
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-test-plugin-ledger-connector-besu/src/test/typescript/integration/plugin-validator-besu/get-block-endpoint.test.ts
+++ packages/cactus-test-plugin-ledger-connector-besu/src/test/typescript/integration/plugin-validator-besu/get-block-endpoint.test.ts
@@ -1,4 +1,4 @@
-import test, { Test } from "tape-promise/tape";
+import "jest-extended";
 
 import { v4 as uuidv4 } from "uuid";
 import { createServer } from "http";
@@ -33,117 +33,119 @@ import { PluginRegistry } from "@hyperledger/cactus-core";
 
 import { PluginKeychainMemory } from "@hyperledger/cactus-plugin-keychain-memory";
 
-const testCase = "Test sign transaction endpoint";
-const logLevel: LogLevelDesc = "TRACE";
+const testCase = "Test get block endpoint";
+const logLevel: LogLevelDesc = "INFO";
 
-test("BEFORE " + testCase, async (t: Test) => {
-  const pruning = pruneDockerAllIfGithubAction({ logLevel });
-  await t.doesNotReject(pruning, "Pruning didn't throw OK");
-  t.end();
-});
-
-test(testCase, async (t: Test) => {
-  const keyEncoder: KeyEncoder = new KeyEncoder("secp256k1");
-  const keychainId = uuidv4();
-  const keychainRef = uuidv4();
-
-  const { privateKey } = Secp256k1Keys.generateKeyPairsBuffer();
-  const keyHex = privateKey.toString("hex");
-  const pem = keyEncoder.encodePrivate(keyHex, KeyFormat.Raw, KeyFormat.PEM);
-
-  const keychain = new PluginKeychainMemory({
-    backend: new Map([[keychainRef, pem]]),
-    keychainId,
-    logLevel,
-    instanceId: uuidv4(),
-  });
-
-  const httpServer1 = createServer();
-  await new Promise((resolve, reject) => {
-    httpServer1.once("error", reject);
-    httpServer1.once("listening", resolve);
-    httpServer1.listen(0, "127.0.0.1");
-  });
-  const addressInfo1 = httpServer1.address() as AddressInfo;
-  t.comment(`HttpServer1 AddressInfo: ${JSON.stringify(addressInfo1)}`);
-  const node1Host = `http://${addressInfo1.address}:${addressInfo1.port}`;
-  t.comment(`Cactus Node 1 Host: ${node1Host}`);
-
+describe(testCase, () => {
   const besuTestLedger = new BesuTestLedger();
-  await besuTestLedger.start();
+  let apiServer: ApiServer;
 
-  const tearDown = async () => {
+  beforeAll(async () => {
+    const pruning = pruneDockerAllIfGithubAction({ logLevel });
+    await expect(pruning).resolves.toBeTruthy();
+    await besuTestLedger.start();
+  });
+
+  afterAll(async () => {
     await besuTestLedger.stop();
     await besuTestLedger.destroy();
-  };
-
-  test.onFinish(tearDown);
-
-  const rpcApiHttpHost = await besuTestLedger.getRpcApiHttpHost();
-  const rpcApiWsHost = await besuTestLedger.getRpcApiWsHost();
-
-  // 2. Instantiate plugin registry which will provide the web service plugin with the key value storage plugin
-  const pluginRegistry = new PluginRegistry({ plugins: [keychain] });
-
-  // 3. Instantiate the web service consortium plugin
-  const options: IPluginLedgerConnectorBesuOptions = {
-    instanceId: uuidv4(),
-    rpcApiHttpHost,
-    rpcApiWsHost,
-    pluginRegistry,
-    logLevel,
-  };
-  const pluginValidatorBesu = new PluginLedgerConnectorBesu(options);
-
-  // 4. Create the API Server object that we embed in this test
-  const configService = new ConfigService();
-  const apiServerOptions = await configService.newExampleConfig();
-  apiServerOptions.authorizationProtocol = AuthorizationProtocol.NONE;
-  apiServerOptions.configFile = "";
-  apiServerOptions.apiCorsDomainCsv = "*";
-  apiServerOptions.apiPort = addressInfo1.port;
-  apiServerOptions.cockpitPort = 0;
-  apiServerOptions.grpcPort = 0;
-  apiServerOptions.crpcPort = 0;
-  apiServerOptions.apiTlsEnabled = false;
-  const config = await configService.newExampleConfigConvict(apiServerOptions);
-
-  pluginRegistry.add(pluginValidatorBesu);
-
-  const apiServer = new ApiServer({
-    httpServerApi: httpServer1,
-    config: config.getProperties(),
-    pluginRegistry,
+    await apiServer.shutdown();
   });
 
-  // 5. make sure the API server is shut down when the testing if finished.
-  test.onFinish(() => apiServer.shutdown());
+  afterAll(async () => {
+    const pruning = pruneDockerAllIfGithubAction({ logLevel });
+    await expect(pruning).resolves.toBeTruthy();
+  });
 
-  // 6. Start the API server which is now listening on port A and it's healthcheck works through the main SDK
-  await apiServer.start();
+  test(testCase, async () => {
+    const keyEncoder: KeyEncoder = new KeyEncoder("secp256k1");
+    const keychainId = uuidv4();
+    const keychainRef = uuidv4();
 
-  // 7. Instantiate the main SDK dynamically with whatever port the API server ended up bound to (port 0)
-  t.comment(`AddressInfo: ${JSON.stringify(addressInfo1)}`);
+    const { privateKey } = Secp256k1Keys.generateKeyPairsBuffer();
+    const keyHex = privateKey.toString("hex");
+    const pem = keyEncoder.encodePrivate(keyHex, KeyFormat.Raw, KeyFormat.PEM);
 
-  const request: GetBlockV1Request = {
-    blockHashOrBlockNumber: 0,
-  };
+    const keychain = new PluginKeychainMemory({
+      backend: new Map([[keychainRef, pem]]),
+      keychainId,
+      logLevel,
+      instanceId: uuidv4(),
+    });
 
-  const configuration = new BesuApiClientOptions({ basePath: node1Host });
-  const api = new BesuApiClient(configuration);
+    const httpServer1 = createServer();
+    await new Promise((resolve, reject) => {
+      httpServer1.once("error", reject);
+      httpServer1.once("listening", resolve);
+      httpServer1.listen(0, "127.0.0.1");
+    });
+    const addressInfo1 = httpServer1.address() as AddressInfo;
+    console.log(`HttpServer1 AddressInfo: ${JSON.stringify(addressInfo1)}`);
 
-  // Test for 200 valid response test case
-  const res = await api.getBlockV1(request);
+    const node1Host = `http://${addressInfo1.address}:${addressInfo1.port}`;
+    console.log(`Cactus Node 1 Host: ${node1Host}`);
 
-  const { status, data } = res;
-  t.true(status >= 200, "status GTE 200 OK");
-  t.true(status < 300, "status LT 300 OK");
-  t.ok(data, "GetBlockResponse Truthy OK");
-  t.true(typeof data.block === "object", "Response data is OK");
-});
+    const rpcApiHttpHost = await besuTestLedger.getRpcApiHttpHost();
+    const rpcApiWsHost = await besuTestLedger.getRpcApiWsHost();
 
-test("AFTER " + testCase, async (t: Test) => {
-  const pruning = pruneDockerAllIfGithubAction({ logLevel });
-  await t.doesNotReject(pruning, "Pruning didn't throw OK");
-  t.end();
+    // 2. Instantiate plugin registry which will provide the web service plugin with the key value storage plugin
+    const pluginRegistry = new PluginRegistry({ plugins: [keychain] });
+
+    // 3. Instantiate the web service consortium plugin
+    const options: IPluginLedgerConnectorBesuOptions = {
+      instanceId: uuidv4(),
+      rpcApiHttpHost,
+      rpcApiWsHost,
+      pluginRegistry,
+      logLevel,
+    };
+    const pluginValidatorBesu = new PluginLedgerConnectorBesu(options);
+
+    // 4. Create the API Server object that we embed in this test
+    const configService = new ConfigService();
+    const apiServerOptions = await configService.newExampleConfig();
+    apiServerOptions.authorizationProtocol = AuthorizationProtocol.NONE;
+    apiServerOptions.configFile = "";
+    apiServerOptions.apiCorsDomainCsv = "*";
+    apiServerOptions.apiPort = addressInfo1.port;
+    apiServerOptions.cockpitPort = 0;
+    apiServerOptions.grpcPort = 0;
+    apiServerOptions.crpcPort = 0;
+    apiServerOptions.apiTlsEnabled = false;
+    const config =
+      await configService.newExampleConfigConvict(apiServerOptions);
+
+    pluginRegistry.add(pluginValidatorBesu);
+
+    apiServer = new ApiServer({
+      httpServerApi: httpServer1,
+      config: config.getProperties(),
+      pluginRegistry,
+    });
+
+    // 5. make sure the API server is shut down when the testing if finished.
+
+    // 6. Start the API server which is now listening on port A and it's healthcheck works through the main SDK
+    await apiServer.start();
+
+    // 7. Instantiate the main SDK dynamically with whatever port the API server ended up bound to (port 0)
+    console.log(`AddressInfo: ${JSON.stringify(addressInfo1)}`);
+
+    const request: GetBlockV1Request = {
+      blockHashOrBlockNumber: 0,
+    };
+
+    const configuration = new BesuApiClientOptions({ basePath: node1Host });
+    const api = new BesuApiClient(configuration);
+
+    // Test for 200 valid response test case
+    const res = await api.getBlockV1(request);
+
+    const { status, data } = res;
+
+    expect(status).toBeGreaterThanOrEqual(200);
+    expect(status).toBeLessThan(300);
+    expect(data).toBeTruthy();
+    expect(data.block).toBeObject();
+  });
 });
