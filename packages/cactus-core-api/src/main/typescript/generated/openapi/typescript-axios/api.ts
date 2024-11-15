a1be603c3ceea748579ad96a1d2b38a2438eec8b
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2024-11-15 18:19:32 +0200

    feat(cactus-core-api): add Ethereum ledger type
    Peter's change: Added the missing `ETHEREUM` constant to the leger type
    that the static consortium plugin has built in to it.
    This was necessary because adding it only to the core-api broke the build.
    The two types (since they are duplicates of each other) must be updated in
    tandem otherwise the code won't compile (which is a good thing because it
    is an example if when the compiler saves us from mistakes like this that would
    otherwise stay hidden until much later and only come out as runtime errors)
    
    Authored-by: Bruno Mateus
    
    Co-authored-by: Peter Somogyvari <peter.somogyvari@accenture.com>
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-core-api/src/main/typescript/generated/openapi/typescript-axios/api.ts
+++ packages/cactus-core-api/src/main/typescript/generated/openapi/typescript-axios/api.ts
@@ -524,6 +524,7 @@ export const LedgerType = {
     Besu2X: 'BESU_2X',
     Burrow0X: 'BURROW_0X',
     Corda4X: 'CORDA_4X',
+    Ethereum: 'ETHEREUM',
     Fabric2: 'FABRIC_2',
     Sawtooth1X: 'SAWTOOTH_1X'
 } as const;
