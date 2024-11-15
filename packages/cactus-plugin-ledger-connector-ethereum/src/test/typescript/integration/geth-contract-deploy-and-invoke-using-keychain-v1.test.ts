a7d97462b6491c79cd80f4e70fd6d9fb17748807
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2024-11-15 19:40:36 +0200

    feat(cactus-connector-ethereum): add RunTransactionV1Exchange to share receipt data
    Also improves watch blocks
    
    Authored-by: Bruno Mateus <brumat315@gmail.com>
    Co-authored-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

--- packages/cactus-plugin-ledger-connector-ethereum/src/test/typescript/integration/geth-contract-deploy-and-invoke-using-keychain-v1.test.ts
+++ packages/cactus-plugin-ledger-connector-ethereum/src/test/typescript/integration/geth-contract-deploy-and-invoke-using-keychain-v1.test.ts
@@ -19,6 +19,7 @@ import { v4 as uuidV4 } from "uuid";
 import { AddressInfo } from "net";
 import { Server as SocketIoServer } from "socket.io";
 import Web3, { HexString } from "web3";
+import { Address } from "@ethereumjs/util";
 
 import {
   LogLevelDesc,
@@ -351,11 +352,11 @@ describe("Ethereum contract deploy and invoke using keychain tests", () => {
 
   test("invoke Web3SigningCredentialType.None", async () => {
     const testEthAccount2 = web3.eth.accounts.create();
-
+    const address = Address.fromString(testEthAccount2.address);
     const value = 10e6;
     const { serializedTransactionHex } = signTransaction(
       {
-        to: testEthAccount2.address,
+        to: address,
         value,
         maxPriorityFeePerGas: 0,
         maxFeePerGas: 0x40000000,
