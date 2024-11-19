cbeac7975c857a07332c9a6da96e90d7291d9572
Yogesh01000100 <yogeshone678@gmail.com>
2024-11-19 14:36:19 +0000

    feat: add knex database integration
    Co-authored-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>

--- packages/cactus-plugin-satp-hermes/src/knex/knexfile-remote.ts
+++ packages/cactus-plugin-satp-hermes/src/knex/knexfile-remote.ts
@@ -1,7 +1,10 @@
 import path from "path";
 import { v4 as uuidv4 } from "uuid";
+import dotenv from "dotenv";
 
-// default configuration for knex
+const envPath = process.env.ENV_PATH;
+dotenv.config({ path: envPath });
+
 module.exports = {
   development: {
     client: "sqlite3",
@@ -12,5 +15,18 @@ module.exports = {
       directory: path.resolve(__dirname, "migrations"),
     },
     useNullAsDefault: true,
+  },
+  production: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "migrations"),
+    },
   },
 };
