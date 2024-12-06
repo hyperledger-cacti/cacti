b3aaca5a5adf7707870770fab38b057e3737b12c
Yogesh01000100 <yogeshone678@gmail.com>
2024-12-06 21:16:11 +0000

    fix: add knex config in gateway
    1.Integrated knexLocalConfig and knexRemoteConfig into SATPGatewayConfig
    
    Fixes #3665
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/test/typescript/integration/gateway-blo.test.ts
+++ packages/cactus-plugin-satp-hermes/src/test/typescript/integration/gateway-blo.test.ts
@@ -16,6 +16,10 @@ import {
 } from "../../../main/typescript/core/types";
 import { createClient } from "../test-utils";
 import { HealthCheckResponseStatusEnum } from "../../../main/typescript";
+import {
+  knexClientConnection,
+  knexSourceRemoteConnection,
+} from "../knex.config";
 
 const logLevel: LogLevelDesc = "DEBUG";
 const logger = LoggerProvider.getOrCreate({
@@ -57,6 +61,8 @@ const options: SATPGatewayConfig = {
     gatewayOpenAPIPort: 4010,
     address: "http://localhost",
   },
+  knexLocalConfig: knexClientConnection,
+  knexRemoteConfig: knexSourceRemoteConnection,
 };
 
 describe("GetStatus Endpoint and Functionality testing", () => {
