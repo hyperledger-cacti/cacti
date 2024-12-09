962ef2a01ea0f561b3e91a8cfd45c3af1643f1ed
Daiki.Nakashima <Nakashima.Daiki@df.MitsubishiElectric.co.jp>
2024-12-09 01:38:31 +0000

    <bug>[waver]update network-setups fabric setupCC.sh
    Signed-off-by: Daiki.Nakashima <Nakashima.Daiki@df.MitsubishiElectric.co.jp>

--- weaver/tests/network-setups/fabric/dev/scripts/setupCC.sh
+++ weaver/tests/network-setups/fabric/dev/scripts/setupCC.sh
@@ -4,7 +4,7 @@
 
 directory=$(dirname $0)
 
-CACTI_VERSION=v2.0.0
+CACTI_VERSION="v$(head -n 1 $PWD/../../../../core/network/fabric-interop-cc/contracts/interop/VERSION)"
 TMP_PATH=$PWD/../shared/tmp
 CHAINCODE_PATH=$PWD/../shared/chaincode
 rm -rf $CHAINCODE_PATH/interop
