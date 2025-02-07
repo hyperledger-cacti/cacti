dba6162ad5cedbd6afc8d23a8a9fb1a0c8996ad4
ruzell22 <ruzell.vince.aquino@accenture.com>
2025-02-07 12:55:03 +0800

    ci(github): fix type exports in packages/cacti-plugin-copm-fabric
    Primary Changes
    ---------------
    1. Removed packages/cacti-plugin-copm-fabric/hyperledger-cacti-cacti-plugin-
    copm-fabric-*.tgz in ignore paths in get-all-tgz-path.ts file
    2. Added main, module, types, and files in package.json
    3. While fixing merge conflict, discovered new packages failing and added
    on ignore path and created tickets for it
    
    Fixes: #3629
    
    Signed-off-by: ruzell22 <ruzell.vince.aquino@accenture.com>

--- tools/custom-checks/get-all-tgz-path.ts
+++ tools/custom-checks/get-all-tgz-path.ts
@@ -46,12 +46,14 @@ export async function getAllTgzPath(): Promise<IGetAllTgzPathResponse> {
       "weaver/sdks/fabric/interoperation-node-sdk/hyperledger-cacti-weaver-sdk-fabric-*.tgz",
       // link for issue ticket relating to this package: https://github.com/hyperledger-cacti/cacti/issues/3628
       "packages/cacti-plugin-weaver-driver-fabric/src/main/typescript/hyperledger-cacti-weaver-driver-fabric-*.tgz",
-      // link for issue ticket relating to this package: https://github.com/hyperledger-cacti/cacti/issues/3629
-      "packages/cacti-plugin-copm-fabric/hyperledger-cacti-cacti-plugin-copm-fabric-*.tgz",
       // link for issue ticket relating to this package: https://github.com/hyperledger-cacti/cacti/issues/3632
       "examples/cactus-common-example-server/hyperledger-cactus-common-example-server-*.tgz",
       // link for issue ticket relating to this package: https://github.com/hyperledger-cacti/cacti/issues/3633
       "packages/cactus-verifier-client/hyperledger-cactus-verifier-client-*.tgz",
+      // link for issue ticket relating to this package: https://github.com/hyperledger-cacti/cacti/issues/3740
+      "examples/cactus-example-discounted-asset-trade/hyperledger-cactus-example-discounted-asset-trade-*.tgz",
+      // link for issue ticket relating to this package: https://github.com/hyperledger-cacti/cacti/issues/3741
+      "examples/cactus-example-electricity-trade/hyperledger-cactus-example-electricity-trade-*.tgz",
     ],
   };
 
