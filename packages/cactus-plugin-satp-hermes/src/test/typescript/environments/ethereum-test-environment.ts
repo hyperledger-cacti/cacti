38756a3de761fac90770b969f6042f32b17ec951
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2025-02-26 20:25:30 +0200

    feat(satp-hermes): add monitoring service
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    refactor(satp-hermes): create service folder
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    feat(satp-hermes): implement monitor service

--- packages/cactus-plugin-satp-hermes/src/test/typescript/environments/ethereum-test-environment.ts
+++ packages/cactus-plugin-satp-hermes/src/test/typescript/environments/ethereum-test-environment.ts
@@ -26,7 +26,7 @@ import { ClaimFormat } from "../../../main/typescript/generated/proto/cacti/satp
 import { Asset } from "../../../main/typescript";
 import BesuSATPInteraction from "../../solidity/satp-erc20-interact.json";
 import { LedgerType } from "@hyperledger/cactus-core-api";
-import { NetworkId } from "../../../main/typescript/network-identification/chainid-list";
+import { NetworkId } from "../../../main/typescript/services/network-identification/chainid-list";
 
 // Test environment for Ethereum ledger operations
 export class EthereumTestEnvironment {
