d425c3eeabdf7b72dde1290e99963200158779bc
Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
2025-05-16 16:41:49 +0100

    fix: besu network and cdbc example
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/test/typescript/environments/besu-test-environment.ts
+++ packages/cactus-plugin-satp-hermes/src/test/typescript/environments/besu-test-environment.ts
@@ -85,6 +85,8 @@ export class BesuTestEnvironment {
     this.ledger = new BesuTestLedger({
       emitContainerLogs: true,
       envVars: ["BESU_NETWORK=dev"],
+      containerImageVersion: "2024-06-09-cc2f9c5",
+      containerImageName: "ghcr.io/hyperledger/cactus-besu-all-in-one",
       networkName: this.dockerNetwork,
     });
 
