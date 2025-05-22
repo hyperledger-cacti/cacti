26a7799f43d44485e6aeca05e4262428cccf4c34
Andre Augusto <andre.augusto@tecnico.ulisboa.pt>
2025-05-23 00:09:28 +0100

    fix(satp-hermes): added missing try/expect causing unexpected gateway crash
    * Also removed ABIs and bytecode from the response
    /oracle/status endpoint
    
    Signed-off-by: Andre Augusto <andre.augusto@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/api1/oracle/oracle-get-status-handler-service.ts
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/api1/oracle/oracle-get-status-handler-service.ts
@@ -1,6 +1,10 @@
 import { LoggerProvider, LogLevelDesc } from "@hyperledger/cactus-common";
 import { OracleManager } from "../../cross-chain-mechanisms/oracle/oracle-manager";
-import { OracleStatusRequest, OracleTask } from "../../public-api";
+import {
+  OracleOperation,
+  OracleStatusRequest,
+  OracleTask,
+} from "../../public-api";
 
 export async function getTaskStatus(
   logLevel: LogLevelDesc,
@@ -19,5 +23,38 @@ export async function getTaskStatus(
     throw new Error(`${fnTag} - missing required parameters for task status`);
   }
 
-  return manager.getTask(req.taskID);
+  const task = manager.getTask(req.taskID);
+
+  const returnTask = {
+    taskID: task.taskID,
+    type: task.type,
+    srcNetworkId: task.srcNetworkId,
+    srcContract: task.srcContract,
+    dstNetworkId: task.dstNetworkId,
+    dstContract: task.dstContract,
+    timestamp: task.timestamp,
+    operations: task.operations.map((operation: OracleOperation) => ({
+      id: operation.id,
+      type: operation.type,
+      networkId: operation.networkId,
+      contract: {
+        contractName: operation.contract.contractName,
+        contractAddress: operation.contract.contractAddress,
+        methodName: operation.contract.methodName,
+        params: operation.contract.params,
+      },
+      status: operation.status,
+      output: operation.output,
+      timestamp: operation.timestamp,
+    })),
+    status: task.status,
+    mode: task.mode,
+    pollingInterval: task.pollingInterval,
+    listeningOptions: task.listeningOptions,
+  } as unknown as OracleTask;
+
+  // we will clean the task for the user and remove any bytecode and abis from the response
+  // otherwise, the output is very large and not user friendly
+
+  return returnTask;
 }
