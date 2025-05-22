4a20365280ef81c58c8fd3dd7a758ce731fe5c03
Andre Augusto <andre.augusto@tecnico.ulisboa.pt>
2025-05-23 00:09:28 +0100

    fix(satp-hermes): added missing try/expect causing unexpected gateway crash
    * Also removed ABIs and bytecode from the response /oracle/status
    endpoint
    
    Signed-off-by: Andre Augusto <andre.augusto@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/test/typescript/constants.ts
+++ packages/cactus-plugin-satp-hermes/src/test/typescript/constants.ts
@@ -1,2 +1,2 @@
 export const DOCKER_IMAGE_NAME = "aaugusto11/cacti-satp-hermes-gateway";
-export const DOCKER_IMAGE_VERSION = "ed371241a-2025-05-24";
+export const DOCKER_IMAGE_VERSION = "cc18032a5-2025-05-23";
