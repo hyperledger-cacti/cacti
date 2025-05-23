613848f774d3f4287ffb2a0bce3bbcaaae2ab50e
Andre Augusto <andre.augusto@tecnico.ulisboa.pt>
2025-05-23 22:17:06 +0100

    feat(satp-hermes): remove docker-compose.yml and update docker image
    Signed-off-by: Andre Augusto <andre.augusto@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/test/typescript/constants.ts
+++ packages/cactus-plugin-satp-hermes/src/test/typescript/constants.ts
@@ -1,2 +1,2 @@
 export const DOCKER_IMAGE_NAME = "aaugusto11/cacti-satp-hermes-gateway";
-export const DOCKER_IMAGE_VERSION = "d2bf9f79c-2025-05-21";
+export const DOCKER_IMAGE_VERSION = "deb9e0962-2025-05-23";
