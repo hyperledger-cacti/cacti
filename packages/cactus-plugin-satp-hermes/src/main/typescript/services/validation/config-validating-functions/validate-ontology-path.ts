d7b940a68653cacf2cbae2b340aa8ef676541572
Edson da Veiga <edson.da.veiga@tecnico.ulisboa.pt>
2025-05-28 18:52:50 +0100

    test(satp-hermes): unit tests for the config validators
    Signed-off-by: Edson da Veiga <edson.da.veiga@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/services/validation/config-validating-functions/validate-ontology-path.ts
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/services/validation/config-validating-functions/validate-ontology-path.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+
 export function isOntologyPath(obj: unknown): obj is string | undefined {
   return typeof obj === "string" || typeof obj === "undefined";
 }
@@ -14,6 +16,17 @@ export function validateOntologyPath(opts: {
       `Invalid config.ontologyPath: ${JSON.stringify(
         opts.configValue,
       )}. Expected a string.`,
+    );
+  }
+
+  // Validate that the path exists and is a directory
+  const path = opts.configValue;
+  if (
+    typeof path === "string" &&
+    (!fs.existsSync(path) || !fs.statSync(path).isDirectory())
+  ) {
+    throw new TypeError(
+      `Invalid config.ontologyPath: ${path}. Directory does not exist or is not a folder.`,
     );
   }
   return opts.configValue;
