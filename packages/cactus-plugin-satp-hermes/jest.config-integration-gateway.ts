0333741aac4f609e7a78953d32f4a332280a9d6a
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2025-08-29 17:34:04 +0300

    test(satp-hermes): add jest reporter to satp-hermes
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

---
+++ packages/cactus-plugin-satp-hermes/jest.config-integration-gateway.ts
@@ -0,0 +1,32 @@
+/** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
+const path = require('path');
+module.exports = {
+	preset: 'ts-jest',
+	logHeapUsage: true,
+	testEnvironment: 'node',
+	maxWorkers: 1,
+	maxConcurrency: 3,
+	testTimeout: 60 * 60 * 1000,
+	setupFilesAfterEnv: [
+		'jest-extended/all',
+		path.resolve(__dirname, '../../jest.setup.console.logs.js'),
+	],
+	moduleNameMapper: {
+		'^(\\.\\.?\\/.+)\\.jsx?$': '$1',
+		'^(.+)/(.+)_pb\\.js$': '$1/$2_pb',
+	},
+	testMatch: [
+		'**/src/test/typescript/integration/gateway/*.test.ts',
+	],
+	testPathIgnorePatterns: [],
+	reporters: [
+		'default',
+		[
+			'jest-junit',
+			{
+				outputDirectory: 'reports/junit',
+				outputName: 'satp-hermes-tests-integration-gateway.xml',
+			},
+		],
+	],
+};
