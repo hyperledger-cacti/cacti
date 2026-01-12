0ea87a8f179e48cb3d848f798eaa8ccf15377fa5
Rodolfo Carapau <rodolfo.carapau@gmail.com>
2026-01-12 22:46:30 +0000

    test(satp-hermes): added tests scaffolding
    Signed-off-by: Rodolfo Carapau <rodolfo.carapau@gmail.com>

---
+++ packages/cactus-plugin-satp-hermes/src/test/typescript/integration/admin/satp-admin-audit-endpoint.test.ts
@@ -0,0 +1,29 @@
+// example.test.js
+
+describe("Audit Endpoint Integration Tests", () => {
+  beforeAll(() => {});
+
+  beforeEach(() => {});
+
+  afterEach(() => {});
+
+  afterAll(() => {});
+
+  it("Given valid StartTimestampAndEndTimestamp, When calling an available audit endpoint, Then return Audit data and 200 OK response", () => {
+    // Given
+    // When
+    // Then
+  });
+
+  it("Given invalid StartTimestampAndEndTimestamp, When calling an available audit endpoint, Then return 400 Bad Request response", () => {
+    // Given
+    // When
+    // Then
+  });
+
+  it("Given valid StartTimestampAndEndTimestamp, When calling an unavailable audit endpoint, Then return 503 Service Unavailable response", () => {
+    // Given
+    // When
+    // Then
+  });
+});
---
+++ packages/cactus-plugin-satp-hermes/src/test/typescript/integration/admin/satp-admin-audit-repository.test.ts
@@ -0,0 +1,21 @@
+describe("Audit Endpoint repository Tests", () => {
+  beforeAll(() => {});
+
+  beforeEach(() => {});
+
+  afterEach(() => {});
+
+  afterAll(() => {});
+
+  it("Given valid StartTimestampAndEndTimestamp, When calling GetAuditDataByStartTimestampAndEndTimestamp, Then valid AuditEntry", () => {
+    // Given
+    // When
+    // Then
+  });
+
+  it("Given invalid StartTimestampAndEndTimestamp, When calling GetAuditDataByStartTimestampAndEndTimestamp, Then throw INVALID_TIMESTAMP error", () => {
+    // Given
+    // When
+    // Then
+  });
+});
