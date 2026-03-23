cef3f7f827d9b47c8048f8c4565ec1ec14409d2b
Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
2026-03-23 14:11:54 +0000

    chore(weaver): refactor local build in npm packages
    ci: upgrade npm to yarn
    chore: fix custom check, type declaration, use single tsconfig
    refactor: index.ts now generates index.js and .d.ts
    chor: remove package-local references in weaver
    chore: upgrade weaver scripts to use yarn
    ci(weaver): stabilize besu asset-exchange workflow
    chore: fix local build using yarn
    fix(codegen): rebuild grpc-tools when protoc binary is missing
    chore: fix iinangent, fabric driver docker images
    
    Co-authored-by: Sandeep Nishad <sandeepn.official@gmail.com>
    
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
    Signed-off-by: Sandeep Nishad <sandeepn.official@gmail.com>

---
+++ weaver/sdks/besu/node/src/index.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as AssetManager from "./AssetManager";
+export { AssetManager };
+import * as HashFunctions from "./HashFunctions";
+export { HashFunctions };
