b0586294080b3c5d5143aee05c3586e4f6768ae5
Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
2026-04-20 17:09:12 +0100

    weaver-ci: remove local go.mod replace directives from copied fabric chaincode artifacts
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>

--- weaver/tests/network-setups/fabric/dev/scripts/setupCCLocal.sh
+++ weaver/tests/network-setups/fabric/dev/scripts/setupCCLocal.sh
@@ -15,6 +15,28 @@ if [ -d "${CHAINCODE_PATH}/interop" ]; then
 fi
 (cd $INTEROP_CC_PATH/contracts/interop && make run-vendor)
 cp -r $INTEROP_CC_PATH/contracts/interop $CHAINCODE_PATH/interop
+
+# Remove local path replace directives from the copied chaincode so packaging
+# in network artifacts does not depend on repository-relative paths.
+(cd $CHAINCODE_PATH/interop && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/common/protos-go/v2 || true)
+(cd $CHAINCODE_PATH/interop && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/common/protos-go/v3 || true)
+(cd $CHAINCODE_PATH/interop && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/core/network/fabric-interop-cc/interfaces/asset-mgmt/v2 || true)
+(cd $CHAINCODE_PATH/interop && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/core/network/fabric-interop-cc/interfaces/asset-mgmt/v3 || true)
+(cd $CHAINCODE_PATH/interop && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/core/network/fabric-interop-cc/libs/assetexchange/v2 || true)
+(cd $CHAINCODE_PATH/interop && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/core/network/fabric-interop-cc/libs/assetexchange/v3 || true)
+(cd $CHAINCODE_PATH/interop && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/core/network/fabric-interop-cc/libs/utils/v2 || true)
+(cd $CHAINCODE_PATH/interop && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/core/network/fabric-interop-cc/libs/utils/v3 || true)
+(cd $CHAINCODE_PATH/interop && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/core/network/fabric-interop-cc/libs/testutils || true)
+
 (cd $INTEROP_CC_PATH/contracts/interop && make undo-vendor)
 echo "Done."
 
