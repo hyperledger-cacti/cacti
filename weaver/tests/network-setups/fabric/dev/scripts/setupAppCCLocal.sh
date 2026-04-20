8cc3bc432c25199c04ff3f61cfa073d24644fe31
Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>
2026-04-20 17:09:12 +0100

    weaver-ci: remove local go.mod replace directives from copied fabric chaincode artifacts
    Signed-off-by: Carlos Amaro <carlosrscamaro@tecnico.ulisboa.pt>

--- weaver/tests/network-setups/fabric/dev/scripts/setupAppCCLocal.sh
+++ weaver/tests/network-setups/fabric/dev/scripts/setupAppCCLocal.sh
@@ -19,6 +19,28 @@ fi
 
 (cd ${APP_CC_PATH} && make run-vendor)
 cp -r ${APP_CC_PATH} ${CHAINCODE_PATH}/${CC_NAME}
+
+# Remove repository-local replace directives from the copied chaincode while
+# keeping the vendored dependencies for deployment.
+(cd ${CHAINCODE_PATH}/${CC_NAME} && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/common/protos-go/v2 || true)
+(cd ${CHAINCODE_PATH}/${CC_NAME} && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/common/protos-go/v3 || true)
+(cd ${CHAINCODE_PATH}/${CC_NAME} && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/core/network/fabric-interop-cc/interfaces/asset-mgmt/v2 || true)
+(cd ${CHAINCODE_PATH}/${CC_NAME} && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/core/network/fabric-interop-cc/interfaces/asset-mgmt/v3 || true)
+(cd ${CHAINCODE_PATH}/${CC_NAME} && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/core/network/fabric-interop-cc/libs/assetexchange/v2 || true)
+(cd ${CHAINCODE_PATH}/${CC_NAME} && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/core/network/fabric-interop-cc/libs/assetexchange/v3 || true)
+(cd ${CHAINCODE_PATH}/${CC_NAME} && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/core/network/fabric-interop-cc/libs/utils/v2 || true)
+(cd ${CHAINCODE_PATH}/${CC_NAME} && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/core/network/fabric-interop-cc/libs/utils/v3 || true)
+(cd ${CHAINCODE_PATH}/${CC_NAME} && \
+    go mod edit -dropreplace github.com/hyperledger-cacti/cacti/weaver/core/network/fabric-interop-cc/libs/testutils || true)
+
 (cd ${APP_CC_PATH} && make undo-vendor)
 
 echo "Done."
