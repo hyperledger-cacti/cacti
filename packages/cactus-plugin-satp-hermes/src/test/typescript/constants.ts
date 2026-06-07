70f513cfd256a43ed3df1882656a36df1e4822fd
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2026-06-07 19:27:21 +0300

    fix: satp workflows
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/test/typescript/constants.ts
+++ packages/cactus-plugin-satp-hermes/src/test/typescript/constants.ts
@@ -1,3 +1,2 @@
-//export const SATP_DOCKER_IMAGE_NAME = "hyperledger/cacti-satp-hermes-gateway";
-export const SATP_DOCKER_IMAGE_NAME = "kubaya/cacti-satp-hermes-gateway";
-export const SATP_DOCKER_IMAGE_VERSION = "5f190f37f-2025-08-19";
+export const SATP_DOCKER_IMAGE_NAME = "rafaelapb/cacti-satp-hermes-gateway";
+export const SATP_DOCKER_IMAGE_VERSION = "612643f9f-2026-06-07";
