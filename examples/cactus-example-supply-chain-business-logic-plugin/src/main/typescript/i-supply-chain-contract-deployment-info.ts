094a12b7e03e0731e250493f71b06d793ec4a958
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2026-06-08 17:03:32 +0300

    fix: fix tests
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    fix: fix tests
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    chore: archive cacti supply chain example
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    fix fix satp tests
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    chore: remove supply chain example
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

--- examples/cactus-example-supply-chain-business-logic-plugin/src/main/typescript/i-supply-chain-contract-deployment-info.ts
+++
@@ -1,8 +0,0 @@
-import { IEthContractDeployment } from "./i-eth-contract-deployment";
-import { IFabricContractDeployment } from "./i-fabric-contract-deployment";
-
-export interface ISupplyChainContractDeploymentInfo {
-  bambooHarvestRepository: IEthContractDeployment;
-  bookshelfRepository: IEthContractDeployment;
-  shipmentRepository: IFabricContractDeployment;
-}
