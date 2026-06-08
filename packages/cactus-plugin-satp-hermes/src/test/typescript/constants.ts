e391259aa31e02b143608b7a958dcfaa7d5c0557
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2026-06-08 17:03:32 +0300

    fix: fix tests
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    fix: fix tests
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    chore: archive cacti supply chain example
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    fix fix satp tests
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
    
    chore: remove supply chain example
    
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/test/typescript/constants.ts
+++ packages/cactus-plugin-satp-hermes/src/test/typescript/constants.ts
@@ -1,2 +1,3 @@
+// TODO replace with the official after the release
 export const SATP_DOCKER_IMAGE_NAME = "rafaelapb/cacti-satp-hermes-gateway";
-export const SATP_DOCKER_IMAGE_VERSION = "612643f9f-2026-06-07";
+export const SATP_DOCKER_IMAGE_VERSION = "2026-06-08-dev-788b9a916";
