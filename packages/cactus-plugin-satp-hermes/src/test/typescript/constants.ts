67dcc743dafc60fe1e4902f63b05eb68d23cee2e
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2026-06-09 16:47:12 +0300

    fix: fix satp race condition
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

--- packages/cactus-plugin-satp-hermes/src/test/typescript/constants.ts
+++ packages/cactus-plugin-satp-hermes/src/test/typescript/constants.ts
@@ -1,3 +1,3 @@
 // TODO replace with the official after the release
 export const SATP_DOCKER_IMAGE_NAME = "rafaelapb/cacti-satp-hermes-gateway";
-export const SATP_DOCKER_IMAGE_VERSION = "2026-06-08-dev-788b9a916";
+export const SATP_DOCKER_IMAGE_VERSION = "2026-06-09-dev-593afe351";
