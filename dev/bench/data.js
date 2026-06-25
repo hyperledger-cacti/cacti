dc9d77c5888c701f93174bac85dd09b94fda6193
github-action-benchmark <github@users.noreply.github.com>
2026-06-25 09:20:48 +0000

    add Benchmark (benchmarkjs) benchmark result for 25e0fd3169dee9414fabe773b30557e5d08d9c03

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782378858897,
+  "lastUpdate": 1782379248402,
   "repoUrl": "https://github.com/hyperledger-cacti/cacti",
   "entries": {
     "Benchmark": [
@@ -101,6 +101,35 @@ window.BENCHMARK_DATA = {
             "range": "±1.88%",
             "unit": "ops/sec",
             "extra": "183 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Sandeep Nishad",
+            "username": "sandeepnRES",
+            "email": "sandeepn.official@gmail.com"
+          },
+          "committer": {
+            "name": "Sandeep Nishad",
+            "username": "sandeepnRES",
+            "email": "sandeepn.official@gmail.com"
+          },
+          "id": "25e0fd3169dee9414fabe773b30557e5d08d9c03",
+          "message": "ci: validate PR type and verify PR title matches commit message\n\nAssisted-by: Google:Gemini\nSigned-off-by: Sandeep Nishad <sandeepn.official@gmail.com>",
+          "timestamp": "2026-05-18T08:26:42Z",
+          "url": "https://github.com/hyperledger-cacti/cacti/commit/25e0fd3169dee9414fabe773b30557e5d08d9c03"
+        },
+        "date": 1782379245273,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 865,
+            "range": "±3.60%",
+            "unit": "ops/sec",
+            "extra": "179 samples"
           }
         ]
       }
