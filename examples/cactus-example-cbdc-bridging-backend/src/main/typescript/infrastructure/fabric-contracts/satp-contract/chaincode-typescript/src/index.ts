96b9fa089500192f7aa421783bf15713b8eeae58
Parth Singh <posiedon.1721@gmail.com>
2026-07-15 08:34:10 +0000

    refactor: remove cactus-example-cbdc-bridging-backend
    Remove package from workspace and CI to migrate it to the
    cacti-demos repository.
    Refs: hyperledger-cacti/cacti#4543
    
    Signed-off-by: Parth Singh <posiedon.1721@gmail.com>

--- examples/cactus-example-cbdc-bridging-backend/src/main/typescript/infrastructure/fabric-contracts/satp-contract/chaincode-typescript/src/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { SATPContract } from "./satp-contract";
-
-export { SATPContract } from "./satp-contract";
-
-export const contracts: any[] = [SATPContract];
